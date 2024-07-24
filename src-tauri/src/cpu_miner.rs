b1d36318016fe2424587e135faa340016170b3ab
stringhandler <stringhandler@protonmail.com>
2024-07-24 17:11:53 +0200

    wip

--- src-tauri/src/cpu_miner.rs
+++ src-tauri/src/cpu_miner.rs
@@ -1,20 +1,23 @@
+use crate::xmrig_adapter::{XmrigAdapter, XmrigNodeConnection};
 use tari_shutdown::{Shutdown, ShutdownSignal};
-use tauri::api::process::{Command};
+use tauri::api::process::Command;
 use tauri::async_runtime::JoinHandle;
 use tokio::select;
 use tokio::time::MissedTickBehavior;
-use crate::xmrig_adapter::XmrigAdapter;
+use crate::{CpuMinerConfig, CpuMinerConnection};
+use crate::mm_proxy_manager::MmProxyManager;
 
-
-pub enum CpuMinerEvent{
+pub enum CpuMinerEvent {
     Stdout(String),
     Stderr(String),
-    Exit(i32)
+    Exit(i32),
 }
 
+
+
 pub(crate) struct CpuMiner {
-    watcher_task : Option<JoinHandle<Result<(), anyhow::Error>>>,
-    miner_shutdown: Shutdown
+    watcher_task: Option<JoinHandle<Result<(), anyhow::Error>>>,
+    miner_shutdown: Shutdown,
 }
 
 impl CpuMiner {
@@ -25,19 +28,24 @@ impl CpuMiner {
         }
     }
 
-    pub fn start(&mut self, mut app_shutdown: ShutdownSignal) -> Result<(), anyhow::Error> {
+    pub async fn start(&mut self, mut app_shutdown: ShutdownSignal,  cpu_miner_config: &CpuMinerConfig, local_mm_proxy: &MmProxyManager) -> Result<(), anyhow::Error> {
         if self.watcher_task.is_some() {
             println!("Tried to start mining twice");
             return Ok(());
         }
         let mut inner_shutdown = self.miner_shutdown.to_signal();
 
-        // let (mut rx, mut child) = Command::new_sidecar("minotari_node")
-        //     .expect("failed to create `minotari_node` binary command")
-        //     .spawn()
-        //     .expect("Failed to spawn sidecar");
 
-        let xmrig = XmrigAdapter::new();
+        let xmrig_node_connection = match cpu_miner_config.node_connection {
+            CpuMinerConnection::BuiltInProxy => {
+                local_mm_proxy.wait_ready().await?;
+                XmrigNodeConnection::LocalMmproxy {
+                    host_name: "127.0.0.1".to_string(),
+                    port: local_mm_proxy.try_get_listening_port().await?
+                }
+            }
+        };
+        let xmrig = XmrigAdapter::new(xmrig_node_connection);
         let (mut rx, mut xmrig_child) = xmrig.spawn()?;
 
         self.watcher_task = Some(tauri::async_runtime::spawn(async move {
@@ -46,55 +54,53 @@ impl CpuMiner {
             watch_timer.set_missed_tick_behavior(MissedTickBehavior::Skip);
             // read events such as stdout
             loop {
-               select! {
-                      _ = watch_timer.tick() => {
-                        println!("watching");
-                       if xmrig_child.ping().expect("idk") {
-                           println!("xmrig is running");
-                       } else {
-                           println!("xmrig is not running");
-                           match xmrig_child.stop().await {
-                                 Ok(()) => {
-                                      println!("xmrig exited successfully");
-                                 }
-                                 Err(e) => {
-                                      println!("xmrig exited with error: {}", e);
-                                 }
-                           }
-                           break;
-                       }
-                      },
-                    //   event = rx.recv() => {
-                    //     if let Some(event) = event {
-                    //
-                    //   // if let CommandEvent::Stdout(line) = event {
-                    //   //    window
-                    //   //   .emit("message", Some(format!("'{}'", line)))
-                    //   //   .expect("failed to emit event");
-                    // // write to stdin
-                    // //child.write("message from Rust\n".as_bytes()).unwrap();
-                    //
-                    //    }
-                    // else {
-                    //  break;
-                    // }
-              //         },
-    //
-                _ = inner_shutdown.wait() => {
-                    xmrig_child.stop().await?;
-                    break;
-                },
-                _ = app_shutdown.wait() => {
-                    xmrig_child.stop().await?;
-                    break;
-                }
+                select! {
+                              _ = watch_timer.tick() => {
+                                    println!("watching");
+                                    if xmrig_child.ping().expect("idk") {
+                                       println!("xmrig is running");
+                                    } else {
+                                       println!("xmrig is not running");
+                                       match xmrig_child.stop().await {
+                                           Ok(()) => {
+                                              println!("xmrig exited successfully");
+                                           }
+                                           Err(e) => {
+                                              println!("xmrig exited with error: {}", e);
+                                           }
+                                       }
+                                       break;
+                                    }
+                              },
+                                //   event = rx.recv() => {
+                                //     if let Some(event) = event {
+                                //
+                                //   // if let CommandEvent::Stdout(line) = event {
+                                //   //    window
+                                //   //   .emit("message", Some(format!("'{}'", line)))
+                                //   //   .expect("failed to emit event");
+                                // // write to stdin
+                                // //child.write("message from Rust\n".as_bytes()).unwrap();
+                                //
+                                //    }
+                                // else {
+                                //  break;
+                                // }
+                          //         },
+                //
+                            _ = inner_shutdown.wait() => {
+                                xmrig_child.stop().await?;
+                                break;
+                            },
+                            _ = app_shutdown.wait() => {
+                                xmrig_child.stop().await?;
+                                break;
+                            }
+                        }
             }
-
-
-        }
-        Ok(())
-        }));
             Ok(())
+        }));
+        Ok(())
     }
 
     pub async fn stop(&mut self) -> Result<(), anyhow::Error> {
---
+++ src-tauri/src/process_adapter.rs
@@ -0,0 +1,13 @@
+use tokio::sync::mpsc::Receiver;
+use async_trait::async_trait;
+
+pub trait ProcessAdapter<TInstance: ProcessInstance> {
+    fn spawn(&self) -> Result<(Receiver<()>, TInstance), anyhow::Error>;
+    fn name(&self) -> &str;
+}
+
+#[async_trait]
+pub trait ProcessInstance : Send + Sync + 'static {
+    fn ping(&self) -> bool;
+    async fn stop(&self) -> Result<(), anyhow::Error>;
+}
