e308b0f85ccb82f9b0f47d0ebaa3d41e4328265c
Erika <nova@tari.com>
2024-08-06 12:54:07 +0200

    fix: wallet balance formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,88 +1,101 @@
 import './theme/theme.css';
-import {useEffect} from 'react';
-import {invoke} from '@tauri-apps/api/tauri';
-import {listen} from '@tauri-apps/api/event';
+import { useEffect } from 'react';
+import { invoke } from '@tauri-apps/api/tauri';
+import { listen } from '@tauri-apps/api/event';
 import CssBaseline from '@mui/material/CssBaseline';
-import {ThemeProvider} from '@mui/material/styles';
-import {lightTheme} from './theme/themes';
-import {ContainerInner, DashboardContainer} from './theme/styles';
-import {SideBar} from './containers/SideBar';
-import {Dashboard} from './containers/Dashboard';
-import {TitleBar} from './containers/TitleBar';
-import {AppBackground} from './containers/AppBackground';
+import { ThemeProvider } from '@mui/material/styles';
+import { lightTheme } from './theme/themes';
+import { ContainerInner, DashboardContainer } from './theme/styles';
+import { SideBar } from './containers/SideBar';
+import { Dashboard } from './containers/Dashboard';
+import { TitleBar } from './containers/TitleBar';
+import { AppBackground } from './containers/AppBackground';
 import useAppStateStore from './store/appStateStore';
+import useWalletStore from './store/walletStore';
 import ErrorSnackbar from './containers/Error/ErrorSnackbar';
 
 function App() {
-    const {
-        view, background, setHashRate, setCpuUsage, setAppState, setError,
-        setCpuBrand, setEstimatedEarnings,
-        setBlockHeight, setBlockTime, setIsSynced, setWallet
-    } =
-        useAppStateStore((state) => ({
-            view: state.view,
-            background: state.background,
-            setHashRate: state.setHashRate,
-            setCpuUsage: state.setCpuUsage,
-            setAppState: state.setAppState,
-            setError: state.setError,
-            setCpuBrand: state.setCpuBrand,
-            setEstimatedEarnings: state.setEstimatedEarnings,
-            setBlockHeight: state.setBlockHeight,
-            setBlockTime: state.setBlockTime,
-            setIsSynced: state.setIsSynced,
-            setWallet: state.setWallet
-        }));
+  const {
+    view,
+    background,
+    setHashRate,
+    setCpuUsage,
+    setAppState,
+    setError,
+    setCpuBrand,
+    setEstimatedEarnings,
+    setBlockHeight,
+    setBlockTime,
+    setIsSynced,
+  } = useAppStateStore((state) => ({
+    view: state.view,
+    background: state.background,
+    setHashRate: state.setHashRate,
+    setCpuUsage: state.setCpuUsage,
+    setAppState: state.setAppState,
+    setError: state.setError,
+    setCpuBrand: state.setCpuBrand,
+    setEstimatedEarnings: state.setEstimatedEarnings,
+    setBlockHeight: state.setBlockHeight,
+    setBlockTime: state.setBlockTime,
+    setIsSynced: state.setIsSynced,
+  }));
 
-    useEffect(() => {
-        invoke("init", {}).catch((e) => {
-            console.error('Could not init', e);
-            setError(e.toString());
+  const setBalance = useWalletStore((state) => state.setBalance);
+
+  useEffect(() => {
+    invoke('init', {}).catch((e) => {
+      console.error('Could not init', e);
+      setError(e.toString());
+    });
+    const unlistenPromise = listen('message', (event) => {
+      console.log('some kind of event', event.event, event.payload);
+    });
+
+    const intervalId = setInterval(() => {
+      invoke('status', {})
+        .then((status: any) => {
+          console.log('Status', status);
+          setAppState(status);
+          setCpuUsage(status.cpu?.cpu_usage);
+          setHashRate(status.cpu?.hash_rate);
+          setCpuBrand(status.cpu?.cpu_brand);
+          setEstimatedEarnings(status.cpu?.estimated_earnings);
+          setBlockHeight(status.base_node?.block_height);
+          setBlockTime(status.base_node?.block_time);
+          setIsSynced(status.base_node?.is_synced);
+          setBalance(
+            status.wallet_balance?.available_balance +
+              status.wallet_balance?.timelocked_balance +
+              status.wallet_balance?.pending_incoming_balance
+          );
+        })
+        .catch((e) => {
+          console.error('Could not get status', e);
+          setError(e.toString());
         });
-        const unlistenPromise = listen('message', (event) => {
-            console.log('some kind of event', event.event, event.payload);
-        });
+    }, 1000);
+    return () => {
+      unlistenPromise.then((unlisten) => unlisten());
+      clearInterval(intervalId);
+    };
+  }, []);
 
-        const intervalId = setInterval(() => {
-            invoke('status', {})
-                .then((status: any) => {
-                    console.log('Status', status);
-                    setAppState(status);
-                    setCpuUsage(status.cpu?.cpu_usage);
-                    setHashRate(status.cpu?.hash_rate);
-                    setCpuBrand(status.cpu?.cpu_brand);
-                    setEstimatedEarnings(status.cpu?.estimated_earnings);
-                    setBlockHeight(status.base_node?.block_height);
-                    setBlockTime(status.base_node?.block_time);
-                    setIsSynced(status.base_node?.is_synced);
-                    setWallet({balance: status.wallet_balance?.available_balance + status.wallet_balance?.timelocked_balance + status.wallet_balance?.pending_incoming_balance});
-                })
-                .catch((e) => {
-                    console.error('Could not get status', e);
-                    setError(e.toString());
-                });
-        }, 1000);
-        return () => {
-            unlistenPromise.then((unlisten) => unlisten());
-            clearInterval(intervalId);
-        };
-    }, []);
-
-    return (
-        <ThemeProvider theme={lightTheme}>
-            <CssBaseline enableColorScheme/>
-            <AppBackground status={background}>
-                <DashboardContainer>
-                    <TitleBar/>
-                    <ContainerInner>
-                        <SideBar/>
-                        <Dashboard status={view}/>
-                    </ContainerInner>
-                </DashboardContainer>
-            </AppBackground>
-            <ErrorSnackbar/>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <CssBaseline enableColorScheme />
+      <AppBackground status={background}>
+        <DashboardContainer>
+          <TitleBar />
+          <ContainerInner>
+            <SideBar />
+            <Dashboard status={view} />
+          </ContainerInner>
+        </DashboardContainer>
+      </AppBackground>
+      <ErrorSnackbar />
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- src/store/appStateStore.ts
+++ src/store/appStateStore.ts
@@ -1,117 +1,111 @@
-import {create} from 'zustand';
-import {invoke} from '@tauri-apps/api/tauri';
-import {viewType, backgroundType, modeType} from './types';
+import { create } from 'zustand';
+import { invoke } from '@tauri-apps/api/tauri';
+import { viewType, backgroundType, modeType } from './types';
 
 interface AppState {
-    appState: any;
-    setAppState: (value: any) => void;
-    error: string;
-    setError: (value: string) => void;
-    topStatus: string;
-    setTopStatus: (value: string) => void;
-    errorOpen: boolean;
-    setErrorOpen: (value: boolean) => void;
+  appState: any;
+  setAppState: (value: any) => void;
+  error: string;
+  setError: (value: string) => void;
+  topStatus: string;
+  setTopStatus: (value: string) => void;
+  errorOpen: boolean;
+  setErrorOpen: (value: boolean) => void;
 
-    // gui
-    background: backgroundType;
-    setBackground: (value: backgroundType) => void;
-    view: viewType;
-    setView: (value: viewType) => void;
-    visualMode: boolean;
-    setVisualMode: (value: boolean) => void;
-    wallet: {
-        balance: number;
-    };
-    setWallet: (value: { balance: number }) => void;
-    isMining: boolean;
-    setIsMining: (value: boolean) => void;
-    isAutoMining: boolean;
-  setIsAutoMining: (value: boolean) => void;sidebarOpen: boolean;
-    setSidebarOpen: (value: boolean) => void;
+  // gui
+  background: backgroundType;
+  setBackground: (value: backgroundType) => void;
+  view: viewType;
+  setView: (value: viewType) => void;
+  visualMode: boolean;
+  setVisualMode: (value: boolean) => void;
+  isMining: boolean;
+  setIsMining: (value: boolean) => void;
+  isAutoMining: boolean;
+  setIsAutoMining: (value: boolean) => void;
+  sidebarOpen: boolean;
+  setSidebarOpen: (value: boolean) => void;
 
-    // stats
-    cpuUsage: number;
-    setCpuUsage: (value: number) => void;
-    mode: modeType;
-    setMode: (value: modeType) => void;
-    hashRate: number;
-    setHashRate: (value: number) => void;
-    cpuBrand: string,
-    setCpuBrand: (value: string) => void;
-    estimatedEarnings: number,
-    setEstimatedEarnings: (value: number) => void;
+  // stats
+  cpuUsage: number;
+  setCpuUsage: (value: number) => void;
+  mode: modeType;
+  setMode: (value: modeType) => void;
+  hashRate: number;
+  setHashRate: (value: number) => void;
+  cpuBrand: string;
+  setCpuBrand: (value: string) => void;
+  estimatedEarnings: number;
+  setEstimatedEarnings: (value: number) => void;
 
-    blockHeight: number,
-    setBlockHeight: (value: number) => void;
-  blockTime: number,
-    setBlockTime: (value: number) => void;
-  isSynced: boolean,
-    setIsSynced: (value: boolean) => void;// functions
-    startMining: () => Promise<void>;
-    stopMining: () => Promise<void>;
+  blockHeight: number;
+  setBlockHeight: (value: number) => void;
+  blockTime: number;
+  setBlockTime: (value: number) => void;
+  isSynced: boolean;
+  setIsSynced: (value: boolean) => void; // functions
+  startMining: () => Promise<void>;
+  stopMining: () => Promise<void>;
 }
 
 const useAppStateStore = create<AppState>((set) => ({
-    appState: {},
-    setAppState: (value) => set({appState: value}),
-    error: '',
-    setError: (value) => set({error: value}),
-    topStatus: 'Not mining',
-    setTopStatus: (value) => set({topStatus: value}),
-    errorOpen: false,
-    setErrorOpen: (value) => set({errorOpen: value}),
+  appState: {},
+  setAppState: (value) => set({ appState: value }),
+  error: '',
+  setError: (value) => set({ error: value }),
+  topStatus: 'Not mining',
+  setTopStatus: (value) => set({ topStatus: value }),
+  errorOpen: false,
+  setErrorOpen: (value) => set({ errorOpen: value }),
 
-    // gui
-    background: 'idle',
-    setBackground: (value) => set({background: value}),
-    view: 'mining',
-    setView: (value) => set({view: value}),
-    visualMode: true,
-    setVisualMode: (value) => set({visualMode: value}),
-    wallet: {
-        balance: 0
-    },
-    setWallet: (value) => set({wallet: value}),
-    isMining: false,
-    setIsMining: (value) => set({isMining: value}),
-    isAutoMining: false,
-  setIsAutoMining: (value) => set({ isAutoMining: value }),sidebarOpen: false,
-    setSidebarOpen: (value) => set({sidebarOpen: value}),
+  // gui
+  background: 'idle',
+  setBackground: (value) => set({ background: value }),
+  view: 'mining',
+  setView: (value) => set({ view: value }),
+  visualMode: true,
+  setVisualMode: (value) => set({ visualMode: value }),
+  isMining: false,
+  setIsMining: (value) => set({ isMining: value }),
+  isAutoMining: false,
+  setIsAutoMining: (value) => set({ isAutoMining: value }),
+  sidebarOpen: false,
+  setSidebarOpen: (value) => set({ sidebarOpen: value }),
 
-    // stats
-    cpuUsage: 0,
-    setCpuUsage: (value) => set({cpuUsage: value}),
-    mode: 'eco',
-    setMode: (value) => set({mode: value}),
-    hashRate: 0,
-    setHashRate: (value) => set({hashRate: value}),
-    cpuBrand: '',
-    setCpuBrand: (value) => set({cpuBrand: value}),
-    estimatedEarnings: 0,
-    setEstimatedEarnings: (value) => set({estimatedEarnings: value}),
+  // stats
+  cpuUsage: 0,
+  setCpuUsage: (value) => set({ cpuUsage: value }),
+  mode: 'eco',
+  setMode: (value) => set({ mode: value }),
+  hashRate: 0,
+  setHashRate: (value) => set({ hashRate: value }),
+  cpuBrand: '',
+  setCpuBrand: (value) => set({ cpuBrand: value }),
+  estimatedEarnings: 0,
+  setEstimatedEarnings: (value) => set({ estimatedEarnings: value }),
 
-    blockHeight: 0,
-    setBlockHeight: (value) => set({ blockHeight: value }),
-    blockTime: 0,
-    setBlockTime: (value) => set({ blockTime: value }),
-    isSynced: false,
-    setIsSynced: (value) => set({ isSynced: value }),// functions
-    startMining: async () => {
-        try {
-            await invoke('start_mining', {});
-            console.log('Mining started');
-        } catch (e) {
-            console.error('Could not start mining', e);
-        }
-    },
-    stopMining: async () => {
-        try {
-            await invoke('stop_mining', {});
-            console.log('Mining stopped');
-        } catch (e) {
-            console.error('Could not stop mining', e);
-        }
-    },
+  blockHeight: 0,
+  setBlockHeight: (value) => set({ blockHeight: value }),
+  blockTime: 0,
+  setBlockTime: (value) => set({ blockTime: value }),
+  isSynced: false,
+  setIsSynced: (value) => set({ isSynced: value }), // functions
+  startMining: async () => {
+    try {
+      await invoke('start_mining', {});
+      console.log('Mining started');
+    } catch (e) {
+      console.error('Could not start mining', e);
+    }
+  },
+  stopMining: async () => {
+    try {
+      await invoke('stop_mining', {});
+      console.log('Mining stopped');
+    } catch (e) {
+      console.error('Could not stop mining', e);
+    }
+  },
 }));
 
 export default useAppStateStore;
