84bb9b9849176669c3e1e14bca170e73ed72b2d7
stringhandler <stringhandler@protonmail.com>
2024-08-07 16:53:47 +0200

    add macos kill

--- src-tauri/src/process_killer.rs
+++ src-tauri/src/process_killer.rs
@@ -6,7 +6,11 @@ pub fn kill_process(pid: u32) -> Result<(), anyhow::Error> {
             .args(&["/F", "/PID", &pid.to_string()])
             .output()?;
     } else {
-        todo!()
+        use nix::sys::signal::{self, Signal};
+        use nix::unistd::Pid;
+
+        let pid = Pid::from_raw(pid as i32);
+        signal::kill(pid, Signal::SIGTERM);
     }
     Ok(())
 }
