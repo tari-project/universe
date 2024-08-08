aaceb136d34863f8785677ca6a73148a04c85cfe
Misieq01 <bartosz2000walczak@gmail.com>
2024-08-08 18:45:06 +0200

    remove unused command

--- src/types/invoke.ts
+++ src/types/invoke.ts
@@ -2,9 +2,6 @@ import { AppStatus } from './app-status';
 
 declare module '@tauri-apps/api/tauri' {
     function invoke(param: 'setup_application'): Promise<void>;
-    function invoke(
-        param: 'check_user_mouse_position'
-    ): Promise<[number, number]>;
     function invoke(param: 'status'): Promise<AppStatus>;
     function invoke(param: 'start_mining'): Promise<void>;
     function invoke(param: 'stop_mining'): Promise<void>;
