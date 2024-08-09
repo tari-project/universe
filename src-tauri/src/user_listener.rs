33f4ae9d243371587ab33c4dcee4d2ed769988fe
Misieq01 <38589417+Misieq01@users.noreply.github.com>
2024-08-09 13:06:20 +0200

    chore(ui): implement listening to outside app activity (#30)
    Description
    ---
    Change autominer behavior so it would listen to user mouse movement
    outside app context
    
    Motivation and Context
    ---
    
    How Has This Been Tested?
    ---
    Manually 
    
    What process can a PR reviewer use to test or verify this change?
    ---
    Turn on autominer and position mouse outside app window then be idle for
    30 seconds, minining should by then start and if you move mouse it
    should stop
    <!-- Checklist -->
    <!-- 1. Is the title of your PR in the form that would make nice release
    notes? The title, excluding the conventional commit
    tag, will be included exactly as is in the CHANGELOG, so please think
    about it carefully. -->
    
    
    Breaking Changes
    ---
    
    - [x] None
    - [ ] Requires data directory on base node to be deleted
    - [ ] Requires hard fork
    - [ ] Other - Please specify
    
    <!-- Does this include a breaking change? If so, include this line as a
    footer -->
    <!-- BREAKING CHANGE: Description what the user should do, e.g. delete a
    database, resync the chain -->

---
+++ src-tauri/src/user_listener.rs
@@ -0,0 +1,121 @@
+use device_query::{DeviceQuery, DeviceState};
+use log::info;
+use tokio::time::{sleep, Duration};
+use tokio_util::sync::CancellationToken;
+
+#[derive(Debug, Clone)]
+pub struct UserListener {
+    pub is_listening: bool,
+    pub is_mining_initialized: bool,
+    pub idle_timeout: u64,
+    pub cancelation_token: Option<CancellationToken>,
+}
+
+#[derive(Clone, serde::Serialize)]
+pub struct UserActivityEventPayload {
+    event_type: String,
+}
+
+impl UserListener {
+    pub fn new() -> Self {
+        Self {
+            is_listening: false,
+            is_mining_initialized: false,
+            idle_timeout: 5000,
+            cancelation_token: None,
+        }
+    }
+
+    pub fn read_user_mouse_coords() -> (i32, i32) {
+        let device_state = DeviceState::new();
+        let mouse = device_state.get_mouse();
+
+        return mouse.coords;
+    }
+
+    pub fn start_listening_to_mouse_poisition_change(&mut self, window: tauri::Window) {
+        let idle_timeout = self.idle_timeout;
+
+        let cancellation_token = CancellationToken::new();
+        self.cancelation_token = Some(cancellation_token.clone());
+        self.is_listening = true;
+
+        let mut user_listener = self.to_owned();
+        let window = window.clone();
+
+        let mut timeout_counter: u64 = 0;
+        let mut last_mouse_coords = UserListener::read_user_mouse_coords();
+
+        tokio::spawn(async move {
+            tokio::select! {
+                _ = async {
+                    info!("UserListener::listening for user inactivity has been started");
+                    loop {
+                        let current_mouse_coords = UserListener::read_user_mouse_coords();
+
+                        if current_mouse_coords != last_mouse_coords {
+                            last_mouse_coords = current_mouse_coords;
+                            timeout_counter = 0;
+                        } else {
+                            timeout_counter += 1000;
+                        }
+
+
+                        if timeout_counter >= idle_timeout && !user_listener.is_mining_initialized {
+                            UserListener::on_user_idle(&window);
+                            user_listener.is_mining_initialized = true;
+                        }
+
+                        if timeout_counter < idle_timeout && user_listener.is_mining_initialized {
+                            UserListener::on_user_active(&window);
+                            user_listener.is_mining_initialized = false;
+                        }
+
+                        sleep(Duration::from_millis(idle_timeout)).await;
+                    }
+                } => {},
+                _ = cancellation_token.cancelled() => {
+                    info!("UserListener::listening for user inactivity has been cancelled");
+                    UserListener::on_user_active(&window);
+                    user_listener.is_mining_initialized = false;
+                }
+            }
+        });
+    }
+
+    pub fn stop_listening_to_mouse_poisition_change(&mut self) {
+        match &self.cancelation_token {
+            Some(token) => {
+                token.cancel();
+                self.is_listening = false;
+            }
+            None => {
+                info!(
+                    "UserListener::triggered cancelation of listening for user inactivity but no cancelation token was found"
+                );
+            }
+        }
+    }
+
+    pub fn on_user_idle(window: &tauri::Window) {
+        window
+            .emit(
+                "message",
+                UserActivityEventPayload {
+                    event_type: "user_idle".to_string(),
+                },
+            )
+            .unwrap();
+    }
+
+    pub fn on_user_active(window: &tauri::Window) {
+        window
+            .emit(
+                "message",
+                UserActivityEventPayload {
+                    event_type: "user_active".to_string(),
+                },
+            )
+            .unwrap();
+    }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import { useGetStatus } from './hooks/useGetStatus.ts';
 import { listen } from '@tauri-apps/api/event';
 import { TauriEvent } from './types.ts';
 import useAppStateStore from './store/appStateStore.ts';
+import { useMining } from './hooks/useMining.ts';
 
 import { preload } from './visuals.js';
 
@@ -26,6 +27,7 @@ function App() {
     const startupInitiated = useRef(false);
     const setSetupDetails = useAppStateStore((s) => s.setSetupDetails);
     const settingUpFinished = useAppStateStore((s) => s.settingUpFinished);
+    const { startMining, stopMining } = useMining();
     const visualMode = useUIStore((s) => s.visualMode);
 
     useEffect(() => {
@@ -44,6 +46,16 @@ function App() {
                         if (payload.progress >= 1.0) {
                             settingUpFinished();
                         }
+                        break;
+                    case 'user_idle':
+                        startMining().then(() => {
+                            console.log('Mining started');
+                        });
+                        break;
+                    case 'user_active':
+                        stopMining().then(() => {
+                            console.log('Mining stopped');
+                        });
                         break;
                     default:
                         console.debug('Unknown tauri event: ', {
