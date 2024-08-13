d492a6c1801f30c89ed5ca53a3e8733c78bbe13f
Misieq01 <38589417+Misieq01@users.noreply.github.com>
2024-08-13 17:50:55 +0200

    feat: show cpu gpu temperatures (#47)
    Description
    ---
    Implement monitoring and display for cpu and gpu temperatures
    Motivation and Context
    
    ---
    As mining is heavy operation for hardware we want to see at least their
    temperatures to know if it safe to continue mining as high temperatures
    may cause damages
    
    How Has This Been Tested?
    ---
    Manually
    What process can a PR reviewer use to test or verify this change?
    
    ---
    Open settings tab and you should see Hardware temperatures category
    <!-- Checklist -->
    <!-- 1. Is the title of your PR in the form that would make nice release
    notes? The title, excluding the conventional commit
    tag, will be included exactly as is in the CHANGELOG, so please think
    about it carefully. -->
    
    
    Breaking Changes
    ---
    
    - [x] None
    - [ ] Requires data directory on base node to be deleted
    - [ ] Requires hard fork
    - [ ] Other - Please specify
    
    <!-- Does this include a breaking change? If so, include this line as a
    footer -->
    <!-- BREAKING CHANGE: Description what the user should do, e.g. delete a
    database, resync the chain -->

--- src/types/app-status.ts
+++ src/types/app-status.ts
@@ -2,6 +2,7 @@ import { modeType } from '../store/types';
 
 export interface AppStatus {
     cpu?: CpuMinerStatus;
+    gpu?: GpuMinerStatus;
     base_node?: BaseNodeStatus;
     wallet_balance?: WalletBalance;
     applications_versions?: ApplicationsVersions;
@@ -10,11 +11,29 @@ export interface AppStatus {
     auto_mining: boolean;
 }
 
+export interface GpuMinerStatus {
+    hardware_statuses: GpuMinerHardwareStatus[];
+}
+
+export interface GpuMinerHardwareStatus {
+    uuid: string;
+    temperature: number;
+    max_temperature: number;
+    name: string;
+    load: number;
+}
+export interface CpuCoreTemperature {
+    label: string;
+    temperature: number;
+    max_temperature: number;
+}
+
 export interface CpuMinerStatus {
     is_mining: boolean;
     hash_rate: number;
     cpu_usage: number;
     cpu_brand: string;
+    cpu_temperatures: CpuCoreTemperature[];
     estimated_earnings: number;
     connection: CpuMinerConnectionStatus;
 }
