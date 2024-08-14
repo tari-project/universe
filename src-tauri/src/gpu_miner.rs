8ffdf392df350f8be1a62c2661b915dd7e630fd6
Misieq01 <bartosz2000walczak@gmail.com>
2024-08-14 12:02:08 +0200

    Handle error on initializing nvml

--- src-tauri/src/gpu_miner.rs
+++ src-tauri/src/gpu_miner.rs
@@ -9,17 +9,32 @@ use crate::{GpuMinerHardwareStatus, GpuMinerStatus};
 const LOG_TARGET: &str = "tari::universe::cpu_miner";
 
 pub(crate) struct GpuMiner {
-    nvml: Nvml,
+    nvml: Option<Nvml>,
     status: GpuMinerStatus,
 }
 
 impl GpuMiner {
     pub fn new() -> Self {
         Self {
-            nvml: Nvml::init().unwrap(),
+            nvml: GpuMiner::try_to_initialize_nvml(),
             status: GpuMinerStatus::from(GpuMinerStatus {
                 hardware_statuses: Vec::new(),
             }),
+        }
+    }
+
+    fn try_to_initialize_nvml() -> Option<Nvml> {
+        let nvml = Nvml::init();
+
+         match nvml {
+            Ok(nvml) => {
+                info!(target: LOG_TARGET, "NVML initialized");
+                Some(nvml)
+            },
+            Err(e) => {
+                info!(target: LOG_TARGET, "Failed to initialize NVML: {}", e);
+                None
+            }
         }
     }
 
@@ -34,11 +49,21 @@ impl GpuMiner {
     }
 
     pub fn status(&mut self) -> GpuMinerStatus {
+
+        let nvml = match &self.nvml {
+            Some(nvml) => nvml,
+            None => {
+                self.nvml = GpuMiner::try_to_initialize_nvml();
+                self.status.hardware_statuses = Vec::new();
+                return self.status.clone();
+            }
+        };
+
         if self.status.hardware_statuses.is_empty() {
-            let devices_count = self.nvml.device_count().unwrap();
+            let devices_count = nvml.device_count().unwrap();
 
             for i in 0..devices_count {
-                let device = self.nvml.device_by_index(i).unwrap();
+                let device = nvml.device_by_index(i).unwrap();
 
                 let uuid = device.uuid().unwrap();
                 let name = device.name().unwrap();
@@ -60,7 +85,7 @@ impl GpuMiner {
             .hardware_statuses
             .iter()
             .map(|status| {
-                let device = self.nvml.device_by_uuid(status.uuid.clone()).unwrap();
+                let device = nvml.device_by_uuid(status.uuid.clone()).unwrap();
 
                 let temperature = device.temperature(TemperatureSensor::Gpu).unwrap();
                 let load = device.utilization_rates().unwrap().gpu;
