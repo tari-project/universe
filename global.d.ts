6e102c81365b41e36f773eaaecd14fc4b5a742b5
Shannon Tenner <shanimal@tari.com>
2024-08-14 11:01:06 +0200

    wip: types

--- global.d.ts
+++ global.d.ts
@@ -1,11 +1,27 @@
 // global.d.ts
 export {};
 
+interface Status {
+    NOT_STARTED: 'not-started';
+    STARTED: 'started';
+    FREE: 'free';
+    RESULT: 'result';
+    RESULT_ANIMATION: 'result_animation';
+    RESTART_ANIMATION: 'restart_animation';
+    RESTART: 'restart';
+}
+
+interface Result {
+    NONE: 'none';
+    PAUSE: 'pause';
+    COMPLETED: 'completed';
+    FAILED: 'failed';
+}
 declare global {
     interface Window {
         properties;
-        STATUS;
-        RESULT;
+        STATUS: Status;
+        RESULT: Result;
         glApp;
     }
 }
