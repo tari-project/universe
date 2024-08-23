f462037bb83eddc075c26e2886c088a203946bf6
Juan De Luca <juan.f.d.luca@gmail.com>
2024-08-23 09:55:26 +0100

    Adds prettier fix

--- src/utils/truncateString.ts
+++ src/utils/truncateString.ts
@@ -1,6 +1,6 @@
 const truncateString = (str: string, num: number): string => {
-  if (str.length <= num) return str;
-  return str.slice(0, num) + "...";
+    if (str.length <= num) return str;
+    return str.slice(0, num) + '...';
 };
 
 export default truncateString;
