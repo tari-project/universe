5c70ea377dd65670becf2f907588f9ea8a3ba9f1
Marcin Papież <mmpapiez@gmail.com>
2024-08-23 14:21:12 +0200

    adjust to main

--- src/hooks/mining/useMiningControls.ts
+++ src/hooks/mining/useMiningControls.ts
@@ -5,7 +5,6 @@ import { useVisualisation } from './useVisualisation.ts';
 import { useAppStatusStore } from '@app/store/useAppStatusStore.ts';
 import useAppStateStore from '@app/store/appStateStore.ts';
 import { useCPUStatusStore } from '@app/store/useCPUStatusStore.ts';
-import { useUIStore } from '@app/store/useUIStore.ts';
 
 export enum MiningButtonStateText {
     STARTING = 'starting-mining',
