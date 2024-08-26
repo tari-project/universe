70cc9de0e89e9aff66cd922075793cba037feb3a
Shannon Tenner <shanimal@tari.com>
2024-08-26 15:07:18 +0200

    add timeout for failure state

--- src/hooks/mining/useVisualisation.ts
+++ src/hooks/mining/useVisualisation.ts
@@ -1,19 +1,28 @@
-import { useCallback } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { setAnimationState } from '../../visuals';
 import { GlAppState } from '@app/glApp';
 import { useMiningStore } from '@app/store/useMiningStore.ts';
 
 export function useVisualisation() {
+    const [failure, setFailure] = useState(false);
     const setPostBlockAnimation = useMiningStore((s) => s.setPostBlockAnimation);
     const setTimerPaused = useMiningStore((s) => s.setTimerPaused);
-    return useCallback(
-        (state: GlAppState) => {
-            setAnimationState(state);
-            if (state === 'fail') {
-                setTimerPaused(false);
+
+    useEffect(() => {
+        if (failure) {
+            const failTimeout = setTimeout(() => {
                 setPostBlockAnimation(true);
-            }
-        },
-        [setPostBlockAnimation, setTimerPaused]
-    );
+                setTimerPaused(false);
+                setFailure(false);
+            }, 1500);
+            return () => clearTimeout(failTimeout);
+        }
+    }, [failure, setPostBlockAnimation, setTimerPaused]);
+
+    return useCallback((state: GlAppState) => {
+        setAnimationState(state);
+        if (state === 'fail') {
+            setFailure(true);
+        }
+    }, []);
 }
