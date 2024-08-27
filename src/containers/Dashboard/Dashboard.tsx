38d7880d484d3fdbe23322040f9d9fd7ec62c61c
Shannon Tenner <shanimal@tari.com>
2024-08-27 14:18:25 +0200

    add app background for setupview

--- src/containers/Dashboard/Dashboard.tsx
+++ src/containers/Dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { DashboardContainer, SetupGradient } from './styles';
+import { DashboardContainer } from './styles';
 import MiningView from './MiningView/MiningView';
 import TribesView from './TribesView/TribesView';
 import { viewType } from '../../store/types';
@@ -8,12 +8,7 @@ function Dashboard({ status }: { status: viewType }) {
     const viewMarkup =
         status == 'setup' ? <SetupViewContainer /> : status == 'tribes' ? <TribesView /> : <MiningView />;
 
-    return (
-        <DashboardContainer>
-            {status == 'setup' ? <SetupGradient /> : null}
-            {viewMarkup}
-        </DashboardContainer>
-    );
+    return <DashboardContainer>{viewMarkup}</DashboardContainer>;
 }
 
 export default Dashboard;
