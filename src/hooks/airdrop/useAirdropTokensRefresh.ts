8bfec56ddcc78a85e5413239036791c7dfa6a12d
Juan De Luca <juan.f.d.luca@gmail.com>
2024-08-28 10:09:14 +0100

    Adds refresh token interval

--- src/hooks/airdrop/useAirdropTokensRefresh.ts
+++ src/hooks/airdrop/useAirdropTokensRefresh.ts
@@ -1,31 +1,35 @@
 import { useAirdropStore } from "@app/store/useAirdropStore";
 // import { useInterval } from "../useInterval";
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { invoke } from '@tauri-apps/api/tauri';
 
 export function useAirdropTokensRefresh() {
     const { airdropTokens, setAirdropTokens } = useAirdropStore();
 
     // Handle refreshing the access token
-    // const handleRefresh = useCallback(() => {
-    //     if (airdropTokens && airdropTokens.expiresAt && Date.now() > airdropTokens.expiresAt) {
-    //         fetch('https://airdrop.tari.com/api/auth/local/refresh', {
-    //             method: 'POST',
-    //             headers: {
-    //                 'Content-Type': 'application/json',
-    //             },
-    //             body: JSON.stringify({
-    //                 refreshToken: airdropTokens.refreshToken
-    //             })
-    //         })
-    //             .then(response => response.json())
-    //             .then((data) => {
-    //                 setAirdropTokens(data);
-    //             });
-    //     }
-    // }, [airdropTokens, setAirdropTokens]);
+    const handleRefresh = useCallback(() => {
+        const expired = airdropTokens?.expiresAt && new Date(airdropTokens?.expiresAt * 1000) > new Date();
+        if (airdropTokens && expired) {
+            fetch('https://airdrop.tari.com/api/auth/local/refresh', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    refreshToken: airdropTokens.refreshToken
+                })
+            })
+                .then(response => response.json())
+                .then((data) => {
+                    setAirdropTokens(data);
+                });
+        }
+    }, [airdropTokens, setAirdropTokens]);
 
-    // useInterval(handleRefresh, 1000 * 60 * 60);
+    useEffect(() => {
+        const interval = setInterval(handleRefresh, 1000 * 60 * 60);
+        return () => clearInterval(interval);
+    }, [handleRefresh]);
 
     // Handle setting the access token
     useEffect(() => {
