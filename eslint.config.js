18b46156554a3ad3cf7233982eb9f153229a668b
Juan <47256233+PanchoBubble@users.noreply.github.com>
2024-08-28 11:42:42 +0100

    feat: adds stylisitc (#202)
    Description
    ---
    
    Adds linting configuration used by airbnb
    
    Motivation and Context
    ---
    
    There was inconsistent file formating
    
    How Has This Been Tested?
    ---
    
    - Check your edditor formatter
    - Run `npm run lint:fix` before running a PR
    
    What process can a PR reviewer use to test or verify this change?
    ---
    
    I haven't run the linter on this PR in case there is work in progress
    from @shanimal08 and or @NovaT82 in order to avoid introducing conflicts
    
    <!-- Checklist -->
    <!-- 1. Is the title of your PR in the form that would make nice release
    notes? The title, excluding the conventional commit
    tag, will be included exactly as is in the CHANGELOG, so please think
    about it carefully. -->
    
    
    Breaking Changes
    ---
    
    - [x] None
    - [ ] Requires data directory on base node to be deleted
    - [ ] Requires hard fork
    - [ ] Other - Please specify
    
    <!-- Does this include a breaking change? If so, include this line as a
    footer -->
    <!-- BREAKING CHANGE: Description what the user should do, e.g. delete a
    database, resync the chain -->

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import react from 'eslint-plugin-react';
 import hooksPlugin from 'eslint-plugin-react-hooks';
+import stylistic from '@stylistic/eslint-plugin'
 
 import prettierConfig from 'eslint-config-prettier';
 import globals from 'globals';
@@ -26,17 +27,18 @@ export default [
         },
         files: ['src/**/*.{js,ts,jsx,tsx}'],
         ignores: ['./**/*.config.{js,ts}'],
-        plugins: { react, 'react-hooks': hooksPlugin },
+        plugins: { react, 'react-hooks': hooksPlugin, '@stylistic': stylistic },
         rules: {
             ...react.configs.recommended.rules,
             ...hooksPlugin.configs.recommended.rules,
+            ...prettierConfig.rules,
             'react/jsx-uses-react': 'off',
             'react/react-in-jsx-scope': 'off',
             'no-console': ['warn', { allow: ['info', 'warn', 'debug', 'error'] }],
             'react/prop-types': 'off',
             '@typescript-eslint/no-explicit-any': 'warn',
             '@typescript-eslint/no-unused-vars': 'warn',
+            '@stylistic/indent': ['error', 4],
         },
     },
-    prettierConfig,
 ];
