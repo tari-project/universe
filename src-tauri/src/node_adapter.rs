598b528b28316ed716f9e1b01adbbcc588c687d6
Balazs Sevecsek <mountain@tari.com>
2024-08-29 14:14:44 +0200

    fix: issues around telemetry_manager

--- src-tauri/src/node_adapter.rs
+++ src-tauri/src/node_adapter.rs
@@ -97,12 +97,7 @@ impl ProcessAdapter for MinotariNodeAdapter {
                         .resolve_path(Binaries::MinotariNode)
                         .await?;
                     crate::download_utils::set_permissions(&file_path).await?;
-                    let mut child = tokio::process::Command::new(file_path)
-                        .args(args)
-                        .stdout(std::process::Stdio::null())
-                        .stderr(std::process::Stdio::null())
-                        .kill_on_drop(true)
-                        .spawn()?;
+                    let mut child = process_utils::launch_child_process(&file_path, &args)?;
 
                     if let Some(id) = child.id() {
                         fs::write(data_dir.join("node_pid"), id.to_string())?;
--- src-tauri/src/wallet_adapter.rs
+++ src-tauri/src/wallet_adapter.rs
@@ -108,12 +108,7 @@ impl ProcessAdapter for WalletAdapter {
                         .resolve_path(Binaries::Wallet)
                         .await?;
                     crate::download_utils::set_permissions(&file_path).await?;
-                    let mut child = tokio::process::Command::new(file_path)
-                        .args(args)
-                        .stdout(std::process::Stdio::null())
-                        .stderr(std::process::Stdio::null())
-                        .kill_on_drop(true)
-                        .spawn()?;
+                    let mut child = process_utils::launch_child_process(&file_path, &args)?;
 
                     if let Some(id) = child.id() {
                         std::fs::write(data_dir.join("wallet_pid"), id.to_string())?;
