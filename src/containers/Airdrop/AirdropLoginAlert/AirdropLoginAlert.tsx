147f43f2ac6e183aadbf6f682894af1fbac47f54
Aaron Pepper <pepdog@gmail.com>
2024-09-03 14:23:10 +0700

    init login alert component

---
+++ src/containers/Airdrop/AirdropLoginAlert/AirdropLoginAlert.tsx
@@ -0,0 +1,5 @@
+import { Wrapper } from './styles';
+
+export default function AirdropLoginAlert() {
+    return <Wrapper>Login alert</Wrapper>;
+}
---
+++ src/containers/Airdrop/AirdropLoginAlert/styles.ts
@@ -0,0 +1,5 @@
+import { styled } from '@mui/material/styles';
+
+export const Wrapper = styled('div')`
+    padding: 0 0 0px 0;
+`;
