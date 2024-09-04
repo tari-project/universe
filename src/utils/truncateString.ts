55f6a8f9ac0779a51166f8804b06e93f5432c2f4
karczuRF <oskar.karcz@rumblefish.pl>
2024-09-04 13:15:32 +0200

    reverse truncateString fct changes

--- src/utils/truncateString.ts
+++ src/utils/truncateString.ts
@@ -1,14 +1,6 @@
-const splitStringInHalf = (str: string): string => {
-    const words = str.split(' ');
-    const halfIndex = Math.ceil(words.length / 2);
-    const firstHalf = words.slice(0, halfIndex).join(' ');
-    const secondHalf = words.slice(halfIndex).join(' ');
-    return firstHalf + '\n' + secondHalf;
-};
-
 const truncateString = (str: string, num: number): string => {
     if (str.length <= num) return str;
-    return splitStringInHalf(str);
+    return str.slice(0, num) + '...';
 };
 
 export default truncateString;
