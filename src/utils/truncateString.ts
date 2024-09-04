e1ef315771b64d7025c043707e49d5ff4d1813d7
karczuRF <oskar.karcz@rumblefish.pl>
2024-09-04 11:21:04 +0200

    style: split fct

--- src/utils/truncateString.ts
+++ src/utils/truncateString.ts
@@ -1,14 +1,14 @@
+const splitStringInHalf = (str: string): string => {
+    const words = str.split(' ');
+    const halfIndex = Math.ceil(words.length / 2);
+    const firstHalf = words.slice(0, halfIndex).join(' ');
+    const secondHalf = words.slice(halfIndex).join(' ');
+    return firstHalf + '\n' + secondHalf;
+};
+
 const truncateString = (str: string, num: number): string => {
     if (str.length <= num) return str;
     return splitStringInHalf(str);
 };
 
 export default truncateString;
-
-function splitStringInHalf(str: string) {
-    const words = str.split(' ');
-    const halfIndex = Math.ceil(words.length / 2);
-    const firstHalf = words.slice(0, halfIndex).join(' ');
-    const secondHalf = words.slice(halfIndex).join(' ');
-    return firstHalf + '\n' + secondHalf;
-}
