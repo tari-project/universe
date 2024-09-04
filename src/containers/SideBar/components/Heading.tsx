fe49ef830ff9cfbd523fe64b3c433a92bb63de0c
shan <47271333+shanimal08@users.noreply.github.com>
2024-09-04 10:49:25 +0200

    feat(ui): add chip component and Testnet tag (#281)
    Description
    ---
    
    - added basic chip component
    - added chip with 'Testnet' to sidebar title
    - added 'Testnet' to config window and app name
    
    Motivation and Context
    ---
    
    - resolves #92 
    
    How Has This Been Tested?
    ---
    
    - locally: 
    
    <img width="862" alt="image"
    src="https://github.com/user-attachments/assets/06d26d8b-9c28-4308-b352-d1625b8a0b95">
    
    ---------
    
    Co-authored-by: stringhandler <stringhandler@gmail.com>

--- src/containers/SideBar/components/Heading.tsx
+++ src/containers/SideBar/components/Heading.tsx
@@ -1,11 +1,17 @@
 import SettingsDialog from './Settings/Settings.tsx';
 import { Typography } from '@app/components/elements/Typography.tsx';
 import { Stack } from '@app/components/elements/Stack.tsx';
+import { Chip } from '@app/components/elements/Chip.tsx';
+import { useTranslation } from 'react-i18next';
 
 function Heading() {
+    const { t } = useTranslation('common', { useSuspense: false });
     return (
         <Stack direction="row" justifyContent="space-between">
-            <Typography variant="h3">Tari Universe</Typography>
+            <Stack direction="row" alignItems="center" gap={10}>
+                <Typography variant="h3">{t('tari-universe')}</Typography>
+                <Chip>{t('testnet')}</Chip>
+            </Stack>
             <Stack>
                 <SettingsDialog />
             </Stack>
