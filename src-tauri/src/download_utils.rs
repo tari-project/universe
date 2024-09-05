1b27916c4e667ed3262567e1d3d77e433412d29e
stringhandler <stringhandler@protonmail.com>
2024-09-05 18:52:19 +0200

    fix: fix compiles

--- src-tauri/src/download_utils.rs
+++ src-tauri/src/download_utils.rs
@@ -152,15 +152,18 @@ pub async fn extract_zip(archive: &Path, out_dir: &Path) -> Result<(), anyhow::E
     Ok(())
 }
 
+#[cfg(unix)]
 pub async fn set_permissions(file_path: &Path) -> Result<(), anyhow::Error> {
-    #[cfg(unix)]
-    {
-        use std::os::unix::fs::PermissionsExt;
-        let mut perms = fs::metadata(file_path).await?.permissions();
-        let current_mode = perms.mode();
-        perms.set_mode(current_mode | 0o111);
-        fs::set_permissions(file_path, perms).await?;
-    }
+    use std::os::unix::fs::PermissionsExt;
+    let mut perms = fs::metadata(file_path).await?.permissions();
+    let current_mode = perms.mode();
+    perms.set_mode(current_mode | 0o111);
+    fs::set_permissions(file_path, perms).await?;
+    Ok(())
+}
+
+#[cfg(windows)]
+pub async fn set_permissions(_file_path: &Path) -> Result<(), anyhow::Error> {
     Ok(())
 }
 
