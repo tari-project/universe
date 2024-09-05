5bb586d91e8c144d5f66dda041144dd25d2d633c
karczuRF <88723742+karczuRF@users.noreply.github.com>
2024-09-05 19:06:15 +0200

    fix(ui): wallet balance text (#308)
    Description
    Fix color and position of Wallet Balance text
    
    Motivation and Context
    Resolves #304 
    
    Before:
    
    ![image](https://github.com/user-attachments/assets/0bdaefa6-98fb-44a2-8143-2ca66c4206f2)
    
    After:
    
    ![image](https://github.com/user-attachments/assets/b0e76cdb-7401-415e-9033-bc110c6b2727)
    
    
    How Has This Been Tested?
    Manually
    
    
    <!-- Checklist -->
    <!-- 1. Is the title of your PR in the form that would make nice release
    notes? The title, excluding the conventional commit
    tag, will be included exactly as is in the CHANGELOG, so please think
    about it carefully. -->
    
    
    Breaking Changes
    ---
    
    - [x] None
    - [ ] Requires data directory on base node to be deleted
    - [ ] Requires hard fork
    - [ ] Other - Please specify
    
    <!-- Does this include a breaking change? If so, include this line as a
    footer -->
    <!-- BREAKING CHANGE: Description what the user should do, e.g. delete a
    database, resync the chain -->

--- src/containers/SideBar/components/Wallet.styles.ts
+++ src/containers/SideBar/components/Wallet.styles.ts
@@ -6,3 +6,13 @@ export const WalletBalance = styled.div`
     color: #fff;
     width: 100%;
 `;
+
+export const WalletBalanceContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    padding: 5px;
+    width: 100%;
+    color: ${({ theme }) => theme.palette.text.secondary};
+`;
