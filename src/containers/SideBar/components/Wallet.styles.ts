586251aa6e8073bac3a8f91c2cf02223ad520159
karczuRF <oskar.karcz@rumblefish.pl>
2024-09-05 13:26:14 +0200

    fix: wallet balance text

--- src/containers/SideBar/components/Wallet.styles.ts
+++ src/containers/SideBar/components/Wallet.styles.ts
@@ -6,3 +6,13 @@ export const WalletBalance = styled.div`
     color: #fff;
     width: 100%;
 `;
+
+export const WalletBalanceContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    padding: 10px;
+    width: 100%;
+    color: ${({ theme }) => theme.palette.text.secondary};
+`;
