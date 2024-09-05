856ec1f5ba3273f2ad9d3c812ae30d62bc367973
Marcin Papież <mmpapiez@gmail.com>
2024-09-05 19:15:31 +0200

    chore(ui): add translations & improve ui for reset settings (#314)
    [Screencast from 05.09.2024
    19:05:00.webm](https://github.com/user-attachments/assets/605b37c2-f0be-4e99-a244-35969da78c26)

--- src/containers/SideBar/components/Settings/ResetSettingsButton.tsx
+++ src/containers/SideBar/components/Settings/ResetSettingsButton.tsx
@@ -7,11 +7,13 @@ import { Typography } from '@app/components/elements/Typography.tsx';
 import { CircularProgress } from '@app/components/elements/CircularProgress.tsx';
 import { Stack } from '@app/components/elements/Stack.tsx';
 import { Dialog, DialogContent } from '@app/components/elements/dialog/Dialog.tsx';
+import { useTranslation } from 'react-i18next';
 
 export const ResetSettingsButton = () => {
     const [open, setOpen] = useState(false);
     const [loading, setLoading] = useState(false);
     const setError = useAppStateStore((state) => state.setError);
+    const { t } = useTranslation('settings', { useSuspense: false });
 
     const resetSettings = () => {
         setLoading(true);
@@ -33,19 +35,24 @@ export const ResetSettingsButton = () => {
     return (
         <Dialog open={open} onOpenChange={setOpen}>
             <Button onClick={() => setOpen(true)} styleVariant="outline" color="error">
-                Reset Settings
+                {t('reset-settings')}
             </Button>
             <DialogContent>
                 <Stack direction="column" alignItems="center" justifyContent="space-between">
-                    <Typography variant="h2">Reset Settings</Typography>
-                    <Typography variant="p">Are you sure you want to reset all settings permanently?</Typography>
+                    <Typography variant="h2">{t('reset-settings')}</Typography>
+                    <Typography variant="p">{t('reset-permanently')}</Typography>
                     <Stack direction="row">
                         <Button disabled={loading} onClick={handleClose} color="warning">
-                            Cancel
+                            {t('cancel')}
                         </Button>
-                        <Button disabled={loading} onClick={resetSettings}>
-                            {loading ? <CircularProgress /> : 'Yes'}
-                        </Button>
+
+                        {loading ? (
+                            <CircularProgress />
+                        ) : (
+                            <Button disabled={loading} onClick={resetSettings}>
+                                {t('yes')}
+                            </Button>
+                        )}
                     </Stack>
                 </Stack>
             </DialogContent>
