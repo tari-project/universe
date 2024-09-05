2d39a68913f7d84eecea1425447ffc013b0a4011
Juan De Luca <juan.f.d.luca@gmail.com>
2024-09-05 16:18:20 +0100

    fix: fixes missing type

--- src/types/app-status.ts
+++ src/types/app-status.ts
@@ -17,6 +17,7 @@ export interface AppStatus {
     p2pool_stats?: P2poolStatsResult;
     cpu_mining_enabled: boolean;
     gpu_mining_enabled: boolean;
+    telemetry_mode: boolean;
 }
 
 export interface P2poolStatsResult {
