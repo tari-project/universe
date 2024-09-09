c3e9a8640deb46cb0d6448cb2816e1e51cbbc4d1
Shannon Tenner <shanimal@tari.com>
2024-09-09 15:27:57 +0200

    remove zustand store persist (except for airdrop token)

--- src/store/useCPUStatusStore.ts
+++ src/store/useCPUStatusStore.ts
@@ -1,6 +1,5 @@
 import { create } from './create';
 import { CpuMinerStatus } from '../types/app-status.ts';
-import { persist } from 'zustand/middleware';
 
 interface Actions {
     setCPUStatus: (cpuStatus?: CpuMinerStatus) => void;
@@ -13,12 +12,7 @@ const initialState: CpuMinerStatus = {
     estimated_earnings: 0,
     connection: { is_connected: false },
 };
-export const useCPUStatusStore = create<CPUStatusStoreState>()(
-    persist(
-        (set) => ({
-            ...initialState,
-            setCPUStatus: (cpuStatus) => set({ ...cpuStatus }),
-        }),
-        { name: 'cpu', version: 0.1 }
-    )
-);
+export const useCPUStatusStore = create<CPUStatusStoreState>()((set) => ({
+    ...initialState,
+    setCPUStatus: (cpuStatus) => set({ ...cpuStatus }),
+}));
--- src/store/walletStore.ts
+++ src/store/walletStore.ts
@@ -1,5 +1,5 @@
 import { create } from './create';
-import { persist } from 'zustand/middleware';
+
 import { WalletBalance } from '@app/types/app-status.ts';
 
 interface State extends WalletBalance {
@@ -23,35 +23,23 @@ const initialState: State = {
     pending_outgoing_balance: 0,
 };
 
-export const useWalletStore = create<WalletStore>()(
-    persist(
-        (set) => ({
-            ...initialState,
-            setBalanceData: (wallet_balance) =>
-                set((state) => {
-                    const {
-                        available_balance = 0,
-                        timelocked_balance = 0,
-                        pending_incoming_balance = 0,
-                    } = wallet_balance || {};
+export const useWalletStore = create<WalletStore>()((set) => ({
+    ...initialState,
+    setBalanceData: (wallet_balance) =>
+        set((state) => {
+            const {
+                available_balance = 0,
+                timelocked_balance = 0,
+                pending_incoming_balance = 0,
+            } = wallet_balance || {};
 
-                    const newBalance = available_balance + timelocked_balance + pending_incoming_balance; //TM
-                    const hasChanged = state.balance != newBalance;
-                    const prevValue = hasChanged ? state.balance : state.previousBalance;
-                    return {
-                        ...wallet_balance,
-                        balance: newBalance,
-                        previousBalance: prevValue,
-                    };
-                }),
+            const newBalance = available_balance + timelocked_balance + pending_incoming_balance; //TM
+            const hasChanged = state.balance != newBalance;
+            const prevValue = hasChanged ? state.balance : state.previousBalance;
+            return {
+                ...wallet_balance,
+                balance: newBalance,
+                previousBalance: prevValue,
+            };
         }),
-        {
-            name: 'wallet_balance',
-            partialize: (s) => ({
-                balance: s.balance,
-                previousBalance: s.previousBalance,
-            }),
-            version: 0.1,
-        }
-    )
-);
+}));
