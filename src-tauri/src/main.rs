220fcf489349de8da63e9d14aa9a8263ac3252d2
karczuRF <88723742+karczuRF@users.noreply.github.com>
2024-09-10 15:12:03 +0200

    feat(style): format systray value (#320)
    Description
    Format est earnings from the systray info
    
    Motivation and Context
    Resolves #300 
    Est earning now are the same format both in TU and systray info
    
    How Has This Been Tested?
    
    ![image](https://github.com/user-attachments/assets/4b312dd2-f0c9-43be-a988-611e6f0a25c7)
    
    What process can a PR reviewer use to test or verify this change?
    ---
    
    <!-- Checklist -->
    <!-- 1. Is the title of your PR in the form that would make nice release
    notes? The title, excluding the conventional commit
    tag, will be included exactly as is in the CHANGELOG, so please think
    about it carefully. -->
    
    
    Breaking Changes
    ---
    
    - [x] None
    - [ ] Requires data directory on base node to be deleted
    - [ ] Requires hard fork
    - [ ] Other - Please specify
    
    <!-- Does this include a breaking change? If so, include this line as a
    footer -->
    <!-- BREAKING CHANGE: Description what the user should do, e.g. delete a
    database, resync the chain -->

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,6 +7,7 @@ mod binary_resolver;
 mod consts;
 mod cpu_miner;
 mod download_utils;
+mod format_utils;
 mod github;
 mod gpu_miner;
 mod hardware_monitor;
@@ -768,7 +769,7 @@ async fn status(
         }
     };
 
-    let gpu_status = match gpu_miner.status(sha_hash_rate, block_reward).await {
+    let gpu = match gpu_miner.status(sha_hash_rate, block_reward).await {
         Ok(gpu) => gpu,
         Err(e) => {
             warn!(target: LOG_TARGET, "Error getting gpu miner status: {:?}", e);
@@ -805,7 +806,7 @@ async fn status(
 
     let new_systemtray_data: SystrayData = SystemtrayManager::current().create_systemtray_data(
         cpu.hash_rate,
-        0.0,
+        gpu.hash_rate as f64,
         hardware_status.clone(),
         cpu.estimated_earnings as f64,
     );
@@ -814,7 +815,7 @@ async fn status(
 
     Ok(Some(AppStatus {
         cpu,
-        gpu: gpu_status,
+        gpu,
         hardware_status: hardware_status.clone(),
         base_node: BaseNodeStatus {
             block_height,
