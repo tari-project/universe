0b834eede788d4ddb3d8d6809af35af97d2ae155
Shannon Tenner <shanimal@tari.com>
2024-09-10 12:19:44 +0200

    wip: tower centering

--- src/theme/styles.ts
+++ src/theme/styles.ts
@@ -6,7 +6,7 @@ export const sidebarWidth = '348px'; // if this is updated please update the val
 
 export const DashboardContainer = styled(motion.div)`
     display: grid;
-    grid-template-columns: ${sidebarWidth} 2fr;
+    grid-template-columns: ${sidebarWidth} auto;
     position: relative;
     gap: 20px;
     padding: 20px;
@@ -24,4 +24,43 @@ export const BackgroundImage = styled(motion.div)`
     left: 0;
     height: 100%;
     width: 100%;
+`;
+
+export const Row = styled(motion.div)`
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    box-sizing: border-box;
+    position: relative;
+    width: 100%;
+    height: 100%;
+    &:after {
+        content: '---';
+        position: absolute;
+        color: rgba(115, 28, 115, 0.89);
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+`;
+export const Col = styled(motion.div)`
+    display: grid;
+    width: 100%;
+    height: 100%;
+    position: relative;
+    &:after {
+        content: '|';
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        color: rgba(16, 166, 199, 0.75);
+    }
+`;
+export const Guide = styled(motion.div)`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    bottom: 0;
+    display: grid;
+    grid-auto-flow: row;
 `;
