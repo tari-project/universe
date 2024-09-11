e00ae0b597843675f1964429cb2740c4e74a2975
shan <47271333+shanimal08@users.noreply.github.com>
2024-09-11 18:13:02 +0200

    fix: sync animation status with mining state (#440)
    - check that the glApp animation status matches our mining state

--- src/visuals.ts
+++ src/visuals.ts
@@ -6,7 +6,6 @@ export function setAnimationState(state: GlAppState) {
     if (sM?.isPaused && state == 'start') {
         newState = 'resume';
     }
-
     sM?.set(newState);
     if (state == 'pause') {
         // needed to add this because the set() doesn't update their isPaused for some reason
