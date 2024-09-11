778d86ba9b059ef6043cfe231dfd8b0c3ab2bd0d
Shannon Tenner <shanimal@tari.com>
2024-09-11 08:28:26 +0200

    fix: make ruler check more specific

--- src/containers/Dashboard/MiningView/components/Ruler.tsx
+++ src/containers/Dashboard/MiningView/components/Ruler.tsx
@@ -32,12 +32,15 @@ export function Ruler() {
     const bottomMarkSegments = rulerSegments.map((segment, i) => {
         const diff = height && height > 50 ? 10 : 5;
         const renderNumber = heightSegment && heightSegment > diff;
+
         if (renderNumber && heightSegment) {
             heightSegment -= diff;
         }
 
+        const prevSegment = (heightSegment || 0) + diff;
         const groupOpacity = (rulerSegments.length * 1.25 - segment) * 0.075;
-        const numberMark = heightSegment && heightSegment > 1 ? heightSegment?.toString() : '';
+        const numberMark =
+            heightSegment && heightSegment > diff && heightSegment != prevSegment ? heightSegment?.toString() : '';
         return (
             <MarkGroup key={`row-${segment}-${i}`} layout style={{ opacity: groupOpacity }}>
                 <RulerMarkGroup>
