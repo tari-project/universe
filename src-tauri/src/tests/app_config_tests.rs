b9461abb88579e15296ef6daff72e44207e8d357
stringhandler <stringhandler@protonmail.com>
2024-09-11 14:19:29 +0200

    fix tests

--- src-tauri/src/tests/app_config_tests.rs
+++ src-tauri/src/tests/app_config_tests.rs
@@ -20,16 +20,16 @@ mod tests {
 
         config.apply_loaded_config(config_json.to_string());
 
-        assert_eq!(config.mode, MiningMode::Eco);
+        assert_eq!(config.mode(), MiningMode::Eco);
         // it doesn't affect auto_mining value saved in the config
-        assert_eq!(config.auto_mining, true);
-        assert_eq!(config.p2pool_enabled, false);
-        assert_ne!(format!("{:?}", config.last_binaries_update_timestamp), "");
-        assert_eq!(config.allow_telemetry, false);
-        assert_eq!(config.anon_id.len(), 20);
-        assert_eq!(config.monero_address, DEFAULT_MONERO_ADDRESS.to_string());
-        assert_eq!(config.gpu_mining_enabled, true);
-        assert_eq!(config.cpu_mining_enabled, true);
+        assert_eq!(config.auto_mining(), true);
+        assert_eq!(config.p2pool_enabled(), false);
+        assert_ne!(format!("{:?}", config.last_binaries_update_timestamp()), "");
+        assert_eq!(config.allow_telemetry(), false);
+        assert_eq!(config.anon_id().len(), 20);
+        assert_eq!(config.monero_address(), DEFAULT_MONERO_ADDRESS.to_string());
+        assert_eq!(config.gpu_mining_enabled(), true);
+        assert_eq!(config.cpu_mining_enabled(), true);
     }
 
     #[test]
@@ -55,19 +55,19 @@ mod tests {
 
         config.apply_loaded_config(config_json.to_string());
 
-        assert_eq!(config.mode, MiningMode::Ludicrous);
-        assert_eq!(config.auto_mining, false);
+        assert_eq!(config.mode(), MiningMode::Ludicrous);
+        assert_eq!(config.auto_mining(), false);
         // For now always false by default
-        assert_eq!(config.p2pool_enabled, false);
+        assert_eq!(config.p2pool_enabled(), false);
         let expected_timestamp = SystemTime::UNIX_EPOCH + Duration::new(1725545367, 379078628);
-        assert_eq!(config.last_binaries_update_timestamp, expected_timestamp);
-        assert_eq!(config.allow_telemetry, true);
-        assert_eq!(config.anon_id, "5GGl^0NQiChrGMsjYXs5");
+        assert_eq!(config.last_binaries_update_timestamp(), expected_timestamp);
+        assert_eq!(config.allow_telemetry(), true);
+        assert_eq!(config.anon_id(), "5GGl^0NQiChrGMsjYXs5");
         assert_eq!(
-            config.monero_address,
+            config.monero_address(),
             "44AFFq5kSiGBoZ4NMDwYtN18obc8AemS33DBLWs3H7otXft3XjrpDtQGv7SqSsaBYBb98uNbr2VBBEt7f2wfn3RVGQBEP3A"
         );
-        assert_eq!(config.gpu_mining_enabled, true);
-        assert_eq!(config.cpu_mining_enabled, true);
+        assert_eq!(config.gpu_mining_enabled(), true);
+        assert_eq!(config.cpu_mining_enabled(), true);
     }
 }
