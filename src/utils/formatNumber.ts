5ca5c2e377320606c5e037c50461433839257aeb
karczuRF <oskar.karcz@rumblefish.pl>
2024-09-16 13:55:57 +0200

    fix: format amount

--- src/utils/formatNumber.ts
+++ src/utils/formatNumber.ts
@@ -1,10 +1,12 @@
-export function formatNumber(value: number, maxDigits?: number): string {
+export function formatNumber(value: number, maxDigits = 3, isHashrate = true): string {
     //TODO: add props for customisation
-    return Intl.NumberFormat(undefined, {
+    const formatted = Intl.NumberFormat(undefined, {
         notation: 'compact',
-        maximumFractionDigits: maxDigits || 3,
+        maximumFractionDigits: maxDigits,
         style: 'decimal',
     }).format(value);
+    // workaround because there is no "hashrate" unit
+    return isHashrate ? formatted.replace('B', 'G') : formatted;
 }
 export function formatPercent(value = 0) {
     const p = Math.floor(value || 0).toLocaleString();
