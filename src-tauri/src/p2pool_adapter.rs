2d4d57f6958dd01a88d263c6bbb5973e3849c05b
stringhandler <stringhandler@protonmail.com>
2024-09-18 16:07:27 +0200

    chore: fix formatting and lints

--- src-tauri/src/p2pool_adapter.rs
+++ src-tauri/src/p2pool_adapter.rs
@@ -3,7 +3,6 @@ use anyhow::Error;
 use async_trait::async_trait;
 use dirs_next::data_local_dir;
 use log::{info, warn};
-use rand::seq::SliceRandom;
 use std::collections::HashMap;
 use std::fs;
 use std::path::PathBuf;
@@ -12,11 +11,11 @@ use tari_shutdown::Shutdown;
 use tokio::select;
 
 use crate::binary_resolver::{Binaries, BinaryResolver};
+use crate::p2pool;
 use crate::p2pool::models::Stats;
 use crate::p2pool_manager::P2poolConfig;
 use crate::process_adapter::{ProcessAdapter, ProcessInstance, StatusMonitor};
 use crate::process_utils::launch_child_process;
-use crate::{p2pool, process_utils};
 
 const LOG_TARGET: &str = "tari::universe::p2pool_adapter";
 
