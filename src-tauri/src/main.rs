1c9784391f24cca92b4af191b89e7ef33e421c35
Maciej Kozuszek <mackozuszek@gmail.com>
2024-09-20 15:50:20 +0200

    Force gpu detect every app setup

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -310,6 +310,14 @@ async fn setup_inner(
         .read_current_highest_version(Binaries::GpuMiner, progress.clone())
         .await?;
 
+    let _unused = state
+        .gpu_miner
+        .write()
+        .await
+        .detect()
+        .await
+        .inspect_err(|e| error!(target: LOG_TARGET, "Could not detect gpu miner: {:?}", e));
+
     if now
         .duration_since(last_binaries_update_timestamp)
         .unwrap_or(Duration::from_secs(0))
@@ -357,16 +365,6 @@ async fn setup_inner(
         let _unused = BinaryResolver::current()
             .ensure_latest(Binaries::GpuMiner, progress.clone())
             .await;
-
-        drop(
-            state
-                .gpu_miner
-                .write()
-                .await
-                .detect()
-                .await
-                .inspect_err(|e| error!(target: LOG_TARGET, "Could not detect gpu miner: {:?}", e)),
-        );
 
         progress.set_max(30).await;
         progress
