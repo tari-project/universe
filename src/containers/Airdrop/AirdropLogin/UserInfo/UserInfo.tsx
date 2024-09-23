ff5c55fdf38ed110886891e6d8469a4a16c1a4e9
Juan <47256233+PanchoBubble@users.noreply.github.com>
2024-09-23 14:19:33 +0100

    feat: referral claim flow updates (#559)
    Referral flow updates

--- src/containers/Airdrop/AirdropLogin/UserInfo/UserInfo.tsx
+++ src/containers/Airdrop/AirdropLogin/UserInfo/UserInfo.tsx
@@ -16,7 +16,7 @@ import {
 import gemImage from './images/gems.png';
 import { FaBell } from 'react-icons/fa6';
 import { useCallback, useEffect, useState } from 'react';
-import { useAirdropStore } from '@app/store/useAirdropStore';
+import { GIFT_GEMS, REFERRAL_GEMS, useAirdropStore } from '@app/store/useAirdropStore';
 import { useTranslation } from 'react-i18next';
 import { AnimatePresence } from 'framer-motion';
 import DownloadReferralModal from './DownloadReferralModal/DownloadReferralModal';
@@ -26,7 +26,7 @@ import GemsPill from './GemsPill/GemsPill';
 export default function UserInfo() {
     const { logout, userDetails, airdropTokens, userPoints, wipUI, referralCount } = useAirdropStore();
     const [open, setOpen] = useState(false);
-    const [referalOpen, setReferalOpen] = useState(false);
+    const [modalOpen, setModalOpen] = useState<'claim' | 'invite' | undefined>(undefined);
 
     const { t } = useTranslation(['airdrop'], { useSuspense: false });
 
@@ -44,12 +44,8 @@ export default function UserInfo() {
         logout();
     };
 
-    const handleReferral = () => {
-        setReferalOpen(true);
-    };
-
     const handleReferalClose = () => {
-        setReferalOpen(false);
+        setModalOpen(undefined);
     };
 
     const handleClickOutside = useCallback(
@@ -111,10 +107,17 @@ export default function UserInfo() {
                     <AnimatePresence>
                         {open && (
                             <Menu initial={{ opacity: 0, y: -10 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0 }}>
-                                <MenuItem onClick={handleReferral}>
+                                <MenuItem onClick={() => setModalOpen('invite')}>
                                     {t('referral')}{' '}
                                     <NumberPill>
-                                        <StatsIcon src={gemImage} alt="Gems" className="StatsIcon-gems" /> +2000
+                                        <StatsIcon src={gemImage} alt="Gems" className="StatsIcon-gems" /> +
+                                        {REFERRAL_GEMS}
+                                    </NumberPill>
+                                </MenuItem>
+                                <MenuItem onClick={() => setModalOpen('claim')}>
+                                    {t('claimGems')}{' '}
+                                    <NumberPill>
+                                        <StatsIcon src={gemImage} alt="Gems" className="StatsIcon-gems" /> +{GIFT_GEMS}
                                     </NumberPill>
                                 </MenuItem>
                                 <MenuItem onClick={handleLogout}>{t('logout')}</MenuItem>
@@ -124,9 +127,9 @@ export default function UserInfo() {
                 </MenuWrapper>
             </Wrapper>
             <AnimatePresence>
-                {referalOpen && (
+                {modalOpen && (
                     <DownloadReferralModal
-                        referralCode={userDetails?.user?.referral_code || ''}
+                        referralCode={modalOpen === 'invite' ? userDetails?.user?.referral_code : undefined}
                         onClose={handleReferalClose}
                     />
                 )}
