967f0a66891addc130b29e21f221c0571446ffe3
Maciej Kożuszek <mackozuszek@gmail.com>
2024-09-23 16:11:10 +0200

    fix(gpu): prevent app crash if gpu detect fails (#555)
    Description
    ---
    On my integrated intel gpu the gpu miner would fail to detect the gpu
    causing crash of the entire application. This PR only makes it log error
    and continue working as gpu mining will be disabled.
    
    Motivation and Context
    ---
    Stop crashing app with unsupported GPUs.
    
    How Has This Been Tested?
    ---
    Tested manually on my machine (Ubuntu with integrated intel gpu)
    
    What process can a PR reviewer use to test or verify this change?
    ---
    same as above
    
    
    Breaking Changes
    ---
    
    - [x] None
    - [ ] Requires data directory on base node to be deleted
    - [ ] Requires hard fork
    - [ ] Other - Please specify

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -354,6 +354,14 @@ async fn setup_inner(
         .read_current_highest_version(Binaries::GpuMiner, progress.clone())
         .await?;
 
+    let _unused = state
+        .gpu_miner
+        .write()
+        .await
+        .detect()
+        .await
+        .inspect_err(|e| error!(target: LOG_TARGET, "Could not detect gpu miner: {:?}", e));
+
     if now
         .duration_since(last_binaries_update_timestamp)
         .unwrap_or(Duration::from_secs(0))
@@ -401,8 +409,6 @@ async fn setup_inner(
         let _unused = BinaryResolver::current()
             .ensure_latest(Binaries::GpuMiner, progress.clone())
             .await.inspect_err(|e| error!(target: LOG_TARGET, "Could not ensure latest version of GpuMiner: {:?}", e));
-
-        state.gpu_miner.write().await.detect().await?;
 
         progress.set_max(30).await;
         progress
