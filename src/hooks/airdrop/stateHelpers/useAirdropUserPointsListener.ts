839230c8ab29ac5afd342154adfe6e57746724ce
Juan De Luca <juan.f.d.luca@gmail.com>
2024-09-24 16:54:48 +0100

    fixes

--- src/hooks/airdrop/stateHelpers/useAirdropUserPointsListener.ts
+++ src/hooks/airdrop/stateHelpers/useAirdropUserPointsListener.ts
@@ -45,5 +45,5 @@ export const useAirdropUserPointsListener = () => {
             unListen();
         };
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    }, [bonusTiers, referralCount?.count]);
 };
