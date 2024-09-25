94f91c8eeebd0a6fc3f95e7a3c4c9b92e611d8ee
Juan De Luca <juan.f.d.luca@gmail.com>
2024-09-25 12:11:32 +0100

    Adds airdrop ui setting to settings file

--- src/containers/Airdrop/AirdropGiftTracker/AirdropGiftTracker.tsx
+++ src/containers/Airdrop/AirdropGiftTracker/AirdropGiftTracker.tsx
@@ -3,12 +3,14 @@ import { Title, Wrapper, TitleWrapper } from './styles';
 import LoggedOut from './sections/LoggedOut/LoggedOut';
 import LoggedIn from './sections/LoggedIn/LoggedIn';
 import { useAirdropSyncState } from '@app/hooks/airdrop/useAirdropSyncState';
+import { useAppConfigStore } from '@app/store/useAppConfigStore';
 
 export default function AirdropGiftTracker() {
     useAirdropSyncState();
-    const { airdropTokens, wipUI } = useAirdropStore();
+    const airdrop_ui_enabled = useAppConfigStore((s) => s.airdrop_ui_enabled);
+    const { airdropTokens } = useAirdropStore();
 
-    if (!wipUI) return null;
+    if (!airdrop_ui_enabled) return null;
 
     const isLoggedIn = !!airdropTokens;
 
