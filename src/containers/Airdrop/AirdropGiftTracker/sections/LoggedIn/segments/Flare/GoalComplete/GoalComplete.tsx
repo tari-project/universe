5cd01c82e3c0db6821690bc735231ec278f6e144
Aaron Pepper <pepdog@gmail.com>
2024-09-25 20:25:09 +0800

    feat(ui): airdrop gift box animation (#603)
    This adds a nice gift box opening animation to the GoalComplete flare on
    airdrop.
    
    
    https://www.loom.com/share/5885720a165b4ed5b256e34560b49ef4?sid=b8b94fd8-dab0-4d06-a839-057ce210de2f

--- src/containers/Airdrop/AirdropGiftTracker/sections/LoggedIn/segments/Flare/GoalComplete/GoalComplete.tsx
+++ src/containers/Airdrop/AirdropGiftTracker/sections/LoggedIn/segments/Flare/GoalComplete/GoalComplete.tsx
@@ -1,7 +1,11 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import GemsAnimation from '../GemsAnimation/GemsAnimation';
-import { Background, Wrapper } from './styles';
+import { Background, GiftBox, GiftBoxLid, GiftBoxShine, GiftBoxWrapper, IntroBox, Wrapper } from './styles';
 import { Number, Text } from '../styles';
+
+import giftBoxImage from '../images/gold_gift_box.png';
+import giftBoxLidImage from '../images/gold_gift_box_lid.png';
+import giftBoxShineImage from '../images/gift_box_shine.png';
 
 interface Props {
     gems: number;
@@ -9,43 +13,80 @@ interface Props {
 }
 
 export default function FriendAccepted({ gems, onAnimationComplete }: Props) {
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            onAnimationComplete();
-        }, 10000);
+    const [showIntro, setShowIntro] = useState(true);
+    const [showGiftBox, setShowGiftBox] = useState(true);
 
-        return () => clearTimeout(timer);
+    const introDuration = 2000;
+    const mainDuration = 11500;
+
+    useEffect(() => {
+        const introTimer = setTimeout(() => {
+            setShowIntro(false);
+            setShowGiftBox(false);
+        }, introDuration);
+
+        const mainTimer = setTimeout(() => {
+            onAnimationComplete();
+        }, mainDuration);
+
+        return () => {
+            clearTimeout(introTimer);
+            clearTimeout(mainTimer);
+        };
     }, [onAnimationComplete]);
 
     return (
         <Wrapper>
-            <Number
-                initial={{ opacity: 0, scale: 2 }}
-                animate={{ opacity: 1, scale: 1 }}
-                exit={{ opacity: 0, y: -20 }}
-                transition={{ delay: 0.5 }}
-            >
-                {gems.toLocaleString()}
-            </Number>
+            <IntroBox>
+                <GiftBoxWrapper
+                    initial={{ opacity: 0, y: '100%' }}
+                    animate={showGiftBox ? { opacity: 1, y: 0 } : { opacity: 1, y: '70%' }}
+                    transition={{ type: 'spring', damping: 10, stiffness: 100 }}
+                >
+                    <GiftBoxShine
+                        src={giftBoxShineImage}
+                        alt=""
+                        initial={{ opacity: 0, scale: 0 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        transition={{ delay: 1, duration: 1 }}
+                    />
+                    <GiftBoxLid
+                        src={giftBoxLidImage}
+                        alt=""
+                        initial={{ y: 0 }}
+                        animate={{ y: -50, opacity: 0, scale: 2 }}
+                        transition={{ delay: 1.5 }}
+                    />
+                    <GiftBox src={giftBoxImage} alt="" />
+                </GiftBoxWrapper>
+            </IntroBox>
 
-            <Text
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: -20 }}
-                transition={{ delay: 0.85 }}
-            >
-                Bonus gems earned
-                <br /> You reached your gifting goal!
-            </Text>
+            {!showIntro && (
+                <>
+                    <Number
+                        initial={{ opacity: 0, scale: 0, y: 100 }}
+                        animate={{ opacity: 1, scale: 1, y: 0 }}
+                        exit={{ opacity: 0, y: -20 }}
+                        transition={{ delay: 0.5 }}
+                    >
+                        {gems.toLocaleString()}
+                    </Number>
 
-            <GemsAnimation delay={1} />
+                    <Text
+                        initial={{ opacity: 0, scale: 0, y: 100 }}
+                        animate={{ opacity: 1, scale: 1, y: 0 }}
+                        exit={{ opacity: 0, y: -20 }}
+                        transition={{ delay: 0.85 }}
+                    >
+                        Bonus gems earned
+                        <br /> You reached your gifting goal!
+                    </Text>
 
-            <Background
-                initial={{ scale: 3 }}
-                animate={{ scale: 1 }}
-                exit={{ scale: 3 }}
-                transition={{ duration: 0.5, ease: 'easeIn' }}
-            />
+                    <GemsAnimation delay={1} />
+                </>
+            )}
+
+            <Background />
         </Wrapper>
     );
 }
