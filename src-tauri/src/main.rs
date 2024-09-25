f9c3510bd1010e8b4a7af16895ee30503135aef5
stringhandler <stringhandler@protonmail.com>
2024-09-25 18:27:02 +0200

    fix: fix critical error on GPU detect

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -473,13 +473,13 @@ async fn setup_inner(
     //drop binary resolver to release the lock
     drop(binary_resolver);
 
-    state
+    let _ = state
         .gpu_miner
         .write()
         .await
         .detect(config_dir.clone())
         .await
-        .inspect_err(|e| error!(target: LOG_TARGET, "Could not detect gpu miner: {:?}", e))?;
+        .inspect_err(|e| error!(target: LOG_TARGET, "Could not detect gpu miner: {:?}", e));
 
     for _i in 0..2 {
         match state
