5df8425b3c7c50404ef4752512f19594d72c8ebb
Aaron Pepper <pepdog@gmail.com>
2024-09-25 20:25:09 +0800

    feat(ui): airdrop gift box animation (#603)
    This adds a nice gift box opening animation to the GoalComplete flare on
    airdrop.
    
    
    https://www.loom.com/share/5885720a165b4ed5b256e34560b49ef4?sid=b8b94fd8-dab0-4d06-a839-057ce210de2f

--- src/containers/Airdrop/AirdropGiftTracker/sections/LoggedIn/segments/Flare/GoalComplete/styles.ts
+++ src/containers/Airdrop/AirdropGiftTracker/sections/LoggedIn/segments/Flare/GoalComplete/styles.ts
@@ -25,3 +25,44 @@ export const Background = styled(m.div)`
     background-size: cover;
     background-position: center;
 `;
+
+export const IntroBox = styled('div')`
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    width: 100%;
+    height: 100%;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const GiftBoxWrapper = styled(m.div)`
+    width: 211px;
+    height: 149px;
+    position: relative;
+    z-index: 2;
+`;
+
+export const GiftBox = styled(m.img)`
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 0;
+`;
+
+export const GiftBoxShine = styled(m.img)`
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 1;
+`;
+
+export const GiftBoxLid = styled(m.img)`
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 2;
+`;
