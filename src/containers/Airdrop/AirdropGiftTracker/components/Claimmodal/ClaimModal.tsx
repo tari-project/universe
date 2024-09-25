fd39867f62af2dba87f708667577691573de45ed
Aaron Pepper <pepdog@gmail.com>
2024-09-25 15:22:50 +0800

    updated claim modal UI

--- src/containers/Airdrop/AirdropGiftTracker/components/Claimmodal/ClaimModal.tsx
+++ src/containers/Airdrop/AirdropGiftTracker/components/Claimmodal/ClaimModal.tsx
@@ -1,26 +1,29 @@
 import {
-    ActionWrapper,
     BoxWrapper,
     ClaimButton,
     Cover,
+    FormWrapper,
     Gem1,
     Gem2,
     Gem3,
     GemsWrapper,
-    GemTextImage,
-    ShareWrapper,
+    InputGems,
+    InputLabel,
+    InputWrapper,
     StyledInput,
     Text,
     TextButton,
     TextWrapper,
     Title,
     Wrapper,
+    XLogo,
 } from './styles';
 import gemImage from './images/gems.png';
 import gemLargeImage from './images/gem-large.png';
 import { useCallback, useState } from 'react';
 import { GIFT_GEMS } from '@app/store/useAirdropStore';
-import { Trans, useTranslation } from 'react-i18next';
+import { GemImage } from '../Gems/styles';
+import XLogoIcon from './icons/XLogoIcon';
 
 interface ClaimModalProps {
     onSubmit: (code?: string) => void;
@@ -28,13 +31,17 @@ interface ClaimModalProps {
 }
 
 export default function ClaimModal({ onSubmit, onClose }: ClaimModalProps) {
-    const { t } = useTranslation(['airdrop'], { useSuspense: false });
+    //const { t } = useTranslation(['airdrop'], { useSuspense: false });
 
     const [claimCode, setClaimCode] = useState('');
 
-    const handleSubmit = useCallback(async () => {
-        return onSubmit(claimCode);
-    }, [claimCode, onSubmit]);
+    const handleSubmit = useCallback(
+        async (e: React.FormEvent<HTMLFormElement>) => {
+            e.preventDefault();
+            return onSubmit(claimCode);
+        },
+        [claimCode, onSubmit]
+    );
 
     return (
         <Wrapper>
@@ -46,35 +53,37 @@ export default function ClaimModal({ onSubmit, onClose }: ClaimModalProps) {
                 </GemsWrapper>
 
                 <TextWrapper>
-                    <Title>
-                        <Trans
-                            ns="airdrop"
-                            i18nKey="claimReferralCode"
-                            components={{ span: <span />, image: <GemTextImage src={gemImage} alt="" /> }}
-                            values={{ gems: GIFT_GEMS }}
-                        />
-                    </Title>
-                    <Text>{t('claimReferralGifts', { gems: GIFT_GEMS })}</Text>
+                    <Title>Earn rewards for Mining on Testnet</Title>
+                    <Text>
+                        Earn gems by mining and referring friends to increase your airdrop reward during testnet! Have a
+                        referral code? Enter it below and earn an additional{' '}
+                        <strong>{GIFT_GEMS.toLocaleString()}</strong> gems for you and your friend!
+                    </Text>
                 </TextWrapper>
-                <ActionWrapper>
-                    <ShareWrapper $isClaim>
+
+                <FormWrapper onSubmit={handleSubmit}>
+                    <InputWrapper>
+                        <InputLabel>Optional</InputLabel>
                         <StyledInput
                             type="text"
                             placeholder="Enter referral code"
                             onChange={(e) => setClaimCode(e.target.value)}
                             value={claimCode}
                         />
-                        {
-                            // <Button color="primary" onClick={handleReferral}>
-                            //     Claim
-                            // </Button>
-                        }
-                    </ShareWrapper>
-                    <ClaimButton onClick={handleSubmit}> Claim gems </ClaimButton>
-                </ActionWrapper>
-                <ActionWrapper>
-                    <TextButton onClick={onClose}>{`I'll do this later`}</TextButton>
-                </ActionWrapper>
+                        <InputGems>
+                            {GIFT_GEMS.toLocaleString()} <GemImage src={gemImage} alt="" />
+                        </InputGems>
+                    </InputWrapper>
+
+                    <ClaimButton type="submit">
+                        Claim gems{' '}
+                        <XLogo>
+                            <XLogoIcon />
+                        </XLogo>
+                    </ClaimButton>
+                </FormWrapper>
+
+                <TextButton onClick={onClose}>{`I'll do this later`}</TextButton>
             </BoxWrapper>
 
             <Cover onClick={onClose} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} />
