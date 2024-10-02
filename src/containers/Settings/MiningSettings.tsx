5de4f12e33cfe2e04cc188e0ed4843899270829c
Misieq01 <bartosz2000walczak@gmail.com>
2024-10-02 14:23:36 +0200

    Remove unused import

--- src/containers/Settings/MiningSettings.tsx
+++ src/containers/Settings/MiningSettings.tsx
@@ -1,6 +1,5 @@
 import CpuMiningMarkup from './sections/mining/CpuMiningMarkup.tsx';
 import GpuMiningMarkup from './sections/mining/GpuMiningMarkup.tsx';
-import SeedWordsMarkup from './sections/wallet/SeedWordsMarkup/index.ts';
 
 export const MiningSettings = () => {
     return (
