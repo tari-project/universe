d1d25e7b3050e8c6db0a9afc0ed16f64fdd13351
Misieq01 <38589417+Misieq01@users.noreply.github.com>
2024-10-07 12:43:24 +0200

    feat: move seed words to wallet tab (#694)
    ### [ Features ]
    - Move Seed words from mining tab to wallet tab

--- src/containers/Settings/MiningSettings.tsx
+++ src/containers/Settings/MiningSettings.tsx
@@ -1,13 +1,11 @@
 import CpuMiningMarkup from './sections/mining/CpuMiningMarkup.tsx';
 import GpuMiningMarkup from './sections/mining/GpuMiningMarkup.tsx';
-import SeedWordsMarkup from './sections/mining/SeedWordsMarkup';
 
 export const MiningSettings = () => {
     return (
         <>
             <CpuMiningMarkup />
             <GpuMiningMarkup />
-            <SeedWordsMarkup />
         </>
     );
 };
