aa6cf71a429fb4923f785a8ca887a290c46a391c
Misieq01 <bartosz2000walczak@gmail.com>
2024-10-09 14:21:53 +0200

    fix download url

--- src-tauri/src/external_dependencies.rs
+++ src-tauri/src/external_dependencies.rs
@@ -120,7 +120,7 @@ impl ExternalDependencies {
                     display_description:
                         "This is the additional runtime required to run Tari applications."
                             .to_string(),
-                    download_url: "https://aka.ms/vs/16/release/vc_redist.x64.exe".to_string(),
+                    download_url: "https://aka.ms/vs/17/release/vc_redist.x64.exe".to_string(),
                     manufacturer: Manufacturer::new(
                         "Microsoft".to_string(),
                         "https://www.microsoft.com".to_string(),
@@ -137,7 +137,7 @@ impl ExternalDependencies {
                     display_name: "Microsoft Visual C++ 2022 x64 Minimum Runtime".to_string(),
                     display_description:
                         "This is the minimum runtime required to run Tari applications.".to_string(),
-                    download_url: "https://aka.ms/vs/16/release/vc_redist.x64.exe".to_string(),
+                    download_url: "https://aka.ms/vs/17/release/vc_redist.x64.exe".to_string(),
                     manufacturer: Manufacturer::new(
                         "Microsoft".to_string(),
                         "https://www.microsoft.com".to_string(),
