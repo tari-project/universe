f47fa6db4a4e14c4025f4e8e8affa76905fc6f4e
Aaron Pepper <pepdog@gmail.com>
2024-10-09 18:35:30 +0800

    fix(ui): design feedback ui tweaks (#784)
    - Adjusted padding and border radius on sidebar container
    
    - Removed divider line between tiles and start mining button
    
    - Adjusted spacing on mining stat tiles to make them tighter
    
    - Adjusted large sidebar text to blend it in to the background more and
    moved it closer to the edge
    
    - Added `toLocaleString` to ruler numbers so they have a comma
    
    ![CleanShot 2024-10-09 at 17 32
    55](https://github.com/user-attachments/assets/486ca684-e0cb-4347-8e1c-028ae05c4029)

--- src/containers/SideBar/SideBar.tsx
+++ src/containers/SideBar/SideBar.tsx
@@ -1,23 +1,21 @@
 import Miner from './Miner/Miner';
 import Wallet from './components/Wallet/Wallet.tsx';
 import Heading from './components/Heading';
-import { Bottom, Scroll, SideBarContainer, Top } from './styles';
+import { Bottom, Scroll, SideBarContainer, SidebarTop, Top } from './styles';
 
 import MiningButton from '@app/containers/Dashboard/MiningView/components/MiningButton.tsx';
 import AirdropGiftTracker from '@app/containers/Airdrop/AirdropGiftTracker/AirdropGiftTracker';
-import { Divider } from '@app/components/elements/Divider.tsx';
 import LostConnectionAlert from './components/LostConnectionAlert';
 
 function SideBar() {
     return (
         <SideBarContainer>
-            <Top style={{ padding: '0 16px 12px', gap: '20px' }}>
+            <SidebarTop>
                 <Heading />
                 <MiningButton />
                 <LostConnectionAlert />
-            </Top>
-            <Divider />
-            <Scroll style={{ padding: '12px 16px 16px' }}>
+            </SidebarTop>
+            <Scroll>
                 <Top>
                     <Miner />
                 </Top>
