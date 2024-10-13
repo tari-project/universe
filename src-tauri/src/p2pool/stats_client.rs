0adaeebf87d7d6ec6218447355e6d36ad8091cb7
stringhandler <stringhandler@protonmail.com>
2024-10-13 17:59:43 +0200

    sdf

--- src-tauri/src/p2pool/stats_client.rs
+++ src-tauri/src/p2pool/stats_client.rs
@@ -1,6 +1,8 @@
 use crate::p2pool::models::Stats;
 use anyhow::Error;
+use log::warn;
 
+const LOG_TARGET: &str = "tari::universe::p2pool_stats_client";
 #[derive(Clone)]
 pub struct Client {
     stats_server_address: String,
@@ -17,7 +19,8 @@ impl Client {
         let stats = reqwest::get(format!("{}/stats", self.stats_server_address))
             .await?
             .json::<Stats>()
-            .await?;
+            .await
+            .inspect_err(|e| warn!(target: LOG_TARGET, "P2pool stats error: {:?}", e))?;
         Ok(stats)
     }
 }
