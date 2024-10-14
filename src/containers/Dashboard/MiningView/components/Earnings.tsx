2c5bd07f04eb937d9987dd1e4afe8928bcb5ed47
shan <47271333+shanimal08@users.noreply.github.com>
2024-10-14 14:36:30 +0200

    feat(ui): earnings recap (#807)
    Description
    ---
    - refactored win/lose animation triggers to check transactions (remove
    balance change checks since we have transaction history now)
    - also includes moving some of the mining hooks & store updaters around
    and into `useMiningStatesSync`
    - moved wallet, transaction, and mining metrics 'fetch' functions out of
    the stores and into their own hooks so they can be used independently of
    the stores
    - store missed wins if `canAnimate` was false for a won block
    - show the recap once window is focused again
    - adjusted `Earnings` styling to cater for the new recap section
    - updated the debug settings block time counter to use block_time from
    base_node instead of the display block height (also updates when not
    mining)
    
    
    Motivation and Context
    ---
    - no ticket but resolves Recap History
    [(figma)](https://www.figma.com/design/Tyh0UcWVo44z66gYHd0TBs/Tari-Universe-Planning-%26-Wires?node-id=2922-148285&node-type=canvas&m=dev)
    
    
    
    How Has This Been Tested?
    ---
    
    - locally
    
    ### **recap after minimised:**
    
    
    https://github.com/user-attachments/assets/7b071ea8-8a1b-48ad-93cd-7218146bf8cc
    
    ----
    
    ### **recap after just being on a different screen:**
    
    (and a win right after focusing, cos i am DOMINATING esmeralda
    apparently)
    
    
    https://github.com/user-attachments/assets/bacc978e-d6f4-4eb0-8bed-ad9bf6069aa1
    
    ----
    
    ### **win while focused still works as normal + debug block time:**
    
    
    
    https://github.com/user-attachments/assets/45f3f7e4-6a0d-49db-868e-3982aa374f99
    
    ----
    
    **~~still trying to get a fail animation, but i think i'm the only
    person on esme right now or something, but it does work :3~~**
    
    ### **lose while focused still working as before:**
    
    
    
    https://github.com/user-attachments/assets/9aa6b04e-f3fa-44aa-8305-dd14196d74f6
    
    
    ----
    
    What process can a PR reviewer use to test or verify this change?
    ---
    
    - run app
    - check if winning and losing a block still works as before 
    - leave the app minimized or on a non-focused screen for a while
    - come back and check that the blocks you won previously are accounted
    for
    
    ---------
    
    Co-authored-by: Brian Pearce <brianp@users.noreply.github.com>

--- src/containers/Dashboard/MiningView/components/Earnings.tsx
+++ src/containers/Dashboard/MiningView/components/Earnings.tsx
@@ -1,16 +1,16 @@
 import { AnimatePresence } from 'framer-motion';
 
-import { EarningsContainer, EarningsWrapper } from './Earnings.styles.ts';
+import { EarningsContainer, EarningsWrapper, RecapText, ShinyWrapper, WinText, WinWrapper } from './Earnings.styles.ts';
 import formatBalance from '@app/utils/formatBalance.ts';
 
 import CharSpinner from '@app/components/CharSpinner/CharSpinner.tsx';
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 import { useBlockchainVisualisationStore } from '@app/store/useBlockchainVisualisationStore.ts';
 
 const variants = {
     visible: {
         opacity: 1,
-        y: -190,
+        y: '-150%',
         scale: 1.05,
         transition: {
             duration: 1.25,
@@ -22,7 +22,7 @@ const variants = {
     },
     hidden: {
         opacity: 0.2,
-        y: -160,
+        y: '-100%',
         transition: { duration: 0.2, delay: 3, ease: 'linear' },
     },
 };
@@ -31,15 +31,35 @@ export default function Earnings() {
     const { t } = useTranslation('mining-view', { useSuspense: false });
 
     const earnings = useBlockchainVisualisationStore((s) => s.earnings);
-    const formatted = formatBalance(earnings || 0);
+    const recapData = useBlockchainVisualisationStore((s) => s.recapData);
+
+    const displayEarnings = recapData?.totalEarnings || earnings;
+
+    const formatted = formatBalance(displayEarnings || 0, 1);
 
     return (
         <EarningsContainer>
             <AnimatePresence mode="wait">
-                {earnings ? (
+                {displayEarnings ? (
                     <EarningsWrapper variants={variants} initial="hidden" animate="visible" exit="hidden">
-                        <span>{t('your-reward-is')}</span>
-                        <CharSpinner value={formatted.toString()} fontSize={72} />
+                        {recapData?.totalEarnings ? (
+                            <RecapText>
+                                <Trans
+                                    ns="mining-view"
+                                    i18nKey={'you-won-while-away'}
+                                    values={{
+                                        blocks: `${recapData.count} block${recapData.count === 1 ? `` : 's'}`,
+                                    }}
+                                    components={{ span: <span /> }}
+                                />
+                            </RecapText>
+                        ) : null}
+                        <WinWrapper>
+                            <WinText>{t('your-reward-is')}</WinText>
+                            <ShinyWrapper>
+                                <CharSpinner value={formatted.toString()} fontSize={76} XTMAlignment="center" />
+                            </ShinyWrapper>
+                        </WinWrapper>
                     </EarningsWrapper>
                 ) : null}
             </AnimatePresence>
