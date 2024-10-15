68c36c824b88e8ad7ba785a8b8d5b4ac835ba0b0
Aaron Pepper <pepdog@gmail.com>
2024-10-15 19:07:27 +0800

    add verify words selector ui

--- src/containers/StagedSecurity/sections/VerifySeedPhrase/VerifySeedPhrase.tsx
+++ src/containers/StagedSecurity/sections/VerifySeedPhrase/VerifySeedPhrase.tsx
@@ -1,10 +1,107 @@
+import { useState } from 'react';
 import { StagedSecuritySectionType } from '../../StagedSecurityModal';
-import { Wrapper } from './styles';
+import { BlackButton, Text, Title } from '../../styles';
+import {
+    ButtonWrapper,
+    PhraseWrapper,
+    TextWrapper,
+    WordButton,
+    WordButtons,
+    WordPill,
+    WordsSelected,
+    Wrapper,
+} from './styles';
+import { AnimatePresence } from 'framer-motion';
+import PillCloseIcon from '../../icons/PillCloseIcon';
+
+const words: string[] = [
+    'Mother',
+    'Dog',
+    'Shaker',
+    'Drag',
+    'Shoe',
+    'Snow',
+    'Bee',
+    'Thirst',
+    'Draw',
+    'Snake',
+    'Pickle',
+    'Smear',
+    'Crane',
+    'Trunk',
+    'Salt',
+    'Drain',
+    'Clap',
+    'Smack',
+    'Rust',
+    'Red',
+    'Juice',
+    'Lemon',
+    'Drink',
+    'Well',
+];
 
 interface Props {
     setSection: (section: StagedSecuritySectionType) => void;
 }
 
 export default function VerifySeedPhrase({ setSection }: Props) {
-    return <Wrapper>VerifySeedPhrase</Wrapper>;
+    const [completed, setCompleted] = useState(false);
+    const [selectedWords, setSelectedWords] = useState<string[]>([]);
+
+    const toggleSelectedWord = (word: string) => {
+        if (selectedWords.includes(word)) {
+            setSelectedWords(selectedWords.filter((w) => w !== word));
+        } else {
+            setSelectedWords([...selectedWords, word]);
+        }
+    };
+
+    return (
+        <Wrapper>
+            <TextWrapper>
+                <Title>Verify your seed phrase</Title>
+                <Text>Select the words in the correct order.</Text>
+            </TextWrapper>
+
+            <PhraseWrapper>
+                <WordsSelected>
+                    <AnimatePresence mode="popLayout">
+                        {selectedWords.map((word) => (
+                            <WordPill
+                                key={word}
+                                layout
+                                initial={{ opacity: 0, y: 10 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                exit={{ opacity: 0, scale: 0.5 }}
+                                transition={{ type: 'spring', stiffness: 300, damping: 25 }}
+                                onClick={() => toggleSelectedWord(word)}
+                            >
+                                {word}
+                                <PillCloseIcon />
+                            </WordPill>
+                        ))}
+                    </AnimatePresence>
+                </WordsSelected>
+
+                <WordButtons>
+                    {words.map((word, index) => (
+                        <WordButton
+                            key={index}
+                            onClick={() => toggleSelectedWord(word)}
+                            $selected={selectedWords.includes(word)}
+                        >
+                            {word}
+                        </WordButton>
+                    ))}
+                </WordButtons>
+            </PhraseWrapper>
+
+            <ButtonWrapper>
+                <BlackButton onClick={() => setSection('VerifySeedPhrase')} disabled={!completed}>
+                    <span>Complete Verification</span>
+                </BlackButton>
+            </ButtonWrapper>
+        </Wrapper>
+    );
 }
--- src/containers/StagedSecurity/sections/VerifySeedPhrase/styles.ts
+++ src/containers/StagedSecurity/sections/VerifySeedPhrase/styles.ts
@@ -1,3 +1,112 @@
-import styled from 'styled-components';
+import { m } from 'framer-motion';
+import styled, { css } from 'styled-components';
 
-export const Wrapper = styled('div')``;
+export const Wrapper = styled('div')`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 25px;
+
+    padding-top: 60px;
+    width: 100%;
+    max-width: 710px;
+`;
+
+export const TextWrapper = styled('div')`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+`;
+
+export const PhraseWrapper = styled('div')`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 25px;
+    width: 100%;
+`;
+
+export const WordButtons = styled('div')`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 7px;
+    padding-bottom: 25px;
+    width: 100%;
+    max-width: 566px;
+`;
+
+export const WordButton = styled('div')<{ $selected?: boolean }>`
+    display: flex;
+    padding: 5px 10px;
+    align-items: flex-start;
+
+    border-radius: 8px;
+    background: rgba(0, 0, 0, 0.1);
+
+    color: #000;
+    font-size: 15px;
+    font-weight: 600;
+    line-height: 200%;
+
+    transition:
+        background 0.2s ease,
+        opacity 0.2s ease;
+    cursor: pointer;
+
+    &:hover {
+        background: rgba(0, 0, 0, 0.2);
+    }
+
+    ${({ $selected }) =>
+        $selected &&
+        css`
+            opacity: 0.2;
+        `};
+`;
+
+export const WordsSelected = styled('div')`
+    width: 100%;
+    max-width: 566px;
+    height: 225px;
+
+    border-radius: 15px;
+    background: rgba(0, 0, 0, 0.15);
+
+    display: flex;
+    align-items: flex-start;
+    justify-content: flex-start;
+    flex-wrap: wrap;
+    gap: 10px;
+
+    padding: 15px;
+`;
+
+export const WordPill = styled(m.div)`
+    border-radius: 8px;
+    background: rgba(0, 0, 0, 0.1);
+    padding: 0 10px;
+    height: 40px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+
+    color: #000;
+    font-size: 15px;
+    font-weight: 600;
+    line-height: 100%;
+
+    cursor: pointer;
+    transition: background 0.2s ease;
+
+    &:hover {
+        background: rgba(0, 0, 0, 0.2);
+    }
+`;
+
+export const ButtonWrapper = styled('div')`
+    width: 100%;
+    max-width: 566px;
+`;
