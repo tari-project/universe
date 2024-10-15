ea28502ef01a7196fac01391818bb9465cc73937
Juan De Luca <juan.f.d.luca@gmail.com>
2024-10-15 14:48:58 +0100

    Fix build and adds types to msg

--- src/hooks/airdrop/useWebsocket.ts
+++ src/hooks/airdrop/useWebsocket.ts
@@ -1,25 +1,31 @@
 import { io } from 'socket.io-client';
 import { useAirdropStore } from '@app/store/useAirdropStore.ts';
+import { QuestCompletedEvent } from '@app/types/ws';
 
-let socket: typeof io;
+let socket: ReturnType<typeof io> | null;
 
 export const useWebsocket = () => {
     const airdropToken = useAirdropStore((state) => state.airdropTokens?.token);
+    const userId = useAirdropStore((state) => state.userDetails?.user?.id);
     const baseUrl = useAirdropStore((state) => state.backendInMemoryConfig?.airdropApiUrl);
-    console.log('ws hook', baseUrl?.replace('http', 'ws'), airdropToken);
+    // console.log('ws hook', baseUrl?.replace('http', 'ws'), airdropToken);
+
     const init = () => {
         if (!socket) {
             socket = io((baseUrl || 'ws://localhost:3004').replace('http', 'ws'), {
                 // path: '/ws',
-                cors: {
-                    origin: '*',
-                },
+                // cors: {
+                //     origin: '*',
+                // },
             });
         }
-        socket.emit('auth', airdropToken);
-        socket.on('message', (msg) => {
-            console.log('got ws message', msg);
-        });
+
+        if (airdropToken && userId) {
+            socket.emit('auth', airdropToken);
+            socket.on(userId, (msg: QuestCompletedEvent) => {
+                console.info('WS message', msg);
+            });
+        }
     };
 
     const disconnect = () => {
---
+++ src/types/ws.ts
@@ -0,0 +1,14 @@
+export enum WebsocketEventNames {
+    COMPLETED_QUEST = 'completed_quest',
+}
+
+export interface QuestCompletedEvent {
+    name: WebsocketEventNames.COMPLETED_QUEST;
+    data: {
+        questName: string;
+        userId: string;
+        questPointType?: unknown;
+        quetPoints?: number;
+        totalUserPoints?: number;
+    };
+}
