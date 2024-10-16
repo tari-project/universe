062ee446592c3257c7af63e0db654f98fb24657c
Maciej Kożuszek <mackozuszek@gmail.com>
2024-10-16 17:34:48 +0200

    fix: ensure single init (#865)
    Description
    ---
    Fixes #864 
    Double initialization caused invalid config in which port of the mmproxy
    was invalid and also caused sometimes progress bar to go back to 0%
    
    Motivation and Context
    ---
    Fix mining
    
    How Has This Been Tested?
    ---
    Run app and check if xmrig is using resources.
    
    What process can a PR reviewer use to test or verify this change?
    ---
    Check if there are 2 warning logs coming from initialization with
    description how much time it took. In my case these were:
    ```
    14:55:11 WARN  setup_application took too long: 66.029057277s
    14:55:11 WARN  setup_application took too long: 66.252814494s
    ```
    This indicates that there were 2 processes running along. After the fix
    there should be only 1 log with time it took.
    
    Another giveaway of invalid init is progress bar moving backward
    sometimes - this indicates that's there is another process messing with
    progress bar. This also shouldn't happen anymore.
    
    
    Breaking Changes
    ---
    
    - [x] None
    - [ ] Requires data directory on base node to be deleted
    - [ ] Requires hard fork
    - [ ] Other - Please specify
    
    ---------
    
    Co-authored-by: Brian Pearce <brianp@users.noreply.github.com>

--- src/hooks/useSetUp.ts
+++ src/hooks/useSetUp.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { listen } from '@tauri-apps/api/event';
 import { TauriEvent } from '../types.ts';
 
@@ -13,6 +13,7 @@ import { ExternalDependency } from '@app/types/app-status.ts';
 import { useHandleAirdropTokensRefresh } from '@app/hooks/airdrop/stateHelpers/useAirdropTokensRefresh.ts';
 
 export function useSetUp() {
+    const [isInitializing, setIsInitializing] = useState(false);
     const setView = useUIStore((s) => s.setView);
     const setSetupDetails = useAppStateStore((s) => s.setSetupDetails);
     const setError = useAppStateStore((s) => s.setError);
@@ -82,12 +83,17 @@ export function useSetUp() {
                     break;
             }
         });
-        if (isAfterAutoUpdate) {
+        if (isAfterAutoUpdate && !isInitializing) {
+            setIsInitializing(true);
             clearStorage();
-            invoke('setup_application').catch((e) => {
-                setCriticalError(`Failed to setup application: ${e}`);
-                setView('mining');
-            });
+            invoke('setup_application')
+                .catch((e) => {
+                    setCriticalError(`Failed to setup application: ${e}`);
+                    setView('mining');
+                })
+                .then(() => {
+                    setIsInitializing(false);
+                });
         }
         return () => {
             unlistenPromise.then((unlisten) => unlisten());
@@ -104,5 +110,6 @@ export function useSetUp() {
         setSeenPermissions,
         backendInMemoryConfig?.airdropApiUrl,
         handleRefreshAirdropTokens,
+        isInitializing,
     ]);
 }
