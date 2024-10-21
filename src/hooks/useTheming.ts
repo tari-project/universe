745f93a9973f5bea8cd1521d0b0fa80a9e3f898e
Shannon Tenner <shanimal@tari.com>
2024-10-21 14:20:10 +0200

    fixed dark mode animation build

--- src/hooks/useTheming.ts
+++ src/hooks/useTheming.ts
@@ -1,17 +1,17 @@
 export const animationLightBg = [
-    { property: 'bgColor1', value: '#F6F6F6' },
-    { property: 'bgColor2', value: '#EEEEEE' },
-    { property: 'neutralColor', value: '#FFFFFF' },
-    { property: 'mainColor', value: '#40d1f7' },
-    { property: 'successColor', value: '#5ff09a' },
+    { property: 'bgColor1', value: '#ffffff' },
+    { property: 'bgColor2', value: '#d0d0d0' },
+    { property: 'neutralColor', value: '#fff8ec' },
+    { property: 'mainColor', value: '#0096ff' },
+    { property: 'failColor', value: '#ca0101' },
     { property: 'goboIntensity', value: 0.4 },
 ];
 
 export const animationDarkBg = [
-    { property: 'bgColor1', value: '#2E2E2E' },
-    { property: 'bgColor2', value: '#2E2E2E' },
+    { property: 'bgColor1', value: '#212121' },
+    { property: 'bgColor2', value: '#212121' },
     { property: 'neutralColor', value: '#000000' },
     { property: 'mainColor', value: '#3880d7' },
-    { property: 'successColor', value: '#00c881' },
+    { property: 'failColor', value: '#e30032' },
     { property: 'goboIntensity', value: 0.7 },
 ];
