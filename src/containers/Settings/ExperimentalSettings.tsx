bfb917e8dbae3141ca1bece72692c7f03ace0bef
shan <47271333+shanimal08@users.noreply.github.com>
2024-10-22 16:29:22 +0200

    fix(ui): restart dialog clarity and move p2p toggle (#910)
    Description
    ---
    - update `Cancel` to `Restart later` on `RestartDialog`
    - added extra copy to explain that the settings will be stored and
    applied on the next start up
    - moved p2pool toggle to mining settings section
    - tweaked tor settings styling a little
    
    Motivation and Context
    ---
    
    - resolves #849 
    - resolves #828 
    
    How Has This Been Tested?
    ---
    - locally:
    
    
    https://github.com/user-attachments/assets/84020eef-dc8d-48ea-9821-99d22791d338

--- src/containers/Settings/ExperimentalSettings.tsx
+++ src/containers/Settings/ExperimentalSettings.tsx
@@ -1,12 +1,10 @@
 import { AnimatePresence } from 'framer-motion';
 import { useUIStore } from '@app/store/useUIStore.ts';
 import ExperimentalWarning from './sections/experimental/ExperimentalWarning.tsx';
-import P2pMarkup from './sections/experimental/P2pMarkup.tsx';
-
 import DebugSettings from '@app/containers/Settings/sections/experimental/DebugSettings.tsx';
 import AppVersions from '@app/containers/Settings/sections/experimental/AppVersions.tsx';
 import VisualMode from '@app/containers/Dashboard/components/VisualMode.tsx';
-import { SettingsGroup, SettingsGroupWrapper } from '@app/containers/Settings/components/SettingsGroup.styles.ts';
+import { SettingsGroup, SettingsGroupAction } from '@app/containers/Settings/components/SettingsGroup.styles.ts';
 import GpuDevices from './sections/experimental/GpuDevices.tsx';
 import { TorMarkup } from './sections/experimental/TorMarkup';
 
@@ -19,17 +17,17 @@ export const ExperimentalSettings = () => {
             <AnimatePresence>
                 {showExperimental && (
                     <>
-                        <P2pMarkup />
-                        {/* <P2poolStatsMarkup /> */}
                         <GpuDevices />
                         <DebugSettings />
                         <AppVersions />
                         <TorMarkup />
-                        <SettingsGroupWrapper>
-                            <SettingsGroup>
+                        <br />
+                        <SettingsGroup>
+                            <div />
+                            <SettingsGroupAction>
                                 <VisualMode />
-                            </SettingsGroup>
-                        </SettingsGroupWrapper>
+                            </SettingsGroupAction>
+                        </SettingsGroup>
                     </>
                 )}
             </AnimatePresence>
--- src/containers/Settings/sections/experimental/TorMarkup/TorMarkup.styles.ts
+++ src/containers/Settings/sections/experimental/TorMarkup/TorMarkup.styles.ts
@@ -4,19 +4,33 @@ import styled from 'styled-components';
 
 export const StyledInput = styled(Input)<{ hasError?: boolean }>(({ theme, hasError }) => ({
     borderColor: hasError ? theme.palette.error.main : theme.palette.colors.darkAlpha[10],
-    marginLeft: '15px',
+    // marginLeft: '15px',
 }));
 
 export const ErrorTypography = styled(Typography)(({ theme }) => ({
     color: theme.palette.error.main,
-    marginLeft: '15px',
     // Prevent jumping when the error message appears
     minHeight: '14px',
 }));
 
-export const SaveButtonWrapper = styled.div({
-    marginLeft: '15px',
-    alignSelf: 'flex-end',
-    // Prevent jumping when save available
-    minHeight: '36px',
-});
+export const TorSettingsContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    position: relative;
+    border-top: 1px solid ${({ theme }) => theme.palette.divider};
+    padding-top: 20px;
+    margin-top: 10px;
+    &:before {
+        content: 'Tor settings';
+        position: absolute;
+        background-color: ${({ theme }) => theme.palette.background.paper};
+        top: -9px;
+
+        color: ${({ theme }) => theme.palette.primary.light};
+        font-size: 12px;
+        font-weight: 600;
+        line-height: 18px;
+        letter-spacing: -0.1px;
+        padding-right: 12px;
+`;
