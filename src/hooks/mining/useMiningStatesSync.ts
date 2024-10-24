5f32ac6d9595fc40f08f7145aeb7b15953ac961e
Shannon Tenner <shanimal@tari.com>
2024-10-24 13:21:10 +0200

    revert sync change

--- src/hooks/mining/useMiningStatesSync.ts
+++ src/hooks/mining/useMiningStatesSync.ts
@@ -3,15 +3,12 @@ import { useBlockInfo } from '@app/hooks/mining/useBlockInfo.ts';
 import { useUiMiningStateMachine } from '@app/hooks/mining/useMiningUiStateMachine.ts';
 
 import { useWalletStore } from '@app/store/useWalletStore.ts';
-import { useCallback, useEffect, useRef } from 'react';
+import { useCallback, useEffect } from 'react';
 import useEarningsRecap from '@app/hooks/mining/useEarningsRecap.ts';
-import { useAppConfigStore } from '@app/store/useAppConfigStore.ts';
 
 export default function useMiningStatesSync() {
     const fetchMiningMetrics = useMiningMetricsUpdater();
     const fetchWalletDetails = useWalletStore((s) => s.fetchWalletDetails);
-    const miningMode = useAppConfigStore((s) => s.mode);
-    const prevMiningMode = useRef(miningMode || null);
 
     useBlockInfo();
     useUiMiningStateMachine();
@@ -21,15 +18,6 @@ export default function useMiningStatesSync() {
         await fetchMiningMetrics();
         await fetchWalletDetails();
     }, [fetchMiningMetrics, fetchWalletDetails]);
-
-    useEffect(() => {
-        // prevent delay in stats when switching modes
-        if (miningMode && prevMiningMode.current !== miningMode) {
-            callIntervalItems().then(() => {
-                prevMiningMode.current = miningMode;
-            });
-        }
-    }, [callIntervalItems, miningMode]);
 
     // intervalItems
     useEffect(() => {
