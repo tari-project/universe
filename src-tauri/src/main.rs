437878c9306cd3254f53f57303c1b146cdb7ba31
Juan De Luca <juan.f.d.luca@gmail.com>
2024-10-25 10:25:33 +0100

    round cast to isize

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -169,7 +169,7 @@ async fn set_mode(
     fn f64_to_isize_safe(_value: Option<f64>) -> Option<isize> {
         let value = _value.unwrap_or(-1.0);
         if value.is_finite() && value >= isize::MIN as f64 && value <= isize::MAX as f64 {
-            Some(value as isize)
+            Some(value.round() as isize)
         } else {
             warn!(target: LOG_TARGET, "Invalid value for custom_cpu_usage or custom_gpu_usage: {:?}", _value);
             None
