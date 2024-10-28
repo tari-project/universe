dc6362468868dfcadcf120e0d50c9f6c6b4b59bf
shan <47271333+shanimal08@users.noreply.github.com>
2024-10-28 13:32:18 +0200

    fix(ui): hide transaction history item if not a won block (#949)
    Description
    ---
    - hide non-win transactions from wallet history:
      - check if transaction message includes block height and no payment id
    - added struct for transaction items that includes `blockHeight` and
    mapped that in so we don't have to check for it in multiple places
    - removed unused structs and commented out unused p2p import for now
    
    Motivation and Context
    ---
    - resolves https://github.com/tari-project/universe/issues/862
    
    
    How Has This Been Tested?
    ---
    - locally:
    
    | before ☹️ | after 😀 |
    | :---: | :---: |
    | <img width="368" alt="image"
    src="https://github.com/user-attachments/assets/dc747313-4dfc-4172-8652-04afce08663b">
    | <img width="373" alt="image"
    src="https://github.com/user-attachments/assets/e01de1a2-c84f-4715-982e-635707f26f8e">
    |
    
    
    What process can a PR reviewer use to test or verify this change?
    ---
    - have a wallet linked to aurora
    - send or spend tXTM
    - check that any spend/send tx is no longer displayed in the 'recent
    wins' list without a. block height

--- src/containers/Settings/sections/p2p/PoolMiningSettings.tsx
+++ src/containers/Settings/sections/p2p/PoolMiningSettings.tsx
@@ -1,5 +1,5 @@
 import P2pMarkup from './P2pMarkup.tsx';
-import P2PoolStats from './P2poolStatsMarkup.tsx';
+// import P2PoolStats from './P2poolStatsMarkup.tsx';
 
 export const PoolMiningSettings = () => {
     return (
