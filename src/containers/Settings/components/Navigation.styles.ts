b11cf67e4c37b99e37d7924d36ac348f0636bde5
shan <47271333+shanimal08@users.noreply.github.com>
2024-11-06 11:55:50 +0200

    chore: add terms link (#1004)
    Description
    ---
    
    - just adds a lil link to our terms & conditions (LICENSE.md) +
    translations for it
    
    Motivation and Context
    ---
    - resovles https://tari.atlassian.net/browse/TC-24
    
    How Has This Been Tested?
    ---
    - locally:
    
    <img width="1256" alt="Screenshot 2024-11-05 at 15 45 27"
    src="https://github.com/user-attachments/assets/97459c3c-7079-4f2a-a408-1356cfb06149">
    
    
    (hover:)
    
    <img width="1256" alt="Screenshot 2024-11-05 at 15 45 48"
    src="https://github.com/user-attachments/assets/2ecebacc-1143-4a3a-8439-93ee50bf51be">
    
    What process can a PR reviewer use to test or verify this change?
    ---
    
    - click link in settings -> window should open
    [LICENSE.md](https://github.com/tari-project/universe/blob/main/LICENSE.md)

--- src/containers/Settings/components/Navigation.styles.ts
+++ src/containers/Settings/components/Navigation.styles.ts
@@ -2,16 +2,16 @@ import styled from 'styled-components';
 import { Button } from '@app/components/elements/buttons/Button.tsx';
 
 export const Container = styled.div`
-    padding: 32px 15px;
+    padding: 32px 15px 15px;
     display: flex;
     background: ${({ theme }) => theme.palette.background.default};
     flex-direction: column;
     justify-content: space-between;
     height: 100%;
-    width: 230px;
+    width: 250px;
 
     @media (max-width: 1200px) {
-        width: 200px;
+        width: 220px;
     }
 `;
 
@@ -27,4 +27,17 @@ export const SectionButton = styled(Button)`
     display: flex;
     text-transform: capitalize;
     cursor: pointer;
+`;
+
+export const TermsBtn = styled.button`
+    display: flex;
+    justify-content: center;
+    gap: 3px;
+    color: ${({ theme }) => theme.palette.text.accent};
+    transition: color 0.3s linear;
+    font-weight: 500;
+    font-size: 13px;
+    &:hover {
+        color: ${({ theme }) => theme.palette.text.secondary};
+    }
 `;
