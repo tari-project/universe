1c3ceba70e1a85199279b36f955486418234a871
Misieq01 <bartosz2000walczak@gmail.com>
2024-11-07 17:28:04 +0100

    remove console log

--- src/containers/floating/Settings/sections/mining/GpuDevices.tsx
+++ src/containers/floating/Settings/sections/mining/GpuDevices.tsx
@@ -41,8 +41,6 @@ const GpuDevices = () => {
         [excludedDevices, setExcludedDevice]
     );
 
-    console.log('gpuDevices', gpuDevices);
-
     return (
         <>
             <SettingsGroupWrapper>
