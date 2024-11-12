tari-project/universe

commit 936430d7bac765b687f2f220ba1baf5ad9ec08d0

fix(ui): animation end on startup (#1057)
Description
---

the main issue was that the _failed_ block animation has been showing
right on startup because it already started the mining animation and
then we'd get the mining metrics and move on to a 'new' block (the first
one really) and the animation would move on to "fail" state because we'd
check against your last tx block height to see if you won it, but it
goes to "fail" for two reasons - 1. a brand new user wouldn't have any
transactions to check against, and 2. even if it's an existing user with
a wallet, the initial mining metrics fetch would be checked against the
initial 0 or undefined block height, so it wouldn't be a win

this PR changes things so 1. we don't call `handleNewBlock` until we're
getting the actual block height from the base_node, and 2. we only show
the tower and start the mining animation at the same time as setting up
is complete and the mining metrics are fetched for the first time

(this won't prevent it from happening _ever again_ though, as there's a
chance you really could lose the latest block right as you've started up
and it moves onto solving for the next :p)

also did a bunch of cleanups re. the set up flow 

---

**animation specific:**

- update glApp build with a version that doesn't automatically
"showVisual" so we have better control over that on our side
- only set showVisual when setup is complete and we start mining (if
auto mining is on, which it is on a completely new install)

**setup flow cleanups/improvements:**

- moved external dependency listener to own hook and call in
`AppWrapper`
- fixed AutoUpdate setState error (split out listener and update logic,
also moved listeners to useEffects)
- added check for if miner metrics is currently being fetched or not
- cleaned up `useSetup` hook and removed old deps form the listener
dependency array that are no longer needed

Motivation and Context
---

src/hooks/useMiningMetricsUpdater.ts
@@ -1,42 +1,48 @@
 import { useMiningStore } from '@app/store/useMiningStore';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 import { invoke } from '@tauri-apps/api';
 import { setAnimationState } from '@app/visuals.ts';
 import { useBlockchainVisualisationStore } from '@app/store/useBlockchainVisualisationStore.ts';
 import useFetchTx from '@app/hooks/mining/useTransactions.ts';
 import * as Sentry from '@sentry/react';
 
-const useMiningMetricsUpdater = () => {
+export default function useMiningMetricsUpdater() {
     const fetchTx = useFetchTx();
     const currentBlockHeight = useMiningStore((s) => s.base_node.block_height);
     const baseNodeConnected = useMiningStore((s) => s.base_node.is_connected);
     const setMiningMetrics = useMiningStore((s) => s.setMiningMetrics);
     const handleNewBlock = useBlockchainVisualisationStore((s) => s.handleNewBlock);
+    const [isFetchingMetrics, setIsFetchingMetrics] = useState(false);
 
     return useCallback(async () => {
+        if (isFetchingMetrics) return;
         try {
+            setIsFetchingMetrics(true);
             const metrics = await invoke('get_miner_metrics');
-            const isMining = metrics.cpu?.mining.is_mining || metrics.gpu?.mining.is_mining;
-            // Pause animation when lost connection to the Tari Network
-            if (isMining && !metrics.base_node?.is_connected && baseNodeConnected) {
-                setAnimationState('stop');
-            } else if (isMining && metrics.base_node?.is_connected && !baseNodeConnected) {
-                setAnimationState('start');
+
+            if (metrics) {
+                const isMining = metrics.cpu?.mining.is_mining || metrics.gpu?.mining.is_mining;
+                // Pause animation when lost connection to the Tari Network
+                if (isMining && !metrics.base_node?.is_connected && baseNodeConnected) {
+                    setAnimationState('stop');
+                } else if (isMining && metrics.base_node?.is_connected && !baseNodeConnected) {
+                    setAnimationState('start');
+                }
+
+                const blockHeight = metrics.base_node.block_height;
+                if (blockHeight && blockHeight > currentBlockHeight) {
+                    await fetchTx();
+                    if (blockHeight > 0) {
+                        await handleNewBlock(blockHeight, isMining);
+                    }
+                }
+                setMiningMetrics(metrics);
+                setIsFetchingMetrics(false);
             }
-
-            const blockHeight = metrics.base_node.block_height;
-
-            if (blockHeight > currentBlockHeight) {
-                await fetchTx();
-                void handleNewBlock(blockHeight, isMining);
-            }
-
-            setMiningMetrics(metrics);
         } catch (e) {
             Sentry.captureException(e);
             console.error('Fetch mining metrics error: ', e);
+            setIsFetchingMetrics(false);
         }
-    }, [baseNodeConnected, currentBlockHeight, fetchTx, handleNewBlock, setMiningMetrics]);
-};
-
-export default useMiningMetricsUpdater;
+    }, [baseNodeConnected, currentBlockHeight, fetchTx, handleNewBlock, isFetchingMetrics, setMiningMetrics]);
+}

src/hooks/useSetUp.ts
@@ -1,46 +1,27 @@
+import * as Sentry from '@sentry/react';
 import { useCallback, useEffect, useState } from 'react';
 import { listen } from '@tauri-apps/api/event';
+import { invoke } from '@tauri-apps/api/tauri';
+
 import { TauriEvent } from '../types.ts';
 
-import { invoke } from '@tauri-apps/api/tauri';
-import { useUIStore } from '../store/useUIStore.ts';
 import { useAppStateStore } from '../store/appStateStore.ts';
 
-import { setAnimationState } from '@app/visuals.ts';
-
 import { useAirdropStore } from '@app/store/useAirdropStore.ts';
-import { ExternalDependency } from '@app/types/app-status.ts';
 import { useHandleAirdropTokensRefresh } from '@app/hooks/airdrop/stateHelpers/useAirdropTokensRefresh.ts';
-import * as Sentry from '@sentry/react';
 
 export function useSetUp() {
     const [isInitializing, setIsInitializing] = useState(false);
-    const setView = useUIStore((s) => s.setView);
-    const setSetupDetails = useAppStateStore((s) => s.setSetupDetails);
-    const setError = useAppStateStore((s) => s.setError);
-    const { setShowExternalDependenciesDialog } = useUIStore();
-    const isAfterAutoUpdate = useAppStateStore((s) => s.isAfterAutoUpdate);
-    const fetchApplicationsVersionsWithRetry = useAppStateStore((s) => s.fetchApplicationsVersionsWithRetry);
-
-    const settingUpFinished = useAppStateStore((s) => s.settingUpFinished);
-    const setSeenPermissions = useAirdropStore((s) => s.setSeenPermissions);
-    const setCriticalError = useAppStateStore((s) => s.setCriticalError);
-    const { backendInMemoryConfig } = useAirdropStore();
     const handleRefreshAirdropTokens = useHandleAirdropTokensRefresh();
 
-    const { loadExternalDependencies } = useAppStateStore();
+    const setSetupDetails = useAppStateStore((s) => s.setSetupDetails);
+    const setCriticalError = useAppStateStore((s) => s.setCriticalError);
+    const isAfterAutoUpdate = useAppStateStore((s) => s.isAfterAutoUpdate);
+    const setSettingUpFinished = useAppStateStore((s) => s.setSettingUpFinished);
+    const setSeenPermissions = useAirdropStore((s) => s.setSeenPermissions);
+    const fetchApplicationsVersionsWithRetry = useAppStateStore((s) => s.fetchApplicationsVersionsWithRetry);
 
-    useEffect(() => {
-        const unlistenPromise = listen<ExternalDependency[]>('missing-applications', (event) => {
-            const missingDependencies = event.payload;
-            loadExternalDependencies(missingDependencies);
-            setShowExternalDependenciesDialog(true);
-        });
-
-        return () => {
-            unlistenPromise.then((unlisten) => unlisten());
-        };
-    }, [loadExternalDependencies, setShowExternalDependenciesDialog]);
+    const backendInMemoryConfig = useAirdropStore((s) => s.backendInMemoryConfig);
 
     useEffect(() => {
         if (backendInMemoryConfig?.airdropApiUrl) {
@@ -48,7 +29,6 @@ export function useSetUp() {
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [backendInMemoryConfig?.airdropApiUrl]);
-
     const clearStorage = useCallback(() => {
         // clear all storage except airdrop data
         const airdropStorage = localStorage.getItem('airdrop-store');
@@ -57,6 +37,10 @@ export function useSetUp() {
             localStorage.setItem('airdrop-store', airdropStorage);
         }
     }, []);
+    const handlePostSetup = useCallback(async () => {
+        await fetchApplicationsVersionsWithRetry();
+        await setSettingUpFinished();
+    }, [fetchApplicationsVersionsWithRetry, setSettingUpFinished]);
 
     useEffect(() => {
         const unlistenPromise = listen('message', ({ event: e, payload: p }: TauriEvent) => {
@@ -64,12 +48,7 @@ export function useSetUp() {
                 case 'setup_status':
                     setSetupDetails(p.title, p.title_params, p.progress);
                     if (p.progress >= 1) {
-                        settingUpFinished();
-                        fetchApplicationsVersionsWithRetry();
-                        setView('mining');
-                        setAnimationState('showVisual');
-
-                        setSeenPermissions(true);
+                        handlePostSetup().finally(() => setSeenPermissions(true));
                     }
                     break;
                 default:
@@ -84,7 +63,6 @@ export function useSetUp() {
                 .catch((e) => {
                     Sentry.captureException(e);
                     setCriticalError(`Failed to setup application: ${e}`);
-                    setView('mining');
                 })
                 .then(() => {
                     setIsInitializing(false);
@@ -95,16 +73,11 @@ export function useSetUp() {
         };
     }, [
         clearStorage,
-        fetchApplicationsVersionsWithRetry,
+        handlePostSetup,
         isAfterAutoUpdate,
-        setError,
-        setSetupDetails,
-        setView,
-        settingUpFinished,
+        isInitializing,
         setCriticalError,
         setSeenPermissions,
-        backendInMemoryConfig?.airdropApiUrl,
-        handleRefreshAirdropTokens,
-        isInitializing,
+        setSetupDetails,
     ]);
 }

src/App/App.styles.ts
@@ -21,12 +21,17 @@ const variants = {
         opacity: 1,
         transition,
     },
+    exit: {
+        y: 0,
+        opacity: 1,
+        transition: { ...transition, duration: 0.3, ease: 'easeOut' },
+    },
 };
 
 export const AppContentContainer = styled(m.div).attrs({
     variants,
     animate: 'visible',
-    exit: 'hidden',
+    exit: 'exit',
 })`
     width: 100vw;
     height: 100vh;

src/App/App.tsx
@@ -30,7 +30,7 @@ export default function App() {
                  */}
                 <MotionConfig reducedMotion="user">
                     <FloatingElements />
-                    <AppContent>
+                    <AppContent key="app-content">
                         {isSettingUp ? <Setup /> : null}
                         {isShuttingDown || isSettingUp ? null : <MainView />}
                         {isShuttingDown ? <ShuttingDownScreen /> : null}

src/App/AppContent.tsx
@@ -4,7 +4,7 @@ import { AppContentWrapper } from './App.styles';
 
 export default function AppContent({ children }: { children: ReactNode }) {
     return (
-        <AppContentWrapper>
+        <AppContentWrapper key="app-content-wrapper">
             <AnimatePresence mode="wait">{children}</AnimatePresence>
         </AppContentWrapper>
     );

src/App/AppWrapper.tsx
@@ -1,3 +1,6 @@
+import { useDisableRefresh } from '@app/hooks/useDisableRefresh';
+import { useListenForExternalDependencies } from '@app/hooks/useListenForExternalDependencies';
+import { useUpdateListener } from '@app/hooks/useUpdateStatus';
 import * as Sentry from '@sentry/react';
 import { useEffect } from 'react';
 
@@ -26,38 +29,14 @@ const sentryOptions = {
 
 setupLogger();
 
-const useDisableRefresh = () => {
-    useEffect(() => {
-        if (process.env.NODE_ENV === 'development') {
-            return;
-        }
-        const keydownListener = function (event: KeyboardEvent) {
-            // Prevent F5 or Ctrl+R (Windows/Linux) and Command+R (Mac) from refreshing the page
-            if (event.key === 'F5' || (event.ctrlKey && event.key === 'r') || (event.metaKey && event.key === 'r')) {
-                event.preventDefault();
-            }
-        };
-
-        const contextmenuListener = function (event: MouseEvent) {
-            event.preventDefault();
-        };
-
-        document.addEventListener('keydown', keydownListener);
-        document.addEventListener('contextmenu', contextmenuListener);
-
-        return () => {
-            document.removeEventListener('keydown', keydownListener);
-            document.removeEventListener('contextmenu', contextmenuListener);
-        };
-    }, []);
-};
-
 export default function AppWrapper() {
-    useDetectMode();
     const allowTelemetry = useAppConfigStore((s) => s.allow_telemetry);
     const fetchAppConfig = useAppConfigStore((s) => s.fetchAppConfig);
-    useLangaugeResolver();
+    useDetectMode();
     useDisableRefresh();
+    useUpdateListener();
+    useLangaugeResolver();
+    useListenForExternalDependencies();
 
     useEffect(() => {
         async function initialize() {

src/containers/floating/AutoUpdateDialog/AutoUpdateDialog.tsx
@@ -1,96 +1,26 @@
-import * as Sentry from '@sentry/react';
+import { useUIStore } from '@app/store/useUIStore';
 
-import { useCallback, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
-import { checkUpdate, installUpdate, onUpdaterEvent } from '@tauri-apps/api/updater';
-
-import { invoke } from '@tauri-apps/api/tauri';
 import { SquaredButton } from '@app/components/elements/buttons/SquaredButton';
 import { DialogContent, Dialog } from '@app/components/elements/dialog/Dialog';
-import { useAppStateStore } from '@app/store/appStateStore';
 import { Typography } from '@app/components/elements/Typography';
 import { ButtonsWrapper } from './AutoUpdateDialog.styles';
-import { useUpdateStatus } from '@app/hooks/useUpdateStatus';
+import { useHandleUpdate, useUpdateStatus } from '@app/hooks/useUpdateStatus';
 import { UpdatedStatus } from './UpdatedStatus';
-import { useInterval } from '@app/hooks/useInterval.ts';
-import { useAppConfigStore } from '@app/store/useAppConfigStore';
 
-const UPDATE_CHECK_INTERVAL = 1000 * 60 * 60; // 1 hour
-function AutoUpdateDialog() {
-    const setIsAfterAutoUpdate = useAppStateStore((s) => s.setIsAfterAutoUpdate);
-    const auto_update = useAppConfigStore((s) => s.auto_update);
-    const [latestVersion, setLatestVersion] = useState<string>();
-    const [isLoading, setIsLoading] = useState(false);
-    const [open, setOpen] = useState(false);
-    const { contentLength, downloaded } = useUpdateStatus();
+export default function AutoUpdateDialog() {
     const { t } = useTranslation('setup-view', { useSuspense: false });
+    const { handleUpdate, handleClose, isLoading } = useHandleUpdate();
+    const { contentLength, downloaded } = useUpdateStatus();
 
-    const handleClose = useCallback(() => {
-        setOpen(false);
-        setIsAfterAutoUpdate(true);
-    }, [setIsAfterAutoUpdate]);
-
-    const handleUpdate = useCallback(async () => {
-        setIsLoading(true);
-        await installUpdate();
-        console.info('Installing latest version of Tari Universe');
-        try {
-            console.info('Restarting application after update');
-            await invoke('restart_application');
-        } catch (e) {
-            Sentry.captureException(e);
-            console.error('Relaunch error', e);
-        }
-        handleClose();
-    }, [handleClose]);
-
-    const checkUpdateTariUniverse = useCallback(async () => {
-        if (isLoading) return;
-        try {
-            const { shouldUpdate, manifest } = await checkUpdate();
-            if (shouldUpdate) {
-                console.info('New Tari Universe version available', manifest);
-                setLatestVersion(manifest?.version);
-                if (auto_update) {
-                    console.info('Proceed with auto-update');
-                    await handleUpdate();
-                }
-                setOpen(true);
-            } else {
-                setIsAfterAutoUpdate(true);
-            }
-        } catch (error) {
-            Sentry.captureException(error);
-            console.error('AutoUpdate error:', error);
-            setIsAfterAutoUpdate(true);
-        }
-    }, [auto_update, handleUpdate, isLoading, setIsAfterAutoUpdate]);
-
-    useInterval(() => checkUpdateTariUniverse(), UPDATE_CHECK_INTERVAL);
-
-    const onOpenChange = (open: boolean) => {
-        if (!open) {
-            setIsAfterAutoUpdate(true);
-        }
-        setOpen(open);
-    };
-
-    useEffect(() => {
-        checkUpdateTariUniverse();
-        const unlistenPromise = onUpdaterEvent(({ error, status }) => {
-            // This will log all updater events, including status updates and errors.
-            console.info('Updater event', error, status);
-        });
-        return () => {
-            unlistenPromise?.then((unlisten) => unlisten());
-        };
-    }, [checkUpdateTariUniverse]);
+    const open = useUIStore((s) => s.dialogToShow === 'autoUpdate');
+    const latestVersion = useUIStore((s) => s.latestVersion);
 
     const subtitle = isLoading ? 'installing-latest-version' : 'would-you-like-to-install';
 
     return (
-        <Dialog open={open} onOpenChange={onOpenChange}>
+        <Dialog open={open} onOpenChange={handleClose} disableClose>
             <DialogContent>
                 <Typography variant="h3">{t('new-tari-version-available')}</Typography>
                 <Typography variant="p">{t(subtitle, { version: latestVersion })}</Typography>
@@ -98,7 +28,7 @@ function AutoUpdateDialog() {
                 <ButtonsWrapper>
                     {!isLoading && (
                         <>
-                            <SquaredButton onClick={handleClose} color="warning">
+                            <SquaredButton onClick={() => handleClose()} color="warning">
                                 {t('no')}
                             </SquaredButton>
                             <SquaredButton onClick={() => handleUpdate()} color="green">
@@ -111,5 +41,3 @@ function AutoUpdateDialog() {
         </Dialog>
     );
 }
-
-export default AutoUpdateDialog;

commit 865c4ae3ccb913dbdd70802e495f55cd58cd5854

fix: don't set setup progress back to 0 (#1059)
### fixes the bug i introduced in #1057 

basically those changes made it so we never fetched wallet details 

- only use `setSetupDetails` if progress is greater than 0 (since the
fetch wallet history call only worked on progress > 0.75 and after setup
completed progress went back to 0)
- made the block height check stricter (check that both are not 0)

src/hooks/useMiningMetricsUpdater.ts
@@ -12,6 +12,8 @@ export default function useMiningMetricsUpdater() {
     const baseNodeConnected = useMiningStore((s) => s.base_node.is_connected);
     const setMiningMetrics = useMiningStore((s) => s.setMiningMetrics);
     const handleNewBlock = useBlockchainVisualisationStore((s) => s.handleNewBlock);
+    const displayBlockHeight = useBlockchainVisualisationStore((s) => s.displayBlockHeight);
+    const setDisplayBlockHeight = useBlockchainVisualisationStore((s) => s.setDisplayBlockHeight);
     const [isFetchingMetrics, setIsFetchingMetrics] = useState(false);
 
     return useCallback(async () => {
@@ -30,10 +32,12 @@ export default function useMiningMetricsUpdater() {
                 }
 
                 const blockHeight = metrics.base_node.block_height;
-                if (blockHeight && blockHeight > currentBlockHeight) {
+                if (blockHeight > 0 && currentBlockHeight > 0 && blockHeight > currentBlockHeight) {
                     await fetchTx();
-                    if (blockHeight > 0) {
-                        await handleNewBlock(blockHeight, isMining);
+                    await handleNewBlock(blockHeight, isMining);
+                } else {
+                    if (blockHeight && !displayBlockHeight) {
+                        setDisplayBlockHeight(blockHeight);
                     }
                 }
                 setMiningMetrics(metrics);
@@ -44,5 +48,14 @@ export default function useMiningMetricsUpdater() {
             console.error('Fetch mining metrics error: ', e);
             setIsFetchingMetrics(false);
         }
-    }, [baseNodeConnected, currentBlockHeight, fetchTx, handleNewBlock, isFetchingMetrics, setMiningMetrics]);
+    }, [
+        baseNodeConnected,
+        currentBlockHeight,
+        displayBlockHeight,
+        fetchTx,
+        handleNewBlock,
+        isFetchingMetrics,
+        setDisplayBlockHeight,
+        setMiningMetrics,
+    ]);
 }

src/hooks/useSetUp.ts
@@ -46,7 +46,9 @@ export function useSetUp() {
         const unlistenPromise = listen('message', ({ event: e, payload: p }: TauriEvent) => {
             switch (p.event_type) {
                 case 'setup_status':
-                    setSetupDetails(p.title, p.title_params, p.progress);
+                    if (p.progress > 0) {
+                        setSetupDetails(p.title, p.title_params, p.progress);
+                    }
                     if (p.progress >= 1) {
                         handlePostSetup().finally(() => setSeenPermissions(true));
                     }