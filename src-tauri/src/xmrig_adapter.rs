5e1a09bc6335671a42816ba61301b7b6a68f9da8
Misieq01 <38589417+Misieq01@users.noreply.github.com>
2024-11-12 14:54:14 +0100

    fix: conflicting ports (#1039)
    ### [ Content ]
    - Implemented PortAllocator that ensures allocated port is free
    
    ---------
    
    Co-authored-by: Brian Pearce <brianp@users.noreply.github.com>

--- src-tauri/src/xmrig_adapter.rs
+++ src-tauri/src/xmrig_adapter.rs
@@ -4,7 +4,7 @@ use log::warn;
 use std::path::PathBuf;
 use tari_shutdown::Shutdown;
 
-use crate::network_utils::get_free_port;
+use crate::port_allocator::PortAllocator;
 use crate::process_adapter::{
     HealthStatus, ProcessAdapter, ProcessInstance, ProcessStartupSpec, StatusMonitor,
 };
@@ -43,7 +43,7 @@ pub struct XmrigAdapter {
 
 impl XmrigAdapter {
     pub fn new() -> Self {
-        let http_api_port = get_free_port().unwrap_or(18000);
+        let http_api_port = PortAllocator::new().assign_port_with_fallback();
         let http_api_token = "pass".to_string();
         Self {
             node_connection: None,
