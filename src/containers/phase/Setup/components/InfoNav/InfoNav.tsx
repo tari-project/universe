a242d3948de385d283ea869c2ed496e201623768
Aaron Pepper <pepdog@gmail.com>
2024-11-14 20:38:14 +0800

    feat(ui): change setup screen slides based on reading speed of text (#1064)
    This PR adds a function to calculate the reading speed of each slide on
    the setup screens based on text length.
    
    So each slide now has a variable animation speed before it switches to
    the next slide. I decided to use a function for this instead of hard
    coding so it an adapt to different languages.
    
    To do this calculation, I had to move the text outside of the `InfoItem`
    component to the parent and lass the title and text down as props.
    
    
    https://www.loom.com/share/df378764a9ac4d10b5fe2ab4a85d2616?sid=cdabc628-7b6d-4e0d-81b4-ad1b4746dd43

--- src/containers/phase/Setup/components/InfoNav/InfoNav.tsx
+++ src/containers/phase/Setup/components/InfoNav/InfoNav.tsx
@@ -4,11 +4,22 @@ import { AnimatePresence } from 'framer-motion';
 import { useCallback, useState } from 'react';
 import InfoItem from './InfoItem';
 import { Nav, NavContainer, NavItem, NavItemCurrent } from './InfoNav.styles';
+import { useTranslation } from 'react-i18next';
 
-const STEP_TIME_SECONDS = 9;
 const steps = Array.from({ length: 6 }, (_, i) => i + 1);
 
+const emojis = {
+    'step-1': ['💜', '🐢'],
+    'step-6': ['🙏'],
+};
+
+const calculateReadingTime = (text) => {
+    const words = text.split(' ').length;
+    return (words / 350) * 60 + 3; // Convert to seconds + 3s for a pause
+};
+
 export default function InfoNav() {
+    const { t } = useTranslation('info');
     const [currentStep, setCurrentStep] = useState(steps[0]);
 
     const handleStepClick = useCallback((newStep: number) => {
@@ -25,9 +36,20 @@ export default function InfoNav() {
         });
     };
 
+    const stepEmojis = emojis[`step-${currentStep}`];
+
+    const emojiParams = {};
+    if (stepEmojis?.length) {
+        stepEmojis.forEach((e: string, i: number) => (emojiParams[`emoji${i > 0 ? i : ''}`] = e));
+    }
+
+    const title = t(`heading.step-${currentStep}`);
+    const text = t(`content.step-${currentStep}`, { ...emojiParams });
+
     const sliderMarkup = steps?.map((step) => {
         const key = `nav-item-${step}`;
         const isSelected = currentStep === step;
+        const duration = calculateReadingTime(title + text);
 
         return (
             <NavItem key={key} $selected={isSelected} onClick={() => handleStepClick(step)}>
@@ -36,7 +58,7 @@ export default function InfoNav() {
                     <NavItemCurrent layoutId="selected" key={`selected:${key}`}>
                         <LinearProgress
                             value={100}
-                            duration={STEP_TIME_SECONDS}
+                            duration={duration}
                             variant="tiny"
                             onAnimationComplete={handleNextStep}
                         />
@@ -49,7 +71,7 @@ export default function InfoNav() {
     return (
         <NavContainer>
             <AnimatePresence mode="wait">
-                <InfoItem key={`step-${currentStep}-content-wrapper`} step={currentStep} />
+                <InfoItem key={`step-${currentStep}-content-wrapper`} title={title} text={text} />
             </AnimatePresence>
             <AnimatePresence>
                 <InfoItemGraphic key={`step-${currentStep}-graphics-wrapper`} step={currentStep} />
