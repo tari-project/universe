29eee291491cf9609ca20a4cfc65d54d9059054f
Aaron Pepper <pepdog@gmail.com>
2024-11-14 16:43:41 +0800

    lint fix

--- src/containers/phase/Setup/components/InfoNav/InfoNav.styles.ts
+++ src/containers/phase/Setup/components/InfoNav/InfoNav.styles.ts
@@ -1,5 +1,5 @@
 import { m } from 'framer-motion';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 export const Container = styled.div`
     display: flex;
