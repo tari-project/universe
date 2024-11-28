1063b48caa42c78cc30b9a3391cb706b5ee8c291
Aaron Pepper <pepdog@gmail.com>
2024-11-28 17:02:18 +0700

    add store and widget toggle

--- src/containers/main/ShellOfSecrets/ShellOfSecrets.tsx
+++ src/containers/main/ShellOfSecrets/ShellOfSecrets.tsx
@@ -1,9 +1,13 @@
 import SoSWidget from './SoSWidget/SoSWidget';
+import { useShellOfSecretsStore } from '../../../store/useShellOfSecretsStore';
+import { AnimatePresence } from 'framer-motion';
 
 export default function ShellOfSecrets() {
+    const { showWidget } = useShellOfSecretsStore();
+
     return (
         <>
-            <SoSWidget />
+            <AnimatePresence>{showWidget && <SoSWidget />}</AnimatePresence>
         </>
     );
 }
