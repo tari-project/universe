5b58ab8aa419fa354ac7ef191899bdbb8dc76835
Marcin Papież <mmpapiez@gmail.com>
2024-11-29 09:09:33 +0100

    make fn public

--- src-tauri/src/utils/macos_utils.rs
+++ src-tauri/src/utils/macos_utils.rs
@@ -4,7 +4,7 @@ use std::env;
 use std::path::Path;
 
 #[cfg(target_os = "macos")]
-fn is_app_in_applications_folder() -> bool {
+pub fn is_app_in_applications_folder() -> bool {
     let current_exe = env::current_exe().unwrap();
     let applications_folder = Path::new("/Applications");
     current_exe.starts_with(applications_folder)
