8fbaa50df30869b8b0a54bf603682d9e6cb38015
Sevecsek Balázs <balazs.sevecsek@gmail.com>
2024-12-02 14:08:05 +0100

    feat: improve websocket handling (#1151)
    Co-authored-by: Juan <47256233+PanchoBubble@users.noreply.github.com>
    Co-authored-by: Aaron Pepper <pepdog@gmail.com>

---
+++ src/containers/main/ShellOfSecrets/components/Scanlines/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Canvas = styled('canvas')`
+    position: absolute;
+    inset: 0;
+    pointer-events: none;
+    z-index: 2;
+    width: 100%;
+    height: 100%;
+`;
+
+export const Vignette = styled.div`
+    position: absolute;
+    inset: 0;
+    pointer-events: none;
+    z-index: 1;
+    background: radial-gradient(circle at center, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.5) 100%);
+    mix-blend-mode: multiply;
+`;
--- src/hooks/airdrop/useWebsocket.ts
+++ src/hooks/airdrop/useWebsocket.ts
@@ -1,49 +1,118 @@
 import { io } from 'socket.io-client';
 import { useAirdropStore } from '@app/store/useAirdropStore.ts';
-import { QuestCompletedEvent } from '@app/types/ws';
-import { useEffect } from 'react';
+import { WebsocketEventNames, WebsocketUserEvent } from '@app/types/ws';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useMiningStore } from '@app/store/useMiningStore';
+import { useAppConfigStore } from '@app/store/useAppConfigStore';
+import { useShellOfSecretsStore } from '@app/store/useShellOfSecretsStore';
 
 let socket: ReturnType<typeof io> | null;
+
+const MINING_EVENT_INTERVAL = 15000;
+const MINING_EVENT_NAME = 'mining-status';
 
 export const useWebsocket = () => {
     const airdropToken = useAirdropStore((state) => state.airdropTokens?.token);
     const userId = useAirdropStore((state) => state.userDetails?.user?.id);
     const setUserGems = useAirdropStore((state) => state.setUserGems);
+    const setTotalBonusTimeMs = useShellOfSecretsStore((state) => state.setTotalBonusTimeMs);
     const baseUrl = useAirdropStore((state) => state.backendInMemoryConfig?.airdropApiUrl);
+    const cpu = useMiningStore((state) => state.cpu);
+    const gpu = useMiningStore((state) => state.gpu);
+    const network = useMiningStore((state) => state.network);
+    const appId = useAppConfigStore((state) => state.anon_id);
+    const base_node = useMiningStore((state) => state.base_node);
+    const [connectedSocket, setConnectedSocket] = useState(false);
+
+    const isMining = useMemo(() => {
+        const isMining = (cpu?.mining.is_mining || gpu?.mining.is_mining) && base_node?.is_connected;
+        return isMining;
+    }, [base_node?.is_connected, cpu?.mining.is_mining, gpu?.mining.is_mining]);
+
+    const handleEmitMiningStatus = useCallback(
+        (isMining: boolean) => {
+            if (!socket || !connectedSocket) return;
+            const arg = { isMining, timestamp: new Date().toISOString() };
+            try {
+                socket.emit(MINING_EVENT_NAME, arg);
+            } catch (e) {
+                console.error(e);
+            }
+        },
+        [connectedSocket]
+    );
+
+    useEffect(() => {
+        if (isMining) {
+            const intervalId = setInterval(() => {
+                handleEmitMiningStatus(isMining);
+            }, MINING_EVENT_INTERVAL);
+            return () => clearInterval(intervalId);
+        } else {
+            handleEmitMiningStatus(isMining);
+        }
+    }, [baseUrl, handleEmitMiningStatus, network, isMining]);
 
     const init = () => {
-        if (!socket && baseUrl) {
-            socket = io(baseUrl, { secure: true, transports: ['websocket', 'polling'] });
-        }
+        try {
+            if (!socket && baseUrl) {
+                socket = io(baseUrl, {
+                    secure: true,
+                    transports: ['websocket', 'polling'],
+                    auth: { token: airdropToken, appId: appId, network: network },
+                });
+            }
 
-        if (!socket) return;
-
-        socket.on('connect', () => {
             if (!socket) return;
-            socket.emit('auth', airdropToken);
-            socket.on(userId as string, (msg: string) => {
-                const msgParsed = JSON.parse(msg) as QuestCompletedEvent;
-                if (msgParsed?.data?.userPoints?.gems) {
-                    setUserGems(msgParsed.data.userPoints?.gems);
-                }
+
+            socket.emit('subscribe-to-gem-updates');
+            socket.on('connect', () => {
+                if (!socket) return;
+                setConnectedSocket(true);
+                socket.emit('auth', airdropToken);
+                socket.on(userId as string, (msg: string) => {
+                    const msgParsed = JSON.parse(msg) as WebsocketUserEvent;
+                    if (msgParsed.name === WebsocketEventNames.COMPLETED_QUEST && msgParsed?.data?.userPoints?.gems) {
+                        setUserGems(msgParsed.data.userPoints?.gems);
+                    }
+                    if (
+                        (msgParsed.name === WebsocketEventNames.MINING_STATUS_CREW_UPDATE ||
+                            msgParsed.name === WebsocketEventNames.MINING_STATUS_USER_UPDATE) &&
+                        msgParsed?.data?.totalTimeBonusMs
+                    ) {
+                        setTotalBonusTimeMs(msgParsed.data.totalTimeBonusMs);
+                    }
+                });
             });
-        });
+        } catch (e) {
+            console.error(e);
+        }
     };
 
     const disconnect = () => {
-        if (socket) {
-            socket.disconnect();
-            socket = null;
+        try {
+            if (socket) {
+                setConnectedSocket(false);
+                handleEmitMiningStatus(isMining);
+                socket.disconnect();
+                socket = null;
+            }
+        } catch (e) {
+            console.error(e);
         }
     };
 
     useEffect(() => {
-        if (airdropToken && userId && baseUrl) {
-            init();
+        try {
+            if (airdropToken && userId && baseUrl) {
+                init();
+            }
+            return () => {
+                disconnect();
+            };
+        } catch (e) {
+            console.error(e);
         }
-        return () => {
-            disconnect();
-        };
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [airdropToken, userId, baseUrl]);
 
