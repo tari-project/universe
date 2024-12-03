065c94e31d84a8451e26a52c156dd3e311e30c8e
Marcin Papież <mmpapiez@gmail.com>
2024-12-03 14:57:27 +0100

    chore: p2pool stats page

--- src/containers/floating/Settings/sections/experimental/ExperimentalSettings.tsx
+++ src/containers/floating/Settings/sections/experimental/ExperimentalSettings.tsx
@@ -7,9 +7,11 @@ import ExperimentalWarning from './ExperimentalWarning.tsx';
 import { TorMarkup } from './TorMarkup';
 import MonerodMarkup from './MonerodMarkup';
 import { useAppConfigStore } from '@app/store/useAppConfigStore.ts';
+import P2poolMarkup from './P2poolMarkup.tsx';
 
 export const ExperimentalSettings = () => {
     const showExperimental = useAppConfigStore((s) => s.show_experimental_settings);
+    const isP2poolEnabled = useAppConfigStore((s) => s.p2pool_enabled);
 
     return (
         <>
@@ -21,7 +23,7 @@ export const ExperimentalSettings = () => {
                         <AppVersions />
                         <TorMarkup />
                         <MonerodMarkup />
-                        <br />
+                        {isP2poolEnabled && <P2poolMarkup />}
                         <VisualMode />
                     </>
                 )}
