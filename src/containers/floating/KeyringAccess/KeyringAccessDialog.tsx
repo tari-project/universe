4f55c7e584cfbd6badc960075a95cc9a0157e102
Shannon Tenner <shanimal@tari.com>
2024-12-10 17:25:51 +0200

    add undismissable banner in settings

--- src/containers/floating/KeyringAccess/KeyringAccessDialog.tsx
+++ src/containers/floating/KeyringAccess/KeyringAccessDialog.tsx
@@ -3,7 +3,8 @@ import { useUIStore } from '@app/store/useUIStore.ts';
 import { ButtonWrapper, Wrapper } from './KeyringAccessDialog.styles.ts';
 import { Typography } from '@app/components/elements/Typography.tsx';
 import { Button } from '@app/components/elements/buttons/Button.tsx';
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
+import { addToast } from '@app/components/ToastStack/useToastStore.tsx';
 
 export const KeyringAccessDialog = () => {
     const { t } = useTranslation(['wallet', 'common']);
@@ -13,18 +14,32 @@ export const KeyringAccessDialog = () => {
     function handleClose() {
         setDialogToShow(null);
     }
+
+    function handleKeychain() {
+        addToast({
+            title: 'Add to keychain',
+            text: 'BE stuff should happen now',
+        });
+    }
     return (
         <Dialog open={dialogToShow === 'keyring'}>
             <DialogContent $unPadded>
                 <Wrapper>
                     <Typography variant="h2">{t('keychain-access')}</Typography>
-                    <Typography>{t('keychain-access-copy')}</Typography>
+                    <Typography>
+                        <Trans
+                            i18nKey="wallet:keychain-access-copy"
+                            components={{
+                                span: <span />,
+                            }}
+                        />
+                    </Typography>
 
                     <ButtonWrapper>
                         <Button color="warning" onClick={() => handleClose()}>
                             {t('common:dismiss')}
                         </Button>
-                        <Button>{t('common:yes')}</Button>
+                        <Button onClick={() => handleKeychain()}>{t('common:yes')}</Button>
                     </ButtonWrapper>
                 </Wrapper>
             </DialogContent>
--- src/containers/floating/Settings/sections/wallet/WalletSettings.tsx
+++ src/containers/floating/Settings/sections/wallet/WalletSettings.tsx
@@ -3,11 +3,14 @@ import WalletAddressMarkup from './WalletAddressMarkup.tsx';
 import MoneroAddressMarkup from './MoneroAddressMarkup';
 import SeedWordsMarkup from './SeedWordsMarkup/SeedWordsMarkup.tsx';
 import MoneroSeedWordSettings from './MoneroSeedWords/MoneroSeedWordSettings.tsx';
+import { KeychainWarning } from '@app/containers/floating/Settings/sections/wallet/components/KeychainWarning.tsx';
 
 export const WalletSettings = () => {
     const monero_address_is_generated = useAppConfigStore((s) => s.monero_address_is_generated);
+    const seedword_not_in_keychain = useAppConfigStore((s) => s.keyring_fallback || !s.keyring_accessed);
     return (
         <>
+            {seedword_not_in_keychain && <KeychainWarning />}
             <WalletAddressMarkup />
             <SeedWordsMarkup />
             <MoneroAddressMarkup />
