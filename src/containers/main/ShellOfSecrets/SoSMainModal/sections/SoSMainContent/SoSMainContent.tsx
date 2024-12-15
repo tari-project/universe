f5663d7754443e0994f4cb0e5823f0ebf2fc255a
Aaron Pepper <pepdog@gmail.com>
2024-12-15 16:16:09 +0700

    added bottom design element

--- src/containers/main/ShellOfSecrets/SoSMainModal/sections/SoSMainContent/SoSMainContent.tsx
+++ src/containers/main/ShellOfSecrets/SoSMainModal/sections/SoSMainContent/SoSMainContent.tsx
@@ -2,6 +2,7 @@ import CrewMining from './CrewMining/CrewMining';
 import PrizeDrops from './PrizeDrops/PrizeDrops';
 import Leaderboard from './Leaderboard/Leaderboard';
 import { Wrapper } from './styles';
+import BottomElement from './BottomElement/BottomElement';
 
 export default function SoSMainContent() {
     return (
@@ -9,6 +10,7 @@ export default function SoSMainContent() {
             <CrewMining />
             <Leaderboard />
             <PrizeDrops />
+            <BottomElement />
         </Wrapper>
     );
 }
