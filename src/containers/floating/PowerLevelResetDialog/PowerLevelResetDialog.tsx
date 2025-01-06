ed4418f2f9d58e30119783395fbca1c6d5c56b31
Aaron Pepper <pepdog@gmail.com>
2025-01-06 15:17:49 -0500

    integrated i18n and hooked up buttons

--- src/containers/floating/PowerLevelResetDialog/PowerLevelResetDialog.tsx
+++ src/containers/floating/PowerLevelResetDialog/PowerLevelResetDialog.tsx
@@ -1,10 +1,10 @@
-/* eslint-disable i18next/no-literal-string */
-
 import { useUIStore } from '@app/store/useUIStore';
 
 import { DialogContent, Dialog } from '@app/components/elements/dialog/Dialog';
 import { useCallback, useEffect, useState } from 'react';
 import { ButtonWrapper, CountdownNumber, KeepButton, RevertButton, Text, TextWrapper, Title, Wrapper } from './styles';
+import { useTranslation } from 'react-i18next';
+import { useMiningStore } from '@app/store/useMiningStore';
 
 function Countdown({ onComplete }: { onComplete: () => void }) {
     const [count, setCount] = useState(30);
@@ -22,8 +22,10 @@ function Countdown({ onComplete }: { onComplete: () => void }) {
 }
 
 export default function PowerLevelResetDialog() {
+    const { t } = useTranslation('components', { useSuspense: false });
     const open = useUIStore((s) => s.dialogToShow === 'powerLevelReset');
     const setDialogToShow = useUIStore((s) => s.setDialogToShow);
+    const changeMiningMode = useMiningStore((s) => s.changeMiningMode);
 
     const handleClose = useCallback(() => {
         setDialogToShow(null);
@@ -34,23 +36,26 @@ export default function PowerLevelResetDialog() {
         setDialogToShow(null);
     }, [setDialogToShow]);
 
+    const handleChange = useCallback(async () => {
+        await changeMiningMode({ mode: 'Ludicrous' });
+    }, [changeMiningMode]);
+
     return (
         <Dialog open={open} onOpenChange={handleClose}>
             <DialogContent>
                 <Wrapper>
                     <TextWrapper>
-                        <Title>Stay in Ludicrous Mode?</Title>
+                        <Title>{t('powerLevelResetDialog.title')}</Title>
                         <Text>
-                            Ludicrous mode may make your system unresponsive. Please confirm you want to start mining in
-                            Ludicrous mode or Tari Universe will switch back to Eco mode in{' '}
-                            <Countdown onComplete={handleCountdownComplete} /> seconds.
+                            {t('powerLevelResetDialog.description')} <Countdown onComplete={handleCountdownComplete} />{' '}
+                            {t('powerLevelResetDialog.seconds')}.
                         </Text>
                     </TextWrapper>
                     <ButtonWrapper>
-                        <KeepButton>
-                            <span>🔥</span> Keep Changes
+                        <KeepButton onClick={handleChange}>
+                            <span>🔥</span> {t('powerLevelResetDialog.keepChanges')}
                         </KeepButton>
-                        <RevertButton>Revert to ECO</RevertButton>
+                        <RevertButton onClick={handleClose}>{t('powerLevelResetDialog.revertToEco')}</RevertButton>
                     </ButtonWrapper>
                 </Wrapper>
             </DialogContent>
