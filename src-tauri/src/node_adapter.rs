c27c3578140d4aa1147837cd26cd11114173326d
Marcin Papież <mmpapiez@gmail.com>
2025-01-08 11:45:03 +0100

    fix: require initial connected peer for wait synced

--- src-tauri/src/node_adapter.rs
+++ src-tauri/src/node_adapter.rs
@@ -468,7 +468,7 @@ impl MinotariNodeStatusMonitor {
                 .map_err(|e| MinotariNodeStatusMonitorError::UnknownError(e.into()))?;
             let tip_res = tip.into_inner();
             let sync_progress = sync_progress.into_inner();
-            if tip_res.initial_sync_achieved {
+            if tip_res.initial_sync_achieved && sync_progress.initial_connected_peers > 0 {
                 break Ok(());
             }
 
