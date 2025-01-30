98318cc0155e6532539f8d652301be07767971b4
stringhandler <stringhandler@protonmail.com>
2025-01-30 10:37:16 +0200

    fixes

--- src-tauri/src/cpu_miner.rs
+++ src-tauri/src/cpu_miner.rs
@@ -142,35 +142,14 @@ impl CpuMiner {
         let xmrig_node_connection = XmrigNodeConnection::Benchmark;
         let max_cpu_available = thread::available_parallelism();
         let max_cpu_available = match max_cpu_available {
-            Ok(available_cpus) => {
-                debug!(target:LOG_TARGET, "Available CPUs: {}", available_cpus);
-                u32::try_from(available_cpus.get()).unwrap_or(1)
-            }
-            Err(err) => {
-                error!("Available CPUs: Unknown, error: {}", err);
-                1
-            }
+            Ok(available_cpus) => u32::try_from(available_cpus.get()).unwrap_or(1),
+            Err(err) => 1,
         };
 
-        // let eco_mode_threads = cpu_miner_config
-        // .eco_mode_cpu_percentage
-        // .unwrap_or((ECO_MODE_CPU_USAGE * max_cpu_available) / 100u32);
-
-        // let cpu_max_percentage = match mode {
-        // MiningMode::Eco => Some(eco_mode_threads),
-        // MiningMode::Custom => {
-        // if custom_cpu_threads.unwrap_or(0) == max_cpu_available {
-        // None
-        // } else {
-        // custom_cpu_threads
-        // }
-        // }
-        // MiningMode::Ludicrous => None,
-        // };
-
         lock.adapter.node_connection = Some(xmrig_node_connection);
+        // We're going to use benchmarking, so the address isn't used
         lock.adapter.monero_address = Some("44AFFq5kSiGBoZ4NMDwYtN18obc8AemS33DBLWs3H7otXft3XjrpDtQGv7SqSsaBYBb98uNbr2VBBEt7f2wfn3RVGQBEP3A".to_string());
-        lock.adapter.cpu_threads = Some(Some(1)); // Ludicrous mode
+        lock.adapter.cpu_threads = Some(Some(1)); // Use one thread so that the machine doesn't lock up
         lock.adapter.extra_options = vec![];
 
         let timeout_duration = duration + Duration::from_secs(10);
@@ -192,19 +171,13 @@ impl CpuMiner {
                 }
                 sleep(Duration::from_secs(1)).await;
                 if let Ok(stats) = status.summary().await {
-                    dbg!(&stats);
                     let hash_rate = stats.hashrate.total[0].unwrap_or_default();
                     if hash_rate > max_hashrate {
                         max_hashrate = hash_rate;
                     }
-                    info!(target: LOG_TARGET, "Xmrig stats available");
-                    dbg!(start_time.elapsed());
-                    dbg!(duration);
                     if start_time.elapsed() > duration {
                         break;
                     }
-                } else {
-                    warn!(target: LOG_TARGET, "Xmrig stats not available yet");
                 }
             } // wait until we have stats from xmrig, so its started
             Ok::<u64, anyhow::Error>(max_hashrate as u64)
@@ -212,7 +185,7 @@ impl CpuMiner {
         .await
         {
             Ok(res) => Ok(res? * max_cpu_available as u64),
-            Err(_) => return Err(anyhow::anyhow!("Benchmarking timed out")),
+            Err(_) => Ok(0),
         };
         let mut lock2 = self.watcher.write().await;
         lock2.stop().await?;
