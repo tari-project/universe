dde9889822f8f50190cd32147af1a27acba8465f
Maciej Kozuszek <mackozuszek@gmail.com>
2025-02-03 11:02:32 +0100

    Remove shutdown handling from frontend

--- src/hooks/app/useShuttingDown.ts
+++ src/hooks/app/useShuttingDown.ts
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import { resetAllStores } from '@app/store/create.ts';
-import { invoke } from '@tauri-apps/api/core';
 import { getCurrentWindow } from '@tauri-apps/api/window';
 const appWindow = getCurrentWindow();
 
@@ -8,10 +7,9 @@ export function useShuttingDown() {
     const [isShuttingDown, setIsShuttingDown] = useState(false);
 
     useEffect(() => {
-        const ul = appWindow.onCloseRequested(async (event) => {
+        const ul = appWindow.onCloseRequested(async () => {
             if (!isShuttingDown) {
                 setIsShuttingDown(true);
-                event.preventDefault();
             }
         });
         return () => {
@@ -23,7 +21,6 @@ export function useShuttingDown() {
         if (isShuttingDown) {
             setTimeout(async () => {
                 resetAllStores();
-                await invoke('exit_application');
             }, 250);
         }
     }, [isShuttingDown]);
