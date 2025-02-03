7e971f770ae278406e9ee6d93a021046f6d1103a
Aaron Pepper <pepdog@gmail.com>
2025-02-03 11:45:47 -0500

    updated app version pill design

--- src/containers/phase/Setup/components/Footer.tsx
+++ src/containers/phase/Setup/components/Footer.tsx
@@ -5,6 +5,7 @@ import { useAppConfigStore } from '@app/store/useAppConfigStore';
 import { Container, LottieWrapper, StatusWrapper } from './Footer.styles';
 import animationData from './lil-soon-cookies.json';
 import AirdropPermission from './AirdropPermission/AirdropPermission';
+import AppVersion from './AppVersion/AppVersion';
 
 export default function Footer() {
     const created_at = useAppConfigStore((s) => s.created_at);
@@ -22,7 +23,7 @@ export default function Footer() {
                 </LottieWrapper>
                 <SetupProgress />
             </StatusWrapper>
-            {isFirstLoad ? <AirdropPermission /> : null}
+            {isFirstLoad ? <AirdropPermission /> : <AppVersion />}
         </Container>
     );
 }
