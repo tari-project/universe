97030403aee64b854d2e9035ec9cab2304ac8d4b
Aaron Pepper <pepdog@gmail.com>
2025-02-05 17:22:48 -0500

    optimize hero text

--- src/containers/phase/Setup/components/HeroText.tsx
+++ src/containers/phase/Setup/components/HeroText.tsx
@@ -2,49 +2,11 @@ import { useTranslation } from 'react-i18next';
 import styled, { css } from 'styled-components';
 import { memo } from 'react';
 
-const TEXT_STYLE_BASE = css`
-    font-family: Druk, sans-serif;
-    font-size: calc(5rem + 15vmin);
-    font-weight: 700;
-    line-height: 0.8;
-    text-transform: uppercase;
-    white-space: nowrap;
+const GridReference = styled.div`
+    grid-area: hero;
     position: relative;
-    width: min-content;
-    user-select: none;
-`;
-const SetupTextMain = styled.div`
-    color: ${({ theme }) => theme.palette.text.contrast};
-    background: ${({ theme }) => theme.palette.text.contrast};
-    background-clip: text;
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-
-    ${({ theme }) => {
-        if (theme.mode === 'dark') {
-            return css`
-                background: -webkit-linear-gradient(#eee, #333);
-                -webkit-background-clip: text;
-                -webkit-text-fill-color: transparent;
-            `;
-        }
-    }}
-    ${TEXT_STYLE_BASE}
-`;
-
-const SetupTextGhost = styled.div`
-    color: transparent;
-    -webkit-text-fill-color: transparent;
-    -webkit-text-stroke: 2px rgba(255, 255, 255, 0.15);
-
-    ${({ theme }) => {
-        if (theme.mode === 'dark') {
-            return css`
-                -webkit-text-stroke: 2px rgba(255, 255, 255, 0.035);
-            `;
-        }
-    }}
-    ${TEXT_STYLE_BASE}
+    min-height: 160px;
+    z-index: 1;
 `;
 
 const TextWrapper = styled.div`
@@ -57,13 +19,48 @@ const TextWrapper = styled.div`
     left: 0;
     user-select: none;
     pointer-events: none;
-`;
 
-const GridReference = styled.div`
-    grid-area: hero;
+    font-family: Druk, sans-serif;
+    font-size: calc(5rem + 15vmin);
+    font-weight: 700;
+    line-height: 0.8;
+    text-transform: uppercase;
+    white-space: nowrap;
     position: relative;
-    min-height: 160px;
-    z-index: 1;
+    width: min-content;
+    user-select: none;
+
+    .title-main {
+        color: ${({ theme }) => theme.palette.text.contrast};
+        background: ${({ theme }) => theme.palette.text.contrast};
+        background-clip: text;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+
+        ${({ theme }) => {
+            if (theme.mode === 'dark') {
+                return css`
+                    background: -webkit-linear-gradient(#eee, #333);
+                    -webkit-background-clip: text;
+                    -webkit-text-fill-color: transparent;
+                `;
+            }
+        }}
+    }
+
+    .title-ghost {
+        color: transparent;
+        -webkit-text-fill-color: transparent;
+        -webkit-text-stroke: 2px rgba(255, 255, 255, 0.15);
+
+        ${({ theme }) => {
+            if (theme.mode === 'dark') {
+                return css`
+                    -webkit-text-stroke: 2px rgba(255, 255, 255, 0.035);
+                `;
+            }
+        }}
+    }
 `;
 
 const BEFORE_GHOSTS = Array(3).fill(null);
@@ -71,15 +68,22 @@ const AFTER_GHOSTS = Array(7).fill(null);
 
 const HeroText = memo(function HeroText() {
     const { t } = useTranslation('common');
+
+    const title = t('tari-universe');
+
     return (
         <GridReference>
             <TextWrapper>
                 {BEFORE_GHOSTS.map((_, i) => (
-                    <SetupTextGhost key={`ghost-before-${i}`}>{t('tari-universe')}</SetupTextGhost>
+                    <div className="title-ghost" key={`ghost-before-${i}`}>
+                        {title}
+                    </div>
                 ))}
-                <SetupTextMain>{t('tari-universe')}</SetupTextMain>
+                <div className="title-main">{title}</div>
                 {AFTER_GHOSTS.map((_, i) => (
-                    <SetupTextGhost key={`ghost-after-${i}`}>{t('tari-universe')}</SetupTextGhost>
+                    <div className="title-ghost" key={`ghost-after-${i}`}>
+                        {title}
+                    </div>
                 ))}
             </TextWrapper>
         </GridReference>
