7d657e34b3c541077b65ab62a21119e081068a5d
Aaron Pepper <pepdog@gmail.com>
2025-02-07 04:33:48 -0500

    fix(ui): setup screen performance optimization (#1488)
    This PR optimizes the Setup screen animations and markup. The goal is to
    reduce the memory impact of the Setup screen by replacing `motion/react`
    with native CSS animation ( without much loss in polish / quality )
    
    Before: 400MB to 1000MB 
    
    Now: 370MB to 530MB
    
    The memory seems to be a lot more stable after these updates, most of
    the time stays in the 400MB range.
    
    - Replace `motion/react` animations with CSS animation where possible
    - Rebuild steps navigation to use plain CSS, no longer uses
    `LinearProgress` component
    - Text animation on the steps now animates by word instead of by
    character
    - Change background "TARI UNIVERSE" text elements to use base tags
    instead of multiple `styled-components`
    - Various Small animation tweaks and layout changes to add polish
    
    ![CleanShot 2025-02-06 at 12 02
    10@2x](https://github.com/user-attachments/assets/33a5ddec-4a90-46bd-8e57-201e4f95717a)
    
    Note: i'm still using `motion/react` just for simple fade out exit
    `AnimatePresence` animations, if we remove those there will be even more
    performance gain... Exit animations are tricky with CSS and would
    require a more drastic restructure... maybe in the future.

--- src/containers/phase/Setup/components/InfoNav/InfoNav.styles.ts
+++ src/containers/phase/Setup/components/InfoNav/InfoNav.styles.ts
@@ -1,7 +1,38 @@
-import * as m from 'motion/react-m';
-import styled from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
+import { m } from 'motion/react';
 
-export const Container = styled.div`
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(8px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+const slideIn = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(50px) scale(1.1);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0) scale(1);
+  }
+`;
+
+const widthIn = keyframes`
+  from {
+    width: 0%;
+  }
+  to {
+    width: 100%;
+  }
+`;
+
+export const Container = styled(m.div)`
     display: flex;
     flex-direction: column;
     gap: 16px;
@@ -9,7 +40,7 @@ export const Container = styled.div`
     z-index: 2;
 `;
 
-export const Heading = styled.div`
+export const Heading = styled.div<{ $step: number }>`
     font-weight: 700;
     font-size: calc(2.6rem + 1vmin);
     color: ${({ theme }) => theme.palette.text.primary};
@@ -17,19 +48,40 @@ export const Heading = styled.div`
     line-height: 1;
     letter-spacing: -2px;
     white-space: pre-line;
+
+    ${({ $step }) => {
+        if ($step === 1) {
+            return css`
+                max-width: 410px;
+            `;
+        }
+    }}
 `;
 
 export const Copy = styled.div`
     font-size: min(calc(1rem + 1vmin), 30px);
     line-height: 1.2;
     letter-spacing: -0.8px;
+    max-width: 520px;
 `;
 
-export const AnimatedTextContainer = styled(m.div)`
+export const AnimatedTextContainer = styled.div`
     height: max-content;
+    opacity: 0;
+    animation: ${fadeIn} 0.5s cubic-bezier(0.2, 0.9, 0.3, 1) forwards;
+    will-change: transform, opacity;
+
     span {
-        white-space: break-spaces;
+        white-space: pre;
+        display: inline-block;
+        opacity: 0;
+        animation: ${fadeIn} 0.5s cubic-bezier(0.2, 0.9, 0.3, 1) forwards;
+        will-change: transform, opacity;
     }
+`;
+
+export const AnimatedSpan = styled.span<{ $index: number }>`
+    animation-delay: ${({ $index }) => $index * 0.025}s !important;
 `;
 
 export const NavContainer = styled.div`
@@ -42,35 +94,43 @@ export const NavContainer = styled.div`
 
 export const Nav = styled.div`
     display: flex;
+    align-items: center;
     gap: 5px;
     position: relative;
     z-index: 2;
+    height: 10px;
 `;
 
-export const NavItem = styled.div<{ $selected?: boolean }>`
+export const NavItem = styled.div`
     border-radius: 50px;
     position: relative;
     display: flex;
     width: 70px;
-    height: 4px;
+    height: 5px;
     cursor: pointer;
+    overflow: hidden;
 
-    transition: transform 0.3s ease;
+    background: rgba(0, 0, 0, 0.1);
+    border-radius: 200px;
+
+    transition:
+        background 0.2s ease,
+        height 0.2s ease;
+    will-change: background, height;
 
     &:hover {
-        transform: scaleY(2);
+        background: rgba(0, 0, 0, 0.2);
+        height: 10px;
     }
 `;
 
-export const NavItemCurrent = styled(m.div)`
-    border-radius: 50px;
-    position: absolute;
-    top: 0;
-    left: 0;
-    display: flex;
-    width: 71px;
-    height: 4px;
+export const NavItemCurrent = styled.div<{ $duration?: number }>`
+    border-radius: 200px;
+    height: 100%;
     z-index: 1;
+    background: #000;
+    animation: ${widthIn} ${({ $duration = 0.3 }) => $duration}s linear forwards;
+    will-change: width, opacity;
 `;
 
 export const GraphicContainer = styled(m.div)`
@@ -91,10 +151,13 @@ export const GraphicContainer = styled(m.div)`
     }
 `;
 
-export const StepImg = styled(m.img)`
+export const StepImg = styled.img<{ $index: number }>`
     max-height: 100%;
     position: absolute;
     bottom: 0;
     right: 0;
     pointer-events: none;
+    opacity: 0;
+    animation: ${slideIn} 2s cubic-bezier(0.16, 1, 0.3, 1) forwards;
+    will-change: transform, opacity;
 `;
