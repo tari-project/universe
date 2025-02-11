da9a9ee6cdab696eed287c7a6ec7be2c08bd1250
Maciej Kożuszek <mackozuszek@gmail.com>
2025-02-11 16:02:22 +0100

    feat: block win sound and CSP (#1497)
    Description
    ---
    Fix bug introduced from merging #372 
    Adds CSP for inline styling to allow rendering tower animation.
    Also changes loading audio file to bundle it with FE assets from
    public/assets instead of bundling it from BE and exposing for FE to
    fetch it.
    
    Some explanations regarding CSP:
    - `"dangerousDisableAssetCspModification": ["style-src"],` and `
    "style-src": "'self' 'unsafe-inline'",` are required to use CSS-in-JS
    styled components to update components styling at runtime
    - `"script-src": "'wasm-unsafe-eval'", and the `http://ipc.localhost
    https://cdn.jsdelivr.net/npm/@lottiefiles/dotlottie-web@0.40.1/dist/dotlottie-player.wasm
    https://unpkg.com/@lottiefiles/dotlottie-web@0.40.1/dist/dotlottie-player.wasm`
    inside ` "connect-src"` are required by animation libraries
    - `"default-src": "'self' asset: tauri: URL: blob: data:",` is the
    fallback for non-defined entries and it's just allows to access local
    assets, read them as a file and `"connect-src": "tauri: ipc:
    http://ipc.localhost ..."` allows to use commands
    - `"https:"` in `"default-src"` and `"connect-src"` is required by
    Windows to load assets (looks like Windows doesn't support custom
    protocols like `asset:` or `tuari:`)
    
    
    ```js
    "security": {
        "dangerousDisableAssetCspModification": ["style-src"],
        "csp": {
            "default-src": "'self' asset: tauri: URL: blob: data: https:",
            "script-src": "'wasm-unsafe-eval'",
            "style-src": "'self' 'unsafe-inline'",
            "connect-src": "https: tauri: ipc: http://ipc.localhost https://cdn.jsdelivr.net/npm/@lottiefiles/dotlottie-web@0.40.1/dist/dotlottie-player.wasm https://unpkg.com/@lottiefiles/dotlottie-web@0.40.1/dist/dotlottie-player.wasm"
        },
    },
    ```
    
    
    Motivation and Context
    ---
    Fix P0 bug and simplify handling of sound effect.
    
    How Has This Been Tested?
    ---
    
    What process can a PR reviewer use to test or verify this change?
    ---
    Few things to test:
    
     - sound effect when winning block mine
     - sound effect when replay won block
     - disabling and enabling audio from general settings
    - right click and toggle 'inspect' and check for any warning or error
    logs coming from lack of any CSP.
    
    All above should be done on release mode since previously this issues
    didn't occur on dev build.
    
    
    Breaking Changes
    ---
    
    - [x] None
    - [ ] Requires data directory on base node to be deleted
    - [ ] Requires hard fork
    - [ ] Other - Please specify
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced audio playback functionality with interactive feedback on
    key events.
      - Added an audio toggle in settings for enabling or disabling sound.
    - Expanded multi-language support with a new “audio enabled” label
    across supported locales.
    
    - **Chores**
      - Updated Linux build processes to include essential audio libraries.
    - Improved configuration and security settings to enhance overall
    performance.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    ---------
    
    Co-authored-by: Brian Pearce <brianp@users.noreply.github.com>

--- src/containers/floating/Settings/sections/general/GeneralSettings.tsx
+++ src/containers/floating/Settings/sections/general/GeneralSettings.tsx
@@ -9,6 +9,7 @@ import StartApplicationOnBootSettings from './StartApplicationOnBootSettings.tsx
 import AutoUpdate from './AutoUpdate.tsx';
 import PreReleaseSettings from './PreReleaseSettings.tsx';
 import VisualMode from '@app/containers/main/Dashboard/components/VisualMode.tsx';
+import AudioSettings from './AudioSettings.tsx';
 
 export const GeneralSettings = () => {
     return (
@@ -19,6 +20,7 @@ export const GeneralSettings = () => {
             <AirdropPermissionSettings />
             <LanguageSettings />
             <VisualMode />
+            <AudioSettings />
             <ThemeSettings />
             <LogsSettings />
             <SettingsGroupWrapper $advanced>
