5e349b3ee9408249a924535d6c1c2eb418893809
shan <47271333+shanimal08@users.noreply.github.com>
2025-02-23 22:33:23 +0200

    feat: reinstate dm (#1550)
    Description
    ---
    - reinstate theme settings
    - update newer UI that didn't use theming
    - updated gl build + fail colour
    - updated DM airdrop CTA
    
    Motivation and Context
    ---
    - https://x.com/tari/status/1891954442343191008
    
    How Has This Been Tested?
    ---
    - locally
    
    
    https://github.com/user-attachments/assets/1280f152-4882-4c02-a6e8-4b30a712093d
    
    
    
    https://github.com/user-attachments/assets/0f5bff5f-8c6f-4b18-81c0-68965a7bfc8f
    
    
    
    https://github.com/user-attachments/assets/c1cf7fde-fd00-4bbe-8fe7-6cabbc778104
    
    
    
    What process can a PR reviewer use to test or verify this change?
    ---
    - switch between the theme modes in settings and ensure colours match up
    to mode
    - also switch OS dark/light mode while on 'System' and ensure the theme
    setting is correct
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **New Features**
    - The webpage now adapts its background and splash screen animations to
    your system’s light or dark mode preference.
    
    - **Style**
    - UI elements, including buttons and interactive controls, have been
    refreshed with dynamic colors, gradients, and transitions for a more
    polished and consistent visual experience.
    - Minor layout spacing improvements enhance overall responsiveness and
    usability.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- src/containers/floating/Settings/sections/general/ThemeSettings.tsx
+++ src/containers/floating/Settings/sections/general/ThemeSettings.tsx
@@ -1,6 +1,4 @@
-import { useAppConfigStore } from '@app/store/useAppConfigStore';
 import { useTranslation } from 'react-i18next';
-
 import { Typography } from '@app/components/elements/Typography.tsx';
 import ThemeSelector from '../../components/ThemeSelector';
 
@@ -12,12 +10,6 @@ import {
 
 export default function ThemeSettings() {
     const { t } = useTranslation('settings', { useSuspense: false });
-    const reset_earnings = useAppConfigStore((s) => s.reset_earnings);
-    const isEsme = useAppConfigStore((s) => s.config_file?.includes('alpha'));
-
-    if (!reset_earnings || !isEsme) {
-        return null;
-    }
     return (
         <SettingsGroupWrapper>
             <SettingsGroupTitle>
