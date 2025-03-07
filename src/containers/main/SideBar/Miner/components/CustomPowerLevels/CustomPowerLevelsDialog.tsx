06849cb5826e07e6dacdcdead938bba402e50dd5
Shannon Tenner <shanimal@tari.com>
2025-03-07 11:37:32 +0200

    fix dialog crash

--- src/containers/main/SideBar/Miner/components/CustomPowerLevels/CustomPowerLevelsDialog.tsx
+++ src/containers/main/SideBar/Miner/components/CustomPowerLevels/CustomPowerLevelsDialog.tsx
@@ -154,8 +154,8 @@ export function CustomPowerLevelsDialog({
                         name={`${FormFields.GPUS}.${index}.gpu_name`}
                         render={({ field: _field }) => (
                             <RangeInputComponent
-                                label={`${t('custom-power-levels.gpu-power-level', { index: index + 1 })}: ${gpu.gpu_name}`}
-                                maxLevel={maxAvailableThreads.max_gpus_threads[index].max_gpu_threads}
+                                label={`${t('custom-power-levels.gpu-power-level', { index: index + 1 })}: ${gpu.gpsu_name}`}
+                                maxLevel={maxAvailableThreads?.max_gpus_threads?.[index]?.max_gpu_threads}
                                 value={gpu.max_gpu_threads}
                                 step={2}
                                 desc={'custom-power-levels.choose-gpu-power-level'}
