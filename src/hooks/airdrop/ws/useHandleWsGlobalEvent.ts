9f6d2f0736980ff5335c13bc6e41140eaf7c781d
Balazs Sevecsek <mountain@tari.com>
2025-03-12 15:53:16 +0100

    fix: rebase problem

--- src/hooks/airdrop/ws/useHandleWsGlobalEvent.ts
+++ src/hooks/airdrop/ws/useHandleWsGlobalEvent.ts
@@ -2,13 +2,11 @@ import { useAirdropStore } from '@app/store/useAirdropStore';
 import { WebsocketEventNames, WebsocketGlobalEvent } from '@app/types/ws';
 
 export const useHandleWsGlobalEvent = () => {
-    const setLatestXSpaceEvent = useAirdropStore((state) => state.setLatestXSpaceEvent);
-
     return (event: string) => {
         const eventParsed = JSON.parse(event) as WebsocketGlobalEvent;
         switch (eventParsed.name) {
             case WebsocketEventNames.X_SPACE_EVENT:
-                setLatestXSpaceEvent(eventParsed.data);
+                useAirdropStore.setState({ latestXSpaceEvent: eventParsed.data });
                 break;
             default:
                 // eslint-disable-next-line no-console
