0ec958127a224c81e33d8761659ec49c044d1551
Misieq01 <bartosz2000walczak@gmail.com>
2025-03-13 15:18:04 +0100

    fix import

--- src/hooks/app/index.ts
+++ src/hooks/app/index.ts
@@ -1,5 +1,4 @@
 export * from './useDisableRefresh.ts';
 export * from './useEnvironment.ts';
-export * from './useListenForExternalDependencies.ts';
 export * from './useSetUp.ts';
 export * from './useShuttingDown.ts';
