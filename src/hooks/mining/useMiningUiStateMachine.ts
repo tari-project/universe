f0db2a32981802bd87ebbe398ffc49a04542d1e1
Shannon Tenner <shanimal@tari.com>
2025-03-19 17:12:18 +0200

    add external trigger - block time

--- src/hooks/mining/useMiningUiStateMachine.ts
+++ src/hooks/mining/useMiningUiStateMachine.ts
@@ -5,35 +5,44 @@ import { useAppStateStore } from '@app/store/appStateStore';
 import { useMiningStore } from '@app/store/useMiningStore';
 import { useMiningMetricsStore } from '@app/store/useMiningMetricsStore.ts';
 import { useAppConfigStore } from '@app/store/useAppConfigStore.ts';
+import { useBlockchainVisualisationStore } from '@app/store';
 
 export const useUiMiningStateMachine = () => {
     const isMiningInitiated = useMiningStore((s) => s.miningInitiated);
     const isChangingMode = useMiningStore((s) => s.isChangingMode);
-    const cpuIsMining = useMiningMetricsStore((s) => s.cpu_mining_status.is_mining);
-    const gpuIsMining = useMiningMetricsStore((s) => s.gpu_mining_status.is_mining);
+    const cpuIsMining = useMiningMetricsStore((s) => s.cpu_mining_status?.is_mining);
+    const gpuIsMining = useMiningMetricsStore((s) => s.gpu_mining_status?.is_mining);
     const isResuming = useAppStateStore((state) => state.appResumePayload?.is_resuming);
     const setupComplete = useAppStateStore((s) => s.setupComplete);
     const visualMode = useAppConfigStore((s) => s.visual_mode);
-    const visualModeToggleLoading = useAppConfigStore((s) => s.visualModeToggleLoading);
+    const visualModeLoading = useAppConfigStore((s) => s.visualModeToggleLoading);
+    const blockTime = useBlockchainVisualisationStore((s) => s.displayBlockTime);
+
+    const stateTrigger = animationStatus;
     const isMining = cpuIsMining || gpuIsMining;
-
-    const indexTrigger = animationStatus;
-
-    useEffect(() => {
-        if (!visualMode || visualModeToggleLoading) return;
-        const notStarted = !animationStatus || animationStatus == 'not-started';
-        if (isMining && notStarted && !isResuming) {
-            setAnimationState('start');
-        }
-    }, [indexTrigger, isMining, isResuming, visualMode, visualModeToggleLoading]);
+    const bockTimeTrigger = Number(blockTime?.seconds) % 5 === 0;
 
     useEffect(() => {
-        if (!visualMode || visualModeToggleLoading) return;
-        const notStopped = animationStatus !== 'not-started';
+        if (!visualMode || visualModeLoading) return;
+
+        const notStarted = stateTrigger === 'not-started';
         const preventStop = !setupComplete || isMiningInitiated || isChangingMode;
-        const shouldStop = !isMining && notStopped && !preventStop;
+        const shouldStop = !isMining && !notStarted && !preventStop;
+        const shouldStartAnimation = isMining && notStarted && !isResuming;
         if (shouldStop) {
             setAnimationState('stop');
+        } else if (shouldStartAnimation) {
+            setAnimationState('start');
         }
-    }, [indexTrigger, setupComplete, isMiningInitiated, isMining, isChangingMode, visualMode, visualModeToggleLoading]);
+    }, [
+        bockTimeTrigger,
+        isChangingMode,
+        isMining,
+        isMiningInitiated,
+        isResuming,
+        setupComplete,
+        stateTrigger,
+        visualMode,
+        visualModeLoading,
+    ]);
 };
