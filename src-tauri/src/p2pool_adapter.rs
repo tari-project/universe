122705e3e81a08709fe4719a60b1a8c98900e193
stringhandler <stringhandler@protonmail.com>
2025-03-28 15:43:50 +0200

    chore(release): v0.9.829

--- src-tauri/src/p2pool_adapter.rs
+++ src-tauri/src/p2pool_adapter.rs
@@ -111,7 +111,7 @@ impl ProcessAdapter for P2poolAdapter {
 
         args.push("--squad-prefix".to_string());
         let mut squad_prefix = "default";
-        let mut num_squads = 10;
+        let mut num_squads = 3;
         if let Some(benchmark) = config.cpu_benchmark_hashrate {
             if benchmark < 4000 {
                 squad_prefix = "mini";
