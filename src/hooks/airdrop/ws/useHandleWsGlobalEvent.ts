407c38e5fb8e87c97411b3eeca8bc6a3ef29e9dd
Balazs Sevecsek <mountain@tari.com>
2025-03-28 14:21:22 +0100

    choir: move the setXSpace

--- src/hooks/airdrop/ws/useHandleWsGlobalEvent.ts
+++ src/hooks/airdrop/ws/useHandleWsGlobalEvent.ts
@@ -1,4 +1,4 @@
-import { useAirdropStore } from '@app/store/useAirdropStore';
+import { setXSpaceEvent } from '@app/store/actions/airdropStoreActions';
 import { WebsocketEventNames, WebsocketGlobalEvent } from '@app/types/ws';
 import { useCallback } from 'react';
 
@@ -7,7 +7,7 @@ export const useHandleWsGlobalEvent = () => {
         const eventParsed = JSON.parse(event) as WebsocketGlobalEvent;
         switch (eventParsed.name) {
             case WebsocketEventNames.X_SPACE_EVENT:
-                useAirdropStore.setState({ latestXSpaceEvent: eventParsed.data });
+                setXSpaceEvent(eventParsed.data);
                 break;
             default:
                 console.warn('Unknown global event', eventParsed);
