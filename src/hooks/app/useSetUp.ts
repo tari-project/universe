f2f974570a58a2e3b2530d9f26643199bd6b6e6e
shan <47271333+shanimal08@users.noreply.github.com>
2025-04-01 09:00:43 +0200

    refactor: remove unnecessary items from useSetUp (#1750)
    Description
    ---
    we no longer need a lot of the old stuff that was left in useSetup so
    cleaned it up

--- src/hooks/app/useSetUp.ts
+++ src/hooks/app/useSetUp.ts
@@ -1,6 +1,5 @@
-import { useCallback, useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import { listen } from '@tauri-apps/api/event';
-import { useUIStore } from '@app/store/useUIStore';
 import { TauriEvent } from '../../types.ts';
 import {
     airdropSetup,
@@ -12,54 +11,30 @@ import {
 } from '@app/store/actions';
 
 export function useSetUp() {
-    const isInitializingRef = useRef(false);
-    const adminShow = useUIStore((s) => s.adminShow);
-    const handlePostSetup = useCallback(async () => {
-        await setSetupComplete();
-        await fetchApplicationsVersionsWithRetry();
-        await airdropSetup();
-    }, [setSetupComplete, fetchApplicationsVersionsWithRetry, airdropSetup]);
-
     useEffect(() => {
-        if (adminShow === 'setup') return;
-
-        let unlisten: () => void;
-
-        (async () => {
-            unlisten = await listen('setup_message', async ({ event: e, payload: p }: TauriEvent) => {
-                console.log('Received tauri event: ', { e, p });
-                switch (p.event_type) {
-                    case 'setup_status':
-                        if (p.progress >= 0) {
-                            setSetupTitle(p.title);
-                            setSetupParams(p.title_params);
-                            setSetupProgress(p.progress);
-                        }
-                        if (p.progress >= 1) {
-                            await handlePostSetup();
-                        }
-                        break;
-                    default:
-                        console.warn('Unknown tauri event: ', { e, p });
-                        break;
-                }
-            });
-        })();
-
-        if (!isInitializingRef.current) {
-            isInitializingRef.current = true;
-            const clearStorage = () => {
-                const airdropStorage = localStorage.getItem('airdrop-store');
-                localStorage.clear();
-                if (airdropStorage) {
-                    localStorage.setItem('airdrop-store', airdropStorage);
-                }
-            };
-            clearStorage();
-        }
+        const unlistenPromise = listen('setup_message', async ({ event: e, payload: p }: TauriEvent) => {
+            console.info('Received tauri event: ', { e, p });
+            switch (p.event_type) {
+                case 'setup_status':
+                    if (p.progress > 0) {
+                        setSetupTitle(p.title);
+                        setSetupParams(p.title_params);
+                        setSetupProgress(p.progress);
+                    }
+                    if (p.progress >= 1) {
+                        await setSetupComplete();
+                        await fetchApplicationsVersionsWithRetry();
+                        await airdropSetup();
+                    }
+                    break;
+                default:
+                    console.warn('Unknown tauri event: ', { e, p });
+                    break;
+            }
+        });
 
         return () => {
-            if (unlisten) unlisten();
+            unlistenPromise.then((unlisten) => unlisten());
         };
-    }, [adminShow, handlePostSetup]);
+    }, []);
 }
