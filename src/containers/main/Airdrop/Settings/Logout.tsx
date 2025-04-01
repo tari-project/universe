289719f19902afdd5ffc13dafa6c6826b09d330d
Juan De Luca <juan.f.d.luca@gmail.com>
2025-04-01 11:29:23 +0100

    feat: adds update username

--- src/containers/main/Airdrop/Settings/Logout.tsx
+++ src/containers/main/Airdrop/Settings/Logout.tsx
@@ -11,11 +11,29 @@ import {
 import { useAirdropStore } from '@app/store/useAirdropStore';
 import { Trans, useTranslation } from 'react-i18next';
 import { handleAirdropLogout } from '@app/store';
+import { Input } from '@app/components/elements/inputs/Input';
+import { useState } from 'react';
+import { handleUsernameChange } from '@app/store/actions/airdropStoreActions';
+import { ErrorText, InputWrapper, UsernameContainer } from './styles';
 
 export default function AirdropLogout() {
     const { t } = useTranslation(['common', 'airdrop'], { useSuspense: false });
     const authUuid = useAirdropStore((s) => s.authUuid);
     const userDetails = useAirdropStore((s) => s.userDetails);
+    const [loading, setLoading] = useState(false);
+    const [username, setUsername] = useState(userDetails?.user.name || '');
+    const [error, setError] = useState('');
+    const handleUpdateUsername = async () => {
+        setLoading(true);
+        if (!username) return;
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        await handleUsernameChange(username, async (response: any) => {
+            const res = await response?.json();
+            console.error('Error updating username', res);
+            setError(res?.message || '');
+        });
+        setLoading(false);
+    };
 
     if (!userDetails && !authUuid) return null;
     return (
@@ -40,6 +58,29 @@ export default function AirdropLogout() {
                     </Button>
                 </SettingsGroupAction>
             </SettingsGroup>
+            <UsernameContainer>
+                <InputWrapper>
+                    {
+                        <Trans
+                            i18nKey="update-username"
+                            ns="airdrop"
+                            values={{ twitter: userDetails?.user.name }}
+                            components={{ strong: <strong /> }}
+                        />
+                    }
+                    <Input
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        disabled={loading}
+                        hasError={!!error}
+                    />
+                </InputWrapper>
+
+                <Button color="warning" size="small" onClick={handleUpdateUsername} disabled={loading}>
+                    {t('update')}
+                </Button>
+            </UsernameContainer>
+            {error && <ErrorText>{error}</ErrorText>}
         </SettingsGroupWrapper>
     );
 }
