af3c9750cf5507c2f22f849ba02afc7fee178692
Misieq01 <bartosz2000walczak@gmail.com>
2025-04-09 11:57:48 +0200

    remove telemetry log

--- src-tauri/src/telemetry_service.rs
+++ src-tauri/src/telemetry_service.rs
@@ -226,8 +226,6 @@ async fn send_telemetry_data(
         )
         .json(&full_data);
 
-    println!("Telemetry data: {:?}", full_data);
-
     let response = request_builder.send().await?;
 
     if response.status() == 429 {
