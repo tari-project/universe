c7814cfdb3c961d96f97d8d848c9ffeea805dc58
Marcin Papież <mmpapiez@gmail.com>
2025-04-15 12:34:28 +0200

    add import for events manager

--- src-tauri/src/utils/platform_utils.rs
+++ src-tauri/src/utils/platform_utils.rs
@@ -27,13 +27,14 @@ use super::macos_utils::is_app_in_applications_folder;
 use crate::external_dependencies::ExternalDependencies;
 
 #[cfg(not(target_os = "linux"))]
+use crate::events_manager::EventsManager;
+#[cfg(not(target_os = "linux"))]
 use crate::UniverseAppState;
 #[cfg(not(target_os = "linux"))]
 use anyhow::anyhow;
+use std::fmt::Display;
 #[cfg(not(target_os = "linux"))]
 use tauri::Manager;
-
-use std::fmt::Display;
 
 #[derive(Clone)]
 pub enum CurrentOperatingSystem {
