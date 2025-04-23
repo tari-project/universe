42fa65761e3a478ec0cf40ad1d13ac15ee8eee4c
JuanOne <juan.f.d.luca@gmail.com>
2025-04-23 14:52:55 +0100

    fix: Show CriticalProblemDialog when problem ex...
    The CriticalProblemDialog was not being displayed because the `open` prop on the `Dialog` component was hardcoded to `false`. This commit changes the `open` prop to `open={!!criticalProblem}` to correctly display the dialog when a critical problem is detected.

--- src/containers/floating/CriticalProblemDialog/CriticalProblemDialog.tsx
+++ src/containers/floating/CriticalProblemDialog/CriticalProblemDialog.tsx
@@ -35,10 +35,7 @@ const CriticalProblemDialog = memo(function CriticalProblemDialog() {
     }, []);
 
     return (
-        <Dialog
-            //open={!!criticalProblem}
-            open={false}
-        >
+        <Dialog open={!!criticalProblem}>
             <DialogContent>
                 <Stack gap={16}>
                     <Stack gap={4} style={{ maxWidth: '480px' }}>
