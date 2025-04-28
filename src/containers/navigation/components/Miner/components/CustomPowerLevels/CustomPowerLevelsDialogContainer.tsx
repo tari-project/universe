6ed7101d5deeb617cef57099edf1a119d149a3e9
Marcin Papież <mmpapiez@gmail.com>
2025-04-28 20:38:53 +0200

    ensure we call getMaxAvailableThreads when both cpu/gpu available

--- src/containers/navigation/components/Miner/components/CustomPowerLevels/CustomPowerLevelsDialogContainer.tsx
+++ src/containers/navigation/components/Miner/components/CustomPowerLevels/CustomPowerLevelsDialogContainer.tsx
@@ -4,20 +4,22 @@ import { useMiningStore } from '@app/store/useMiningStore';
 import { useEffect } from 'react';
 import { getMaxAvailableThreads, setCustomLevelsDialogOpen } from '@app/store';
 import { CircularProgress } from '@app/components/elements/CircularProgress.tsx';
+import { useSetupStore } from '@app/store/useSetupStore';
 
 export const CustomPowerLevelsDialogContainer = () => {
     const customLevelsDialogOpen = useMiningStore((s) => s.customLevelsDialogOpen);
     const isChangingMode = useMiningStore((s) => s.isChangingMode);
     const maxThreads = useMiningStore((s) => s.maxAvailableThreads);
+    const isHardwarePhaseFinished = useSetupStore((s) => s.hardwarePhaseFinished);
 
     const handleClose = () => {
         setCustomLevelsDialogOpen(false);
     };
     useEffect(() => {
-        if (!maxThreads) {
+        if (!maxThreads && isHardwarePhaseFinished) {
             getMaxAvailableThreads();
         }
-    }, [maxThreads]);
+    }, [isHardwarePhaseFinished, maxThreads]);
 
     return (
         <Dialog
