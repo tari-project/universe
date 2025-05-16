0d994889d191fb3a441eb05440f537cc322f2e38
Misieq01 <bartosz2000walczak@gmail.com>
2025-05-16 09:10:41 +0200

    fix ci

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -163,7 +163,7 @@ const APPLICATION_FOLDER_ID: &str = "com.tari.universe.other";
 const APPLICATION_FOLDER_ID: &str = "com.tari.universe";
 #[cfg(all(feature = "release-ci-beta", not(feature = "release-ci")))]
 const APPLICATION_FOLDER_ID: &str = "com.tari.universe.beta";
-#[cfg(feature = "exchange-ci")]
+#[cfg(all(feature = "exchange-ci", not(feature = "release-ci")))]
 const APPLICATION_FOLDER_ID: &str = const_format::formatcp!("com.tari.universe.{}", EXCHANGE_ID);
 
 #[allow(clippy::too_many_lines)]
