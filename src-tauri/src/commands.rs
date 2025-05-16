09ad5df2a7bd1099a7297d8b182c428f6530ec7e
Maciej Kozuszek <mackozuszek@gmail.com>
2025-05-16 15:04:52 +0200

    Fix awaiting address update events emitting

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -609,7 +609,8 @@ pub async fn set_tari_address(address: String, app: tauri::AppHandle) -> Result<
         &handle_clone,
         new_address,
         internal_wallet.get_is_tari_address_generated(),
-    );
+    )
+    .await;
     SetupManager::get_instance()
         .add_phases_to_restart_queue(vec![
             SetupPhase::Wallet,
@@ -647,7 +648,8 @@ pub async fn confirm_exchange_address(
         &handle_clone,
         new_address,
         internal_wallet.get_is_tari_address_generated(),
-    );
+    )
+    .await;
     SetupManager::get_instance()
         .init_exchange_modal_status()
         .await
