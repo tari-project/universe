e2ffc5c2f7e7154ea900cc48da0077a0ff1df03a
Aaron Pepper <pepdog@gmail.com>
2025-05-22 20:26:19 +0900

    remove duplicate QueryClientProvider

--- src/App/AppWrapper.tsx
+++ src/App/AppWrapper.tsx
@@ -1,16 +1,11 @@
 import App from './App.tsx';
 import AppEffects from './AppEffects.tsx';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
 
 export default function AppWrapper() {
     return (
         <>
             <AppEffects />
-            <QueryClientProvider client={queryClient}>
-                <App />
-            </QueryClientProvider>
+            <App />
         </>
     );
 }
