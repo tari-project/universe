75d5ce1af5a152375cfa71dfb76ad65fd2c47f7f
Maciej Kozuszek <mackozuszek@gmail.com>
2025-05-30 09:39:31 +0200

    Refactor

--- src-tauri/src/utils/network_status.rs
+++ src-tauri/src/utils/network_status.rs
@@ -111,7 +111,7 @@ impl NetworkStatus {
                 &reqwest::blocking::Client::new(),
                 NETWORK_DOWNLOAD_SPEED_PAYLOAD_TEST,
                 OutputFormat::None,
-            );
+            )
         })
         .await
         {
@@ -149,9 +149,6 @@ impl NetworkStatus {
     pub async fn run_speed_test_once(&self, app_handle: &AppHandle) -> Result<(), anyhow::Error> {
         match self.perform_speed_test().await {
             Ok((download_speed, upload_speed, latency)) => {
-                info!(target: LOG_TARGET, "[DEBUG NETWORK]Download speed: {:.2} Mbps", download_speed);
-                info!(target: LOG_TARGET, "[DEBUG NETWORK]Upload speed: {:.2} Mbps", upload_speed);
-                info!(target: LOG_TARGET, "[DEBUG NETWORK]Latency: {:.2} ms", latency);
                 self.handle_test_results(app_handle, download_speed, upload_speed, latency)
                     .await;
                 Ok(())
