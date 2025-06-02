2faf8a9ae93547d560c9b37693330c7779176ce3
Misieq01 <bartosz2000walczak@gmail.com>
2025-06-02 16:30:21 +0200

    comments

--- src-tauri/src/setup/utils/timeout_watcher.rs
+++ src-tauri/src/setup/utils/timeout_watcher.rs
@@ -15,12 +15,38 @@ use tokio::{
 
 static LOG_TARGET: &str = "tari::universe::timeout_watcher";
 
+/// A utility function to compute a hash value for a given value of type T.  
+/// This function uses the `DefaultHasher` to create a hash of the value.
 pub fn hash_value<T: Hash>(value: &T) -> u64 {
     let mut hasher = DefaultHasher::new();
     value.hash(&mut hasher);
     hasher.finish()
 }
 
+/// A utility function that sleeps for a specified duration if provided, or indefinitely if `None`.
+/// If a duration is provided, it will sleep for that duration and return `Some(())`.
+/// If `None` is provided, it will wait indefinitely, allowing the caller to use it in a `select!` block.
+/// This is useful for implementing dynamic timeouts
+/// # Example usage:
+/// ```rust
+/// tokio::select! {
+///    _ = conditional_sleeper(Some(Duration::from_secs(5))) => {
+///        println!("Slept for 5 seconds");
+///   }
+///   _ = some_other_future_that_needs_timeout => {
+///       println!("Some other future completed");
+///  }
+/// }
+///
+/// tokio::select! {
+///    _ = conditional_sleeper(None) => {
+///       println!("Waiting indefinitely");
+///   }
+///   _ = some_other_future_that_dont_need_timeout => {
+///      println!("Some other future completed");
+///   }
+/// }
+/// ```
 pub async fn conditional_sleeper(duration: Option<Duration>) -> Option<()> {
     match duration {
         Some(duration) => {
@@ -48,9 +74,43 @@ impl TimeoutWatcher {
             last_hash_value: Mutex::new(None),
         }
     }
+    /// Returns a clone of the sender used to notify about changes in the hash value.
+    /// # Example usage:
+    /// ```rust
+    /// use std::hash::Hash;
+    ///
+    /// #[derive(Hash)]
+    /// struct MyData {
+    ///     value: String,
+    ///     some_field: String,  
+    /// }
+    ///
+    /// let timeout_watcher = TimeoutWatcher::new(Some(Duration::from_secs(60)));
+    /// let sender = timeout_watcher.get_sender();
+    /// let my_data = MyData {
+    ///     value: "example".to_string(),
+    ///    some_field: "field".to_string(),
+    /// };
+    ///
+    ///let _unused = sender.send(hash_value(&my_data)).expect("Failed to send hash value");
+    ///```
     pub fn get_sender(&self) -> Sender<u64> {
         self.sender.clone()
     }
+    /// Resolves the timeout by waiting for either different hash value on the channel or the timeout duration to elapse.
+    ///
+    /// If the timeout duration is `None`, it will wait indefinitely. Its done on purpose to support usage with tokio's `select!` macro.
+    /// # Example usage:
+    /// ```rust
+    /// tokio::select! {
+    ///     _ = timeout_watcher.resolve_timeout() => {
+    ///         println!("Timeout elapsed");
+    ///    }
+    ///     _ = some_other_future => {
+    ///        println!("Some other future completed");
+    ///    }
+    /// }
+    /// ```
     pub async fn resolve_timeout(&self) -> Option<()> {
         let mut receiver: Receiver<u64> = self.sender.subscribe();
         loop {
@@ -66,7 +126,6 @@ impl TimeoutWatcher {
             }
         }
     }
-
     async fn resolve_when_hash_changed(
         &self,
         receiver: &mut Receiver<u64>,
