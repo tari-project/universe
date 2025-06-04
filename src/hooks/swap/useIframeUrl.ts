84985c4a86ff5ea806aaa3396a1a95572e5c93cb
JuanOne <juan.f.d.luca@gmail.com>
2025-06-04 18:13:07 +0100

    feat: Fetch swap URL on component mount
    Fetches the swap URL on component mount using the /swaps/url endpoint.
    This commit introduces a ref to store the URL and ensures that the most recent fetched URL is always available, even during the initial fetch.
    Additionally, the `publicRequest` flag is set to true.

--- src/hooks/swap/useIframeUrl.ts
+++ src/hooks/swap/useIframeUrl.ts
@@ -1,20 +1,23 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { handleAirdropRequest } from '../airdrop/utils/useHandleRequest';
 
 export const useIframeUrl = () => {
-    const [url, setUrl] = useState<string | null>(null);
+    const urlRef = useRef<string | null>(null);
+    const [url, setUrl] = useState<string | null>(urlRef.current);
 
     useEffect(() => {
         handleAirdropRequest<{ url: string }>({
             path: '/swaps/url',
             method: 'GET',
+            publicRequest: true,
             onError: () => setUrl(null),
         }).then((data) => {
             if (data?.url) {
+                urlRef.current = data.url;
                 setUrl(data.url);
             }
         });
     }, []);
 
-    return url;
+    return url || urlRef.current;
 };
