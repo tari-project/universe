adb72e63b60ed78878344f955781b090aa0b8b70
Maciej Kozuszek <mackozuszek@gmail.com>
2025-06-05 17:18:16 +0200

    Fix lint errors

--- src/store/useAppConfigStore.ts
+++ src/store/useAppConfigStore.ts
@@ -22,7 +22,7 @@ const configCoreInitialState: ConfigCore = {
     airdrop_tokens: undefined,
     last_binaries_update_timestamp: '',
     p2pool_stats_server_port: undefined,
-    universal_miner_initialized_exchange_id: undefined,
+    universal_miner_exchange_id: undefined,
 };
 
 const configWalletInitialState: ConfigWallet = {
