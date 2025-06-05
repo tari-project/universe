95bd91819b4fbcd07f48d71b7a17f43e4fcd9043
JuanOne <juan.f.d.luca@gmail.com>
2025-06-05 09:36:22 +0100

    feat: Fetch swaps URL periodically
    This commit introduces a mechanism to periodically fetch the swaps URL from the backend. It uses a `setInterval` to repeatedly attempt fetching the URL every second if it's not already available. Once the URL is successfully fetched, the interval is cleared. This addresses the issue of the swaps iframe failing to load when the initial URL fetch fails, ensuring that the application retries until the URL is available. The `handleFetchUrl` is now a useCallback to prevent unnecessary re-renders and to control the interval correctly.

--- src/hooks/swap/useIframeUrl.ts
+++ src/hooks/swap/useIframeUrl.ts
@@ -1,25 +1,38 @@
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { handleAirdropRequest } from '../airdrop/utils/useHandleRequest';
 
 export const useIframeUrl = () => {
     const urlRef = useRef<string | null>(null);
     const [url, setUrl] = useState<string | null>(urlRef.current);
 
+    const handleFetchUrl = useCallback((onSuccess?: () => void) => {
+        handleAirdropRequest<{ url: string }>({
+            path: '/swaps/url',
+            method: 'GET',
+            publicRequest: true,
+            onError: () => setUrl(null),
+        }).then((data) => {
+            if (!data?.url) {
+                console.error('Failed to fetch swaps url');
+                return;
+            }
+            urlRef.current = data.url;
+            setUrl(data.url);
+            onSuccess?.();
+        });
+    }, []);
+
     useEffect(() => {
-        if (!urlRef.current && url === null) {
-            handleAirdropRequest<{ url: string }>({
-                path: '/swaps/url',
-                method: 'GET',
-                publicRequest: true,
-                onError: () => setUrl(null),
-            }).then((data) => {
-                if (data?.url) {
-                    urlRef.current = data.url;
-                    setUrl(data.url);
-                }
-            });
-        }
-    }, [url]);
+        const interval = setInterval(() => {
+            console.info('Fetching swaps url');
+            if (!urlRef.current && url === null) {
+                handleFetchUrl(() => {
+                    clearInterval(interval);
+                });
+            }
+        }, 1000);
+        return () => clearInterval(interval);
+    }, [handleFetchUrl, url]);
 
     return url || urlRef.current;
 };
