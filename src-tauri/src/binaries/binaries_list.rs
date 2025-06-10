5058cc8fac8aec85a15ec962747d13bde11e4cf0
Misieq01 <38589417+Misieq01@users.noreply.github.com>
2025-06-10 18:29:16 +0200

    feat: timeout improvements for binaries (#2325)
    ### [ Summary ]
    - Connected new timeout handler to binaries download
    - Added display for download progress in setup screen and resume modal
    - removed progress_tracker_old
    - removed `tapplet_resolver` `tapplet_manager` `tapplet_list` as they
    were clones of binary_resolver binary_manager `binary_list`
    
    ### [ Testing ]
    - Remove binaries files from machine, you should be able to observe
    download progress information on setup screen
    - Timeout feature is harder to test as its require low bandwidth

--- src-tauri/src/binaries/binaries_list.rs
+++ src-tauri/src/binaries/binaries_list.rs
@@ -33,6 +33,7 @@ pub enum Binaries {
     ShaP2pool,
     GpuMiner,
     Tor,
+    BridgeTapplet,
 }
 
 impl Binaries {
@@ -45,6 +46,7 @@ impl Binaries {
             Binaries::ShaP2pool => "sha-p2pool",
             Binaries::GpuMiner => "glytex",
             Binaries::Tor => "tor",
+            Binaries::BridgeTapplet => "bridge",
         }
     }
 
@@ -57,6 +59,7 @@ impl Binaries {
             "sha-p2pool" => Binaries::ShaP2pool,
             "glytex" => Binaries::GpuMiner,
             "tor" => Binaries::Tor,
+            "bridge" => Binaries::BridgeTapplet,
             _ => panic!("Unknown binary name: {}", name),
         }
     }
@@ -91,6 +94,10 @@ impl Binaries {
                 let file_name = "tor";
                 PathBuf::from(file_name)
             }
+            Binaries::BridgeTapplet => {
+                let file_name = format!("bridge-{}", version);
+                PathBuf::from(file_name).join("bridge")
+            }
         }
     }
 
@@ -104,6 +111,7 @@ impl Binaries {
             Binaries::ShaP2pool,
             Binaries::GpuMiner,
             Binaries::Tor,
+            Binaries::BridgeTapplet,
         ]
         .iter()
         .copied()
--- src-tauri/src/setup/phase_node.rs
+++ src-tauri/src/setup/phase_node.rs
@@ -28,7 +28,6 @@ use crate::{
     events_emitter::EventsEmitter,
     events_manager::EventsManager,
     node::node_manager::{NodeManagerError, STOP_ON_ERROR_CODES},
-    progress_tracker_old::ProgressTracker,
     progress_trackers::{
         progress_plans::{ProgressPlans, ProgressSetupNodePlan},
         progress_stepper::ProgressStepperBuilder,
@@ -170,28 +169,26 @@ impl SetupPhaseImpl for NodeSetupPhase {
         let (data_dir, config_dir, log_dir) = self.get_app_dirs()?;
         let state = self.app_handle.state::<UniverseAppState>();
 
-        // TODO Remove once not needed
-        let (tx, rx) = watch::channel("".to_string());
-        let progress = ProgressTracker::new(self.app_handle.clone(), Some(tx));
         let binary_resolver = BinaryResolver::current().read().await;
 
         if self.app_configuration.use_tor && !cfg!(target_os = "macos") {
-            tokio::time::sleep(Duration::from_secs(10)).await;
-            progress_stepper
-                .resolve_step(ProgressPlans::Node(ProgressSetupNodePlan::BinariesTor))
-                .await;
+            let tor_binary_progress_tracker = progress_stepper.channel_step_range_updates(
+                ProgressPlans::Node(ProgressSetupNodePlan::BinariesTor),
+                Some(ProgressPlans::Node(ProgressSetupNodePlan::BinariesNode)),
+            );
             binary_resolver
-                .initialize_binary_timeout(Binaries::Tor, progress.clone(), rx.clone())
+                .initialize_binary(Binaries::Tor, tor_binary_progress_tracker)
                 .await?;
         } else {
             progress_stepper.skip_step(ProgressPlans::Node(ProgressSetupNodePlan::BinariesTor));
         };
 
-        progress_stepper
-            .resolve_step(ProgressPlans::Node(ProgressSetupNodePlan::BinariesNode))
-            .await;
+        let node_binary_progress_tracker = progress_stepper.channel_step_range_updates(
+            ProgressPlans::Node(ProgressSetupNodePlan::BinariesNode),
+            Some(ProgressPlans::Node(ProgressSetupNodePlan::StartTor)),
+        );
         binary_resolver
-            .initialize_binary_timeout(Binaries::MinotariNode, progress.clone(), rx.clone())
+            .initialize_binary(Binaries::MinotariNode, node_binary_progress_tracker)
             .await?;
 
         if self.app_configuration.use_tor && !cfg!(target_os = "macos") {
