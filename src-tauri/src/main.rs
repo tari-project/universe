ff90b9b993c93304fb4db37ac2b47a557e2fd923
mpapiez <mmpapiez@gmail.com>
2025-06-11 13:32:11 +0200

    cleanup

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -22,7 +22,6 @@
 
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-#![cfg_attr(not(debug_assertions), feature(iter_repeat_n))]
 
 use commands::CpuMinerStatus;
 use cpu_miner::CpuMinerConfig;
