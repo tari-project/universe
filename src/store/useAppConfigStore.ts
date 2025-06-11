b4d59898689a016f120e01a218c481f34e8ee21c
Maciej Kozuszek <mackozuszek@gmail.com>
2025-06-11 13:50:01 +0200

    Allow seed words recovery on universal miner and switch to TU miner

--- src/store/useAppConfigStore.ts
+++ src/store/useAppConfigStore.ts
@@ -1,7 +1,6 @@
 import { ConfigBackendInMemory, ConfigCore, ConfigMining, ConfigUI, ConfigWallet } from '@app/types/configs';
 import { create } from './create';
 
-export const DEFAULT_EXCHANGE_ID = 'universal';
 type UIConfigStoreState = Partial<ConfigUI> & {
     visualModeToggleLoading: boolean;
 };
