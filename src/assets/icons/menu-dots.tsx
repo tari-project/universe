dd9213c6e92797d7d9a2a7881fed320af9169607
Aaron Pepper <pepdog@gmail.com>
2025-06-17 16:32:47 +0800

    lint fix

--- src/assets/icons/menu-dots.tsx
+++
@@ -1,7 +0,0 @@
-export const MenuDotsSVG = () => (
-    <svg width="11" height="3" viewBox="0 0 11 3" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="1.5" cy="1.5" r="1.5" fill="currentColor" />
-        <circle cx="5.5" cy="1.5" r="1.5" fill="currentColor" />
-        <circle cx="9.5" cy="1.5" r="1.5" fill="currentColor" />
-    </svg>
-);
