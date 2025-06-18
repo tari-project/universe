f4e05f8f2b8e8481aa5dc03579fde1876b6a212c
Aaron Pepper <pepdog@gmail.com>
2025-06-18 19:47:57 +0800

    remove forward ref

--- src/components/exchanges/universal/option/InternalWalletOption.tsx
+++ src/components/exchanges/universal/option/InternalWalletOption.tsx
@@ -29,103 +29,99 @@ import { restartMining } from '@app/store/actions/miningStoreActions.ts';
 
 import { refreshTransactions } from '@app/hooks/wallet/useFetchTxHistory.ts';
 import { truncateMiddle } from '@app/utils/truncateString.ts';
-import { forwardRef } from 'react';
+import { Ref } from 'react';
 
 interface XCOptionProps {
     isCurrent?: boolean;
     isActive?: boolean;
     onActiveClick: (id: string) => void;
+    ref?: Ref<HTMLDivElement>;
 }
 
-export const InternalWalletOption = forwardRef<HTMLDivElement, XCOptionProps>(
-    ({ isCurrent = false, isActive, onActiveClick }, ref) => {
-        const { t } = useTranslation(['exchange', 'settings'], { useSuspense: false });
-        const base_tari_address = useWalletStore((state) => state.tari_address_emoji);
+export function InternalWalletOption({ isCurrent = false, isActive, onActiveClick, ref }: XCOptionProps) {
+    const { t } = useTranslation(['exchange', 'settings'], { useSuspense: false });
+    const base_tari_address = useWalletStore((state) => state.tari_address_emoji);
 
-        const handleRevertToInternalWallet = async () => {
-            await invoke('revert_to_internal_wallet')
-                .then(() => {
-                    setShowUniversalModal(false);
-                    refreshTransactions();
-                    restartMining();
-                })
-                .catch((e) => {
-                    console.error('Could not revert to internal wallet', e);
-                    setError('Could not revert to internal wallet', e);
-                });
-        };
+    const handleRevertToInternalWallet = async () => {
+        await invoke('revert_to_internal_wallet')
+            .then(() => {
+                setShowUniversalModal(false);
+                refreshTransactions();
+                restartMining();
+            })
+            .catch((e) => {
+                console.error('Could not revert to internal wallet', e);
+                setError('Could not revert to internal wallet', e);
+            });
+    };
 
-        const isTari =
-            universalExchangeMinerOption.slug === 'universal' && universalExchangeMinerOption.id === 'universal';
-        const logoSrc = universalExchangeMinerOption.logo_img_small_url;
-        const showExpand = isTari ? !isCurrent && universalExchangeMinerOption.id : universalExchangeMinerOption.id;
+    const isTari = universalExchangeMinerOption.slug === 'universal' && universalExchangeMinerOption.id === 'universal';
+    const logoSrc = universalExchangeMinerOption.logo_img_small_url;
+    const showExpand = isTari ? !isCurrent && universalExchangeMinerOption.id : universalExchangeMinerOption.id;
 
-        return (
-            <Wrapper ref={ref} $isCurrent={isCurrent} $isActive={isActive}>
-                <ContentHeaderWrapper
-                    onClick={() => {
-                        onActiveClick(!isActive ? universalExchangeMinerOption.id : '');
-                    }}
-                >
-                    <XCContent>
-                        {!!logoSrc && (
-                            <ImgWrapper>
-                                <img src={logoSrc} alt={`${universalExchangeMinerOption.name} logo`} />
+    return (
+        <Wrapper ref={ref} $isCurrent={isCurrent} $isActive={isActive}>
+            <ContentHeaderWrapper
+                onClick={() => {
+                    onActiveClick(!isActive ? universalExchangeMinerOption.id : '');
+                }}
+            >
+                <XCContent>
+                    {!!logoSrc && (
+                        <ImgWrapper>
+                            <img src={logoSrc} alt={`${universalExchangeMinerOption.name} logo`} />
+                        </ImgWrapper>
+                    )}
+                    <Heading>{universalExchangeMinerOption.name}</Heading>
+                </XCContent>
+                <SelectOptionWrapper>
+                    {isCurrent && (
+                        <CaptionWrapper>
+                            <CaptionText>{t('selected-exchange-miner', { ns: 'exchange' })}</CaptionText>
+                        </CaptionWrapper>
+                    )}
+                    {showExpand && (
+                        <OpenButton $isOpen={isActive}>
+                            <ImgWrapper $border $isActive={isActive}>
+                                <ChevronSVG />
                             </ImgWrapper>
-                        )}
-                        <Heading>{universalExchangeMinerOption.name}</Heading>
-                    </XCContent>
-                    <SelectOptionWrapper>
-                        {isCurrent && (
-                            <CaptionWrapper>
-                                <CaptionText>{t('selected-exchange-miner', { ns: 'exchange' })}</CaptionText>
-                            </CaptionWrapper>
-                        )}
-                        {showExpand && (
-                            <OpenButton $isOpen={isActive}>
-                                <ImgWrapper $border $isActive={isActive}>
-                                    <ChevronSVG />
-                                </ImgWrapper>
-                            </OpenButton>
-                        )}
-                    </SelectOptionWrapper>
-                </ContentHeaderWrapper>
-                {isActive && (
-                    <ContentBodyWrapper>
-                        <ExchangeAddress
-                            handleIsAddressValid={() => true}
-                            handleAddressChanged={() => null}
-                            value={truncateMiddle(base_tari_address, 7, ' ... ')}
-                            disabled
-                        />
-                        <SeasonReward>
-                            <LeftContent>
-                                {universalExchangeMinerOption.campaign_description ? (
-                                    <>
-                                        <SeasonRewardIcon src="/assets/img/wrapped_gift.png" alt="gift" />
-                                        <SeasonRewardText>
-                                            <b>{t('season-one-reward', { ns: 'exchange' })}:</b>{' '}
-                                            <span>{universalExchangeMinerOption.campaign_description}</span>
-                                        </SeasonRewardText>
-                                    </>
-                                ) : null}
-                            </LeftContent>
-                            {universalExchangeMinerOption.reward_expiry_date ? (
-                                <Countdown>
-                                    <CountdownText>
-                                        {formatCountdown(universalExchangeMinerOption.reward_expiry_date)}
-                                    </CountdownText>
-                                </Countdown>
+                        </OpenButton>
+                    )}
+                </SelectOptionWrapper>
+            </ContentHeaderWrapper>
+            {isActive && (
+                <ContentBodyWrapper>
+                    <ExchangeAddress
+                        handleIsAddressValid={() => true}
+                        handleAddressChanged={() => null}
+                        value={truncateMiddle(base_tari_address, 7, ' ... ')}
+                        disabled
+                    />
+                    <SeasonReward>
+                        <LeftContent>
+                            {universalExchangeMinerOption.campaign_description ? (
+                                <>
+                                    <SeasonRewardIcon src="/assets/img/wrapped_gift.png" alt="gift" />
+                                    <SeasonRewardText>
+                                        <b>{t('season-one-reward', { ns: 'exchange' })}:</b>{' '}
+                                        <span>{universalExchangeMinerOption.campaign_description}</span>
+                                    </SeasonRewardText>
+                                </>
                             ) : null}
-                        </SeasonReward>
-                        <ConfirmButton onClick={handleRevertToInternalWallet}>
-                            <Typography variant="h4">{t('confirm', { ns: 'settings' })}</Typography>
-                        </ConfirmButton>
-                    </ContentBodyWrapper>
-                )}
-            </Wrapper>
-        );
-    }
-);
-
-InternalWalletOption.displayName = 'InternalWalletOption';
+                        </LeftContent>
+                        {universalExchangeMinerOption.reward_expiry_date ? (
+                            <Countdown>
+                                <CountdownText>
+                                    {formatCountdown(universalExchangeMinerOption.reward_expiry_date)}
+                                </CountdownText>
+                            </Countdown>
+                        ) : null}
+                    </SeasonReward>
+                    <ConfirmButton onClick={handleRevertToInternalWallet}>
+                        <Typography variant="h4">{t('confirm', { ns: 'settings' })}</Typography>
+                    </ConfirmButton>
+                </ContentBodyWrapper>
+            )}
+        </Wrapper>
+    );
+}
