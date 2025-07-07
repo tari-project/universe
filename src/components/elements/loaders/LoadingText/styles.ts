279d2b46fe31a589934b0b5c2950a2efe43edbdd
Aaron Pepper <pepdog@gmail.com>
2025-07-07 16:53:59 +0700

    adjust loading text colors

--- src/components/elements/loaders/LoadingText/styles.ts
+++ src/components/elements/loaders/LoadingText/styles.ts
@@ -14,8 +14,8 @@ const gradient = keyframes`
 export const AnimatedGradientText = styled.span`
     background: ${({ theme }) =>
         theme.mode === 'light'
-            ? 'linear-gradient(270deg, #000 0%, #D2D2D2 50%, #000 100%)'
-            : 'linear-gradient(270deg, #fff 0%, #5F5F5F 50%, #fff 100%)'};
+            ? 'linear-gradient(270deg, #000 0%, #B4B4B4 50%, #000 100%)'
+            : 'linear-gradient(270deg, #fff 0%, #828282 50%, #fff 100%)'};
     background-size: 200% 200%;
     -webkit-background-clip: text;
     -webkit-text-fill-color: transparent;
