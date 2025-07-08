3fb999cae20ffac29da30de3b02bcd64bab875f5
Shannon Tenner <shanimal@tari.com>
2025-07-08 11:32:55 +0200

    wip: fix mode detection hook

--- src/main.tsx
+++ src/main.tsx
@@ -3,23 +3,25 @@ import { lazy, StrictMode, Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 const AppWrapper = lazy(() => import('./App/AppWrapper.tsx'));
 
-const rootEl = document.getElementById('root')!;
+const rootEl = document.getElementById('root');
 
-const root = createRoot(rootEl, {
-    onUncaughtError: (error, errorInfo) => {
-        console.error('Uncaught error: ', error, errorInfo);
-    },
-    onCaughtError: (error, errorInfo) => {
-        console.error('Caught error: ', error, errorInfo);
-    },
-    onRecoverableError: (error, errorInfo) => {
-        console.error('Recoverable error: ', error, errorInfo);
-    },
-});
-root.render(
-    <StrictMode>
-        <Suspense fallback={<div />}>
-            <AppWrapper />
-        </Suspense>
-    </StrictMode>
-);
+if (rootEl) {
+    const root = createRoot(rootEl, {
+        onUncaughtError: (error, errorInfo) => {
+            console.error('Uncaught error: ', error, errorInfo);
+        },
+        onCaughtError: (error, errorInfo) => {
+            console.error('Caught error: ', error, errorInfo);
+        },
+        onRecoverableError: (error, errorInfo) => {
+            console.error('Recoverable error: ', error, errorInfo);
+        },
+    });
+    root.render(
+        <StrictMode>
+            <Suspense fallback={<div />}>
+                <AppWrapper />
+            </Suspense>
+        </StrictMode>
+    );
+}
