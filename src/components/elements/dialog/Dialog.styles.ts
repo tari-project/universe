9549d49d4c4ba1ed0571e221eec99056a3451bf1
Shannon Tenner <shanimal@tari.com>
2025-07-16 11:53:49 +0200

    add webkit backdrop fitler on dialog

--- src/components/elements/dialog/Dialog.styles.ts
+++ src/components/elements/dialog/Dialog.styles.ts
@@ -12,6 +12,7 @@ export interface ContentWrapperProps {
 export const ContentWrapper = styled.div<ContentWrapperProps>`
     background-color: ${({ theme }) => theme.palette.background.paper};
     backdrop-filter: blur(20px);
+    -webkit-backdrop-filter: blur(20px);
     border-radius: ${({ theme, $borderRadius }) => $borderRadius || theme.shape.borderRadius.dialog};
     box-shadow: 0 4px 45px 0 rgba(0, 0, 0, 0.08);
     display: flex;
