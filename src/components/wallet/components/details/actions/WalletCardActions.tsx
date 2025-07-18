952225cd4bc2641c7af005929cd3c0462fe9cc6c
Aaron Pepper <pepdog@gmail.com>
2025-07-18 14:50:40 -0400

    remove sync to phone icon and added it to settings section

--- src/components/wallet/components/details/actions/WalletCardActions.tsx
+++ src/components/wallet/components/details/actions/WalletCardActions.tsx
@@ -1,14 +1,9 @@
-import { useConfigUIStore } from '@app/store';
 import ActionCopyAddress from './ActionCopyAddress';
-import ActionPhoneSync from './ActionPhoneSync';
 import { Wrapper } from './styles.ts';
-import { WalletUIMode } from '@app/types/events-payloads.ts';
 
 export default function WalletCardActions() {
-    const isStandardWalletUI = useConfigUIStore((s) => s.wallet_ui_mode === WalletUIMode.Standard);
     return (
         <Wrapper>
-            {isStandardWalletUI && <ActionPhoneSync />}
             <ActionCopyAddress />
         </Wrapper>
     );
