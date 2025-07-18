876a389b6108a88b79f73525b37388a5a9b8657b
Misieq01 <bartosz2000walczak@gmail.com>
2025-07-18 13:03:43 +0200

    change migration_id to version and default value to 0

--- src-tauri/src/configs/config_core.rs
+++ src-tauri/src/configs/config_core.rs
@@ -49,7 +49,7 @@ static INSTANCE: LazyLock<RwLock<ConfigCore>> = LazyLock::new(|| RwLock::new(Con
 #[derive(Getters, Setters)]
 #[getset(get = "pub", set = "pub")]
 pub struct ConfigCoreContent {
-    migration_id: u32,
+    version: u32,
     was_config_migrated: bool,
     created_at: SystemTime,
     is_p2pool_enabled: bool,
@@ -104,7 +104,7 @@ impl Default for ConfigCoreContent {
             .unwrap_or(ABTestSelector::GroupA);
 
         Self {
-            migration_id: 1,
+            version: 0,
             was_config_migrated: false,
             created_at: SystemTime::now(),
             is_p2pool_enabled: true,
@@ -145,9 +145,9 @@ impl ConfigCore {
         let mut config = Self::current().write().await;
         config.load_app_handle(app_handle.clone()).await;
 
-        if config.content.migration_id.eq(&0) {
+        if config.content.version.eq(&0) {
             config.content.node_type = NodeType::RemoteUntilLocal;
-            config.content.migration_id = 1;
+            config.content.version = 1;
             let _unused = Self::_save_config(config._get_content().clone());
         };
     }
