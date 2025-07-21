772deb36876b8a6ec5696e11ea343fb0c8b62699
Shannon Tenner <shanimal@tari.com>
2025-07-21 16:01:57 +0200

    wip: loader

--- src/components/elements/loaders/CircularProgress/Progress.tsx
+++ src/components/elements/loaders/CircularProgress/Progress.tsx
@@ -6,15 +6,14 @@ interface ProgressProps {
     percentage?: number;
     isInfinite?: boolean;
 }
-
-const Wrapper = styled.div`
+const Wrapper = styled(m.div)`
     margin: auto;
     width: 28px;
     height: 28px;
     display: flex;
     align-items: center;
     justify-content: center;
-    color: ${({ theme }) => theme.palette.success.main};
+    color: ${({ theme }) => theme.colors.greyscale[50]};
     svg {
         display: flex;
         width: 100%;
@@ -24,32 +23,31 @@ const Wrapper = styled.div`
 
 export function Progress({ percentage = 0, isInfinite = false }: ProgressProps) {
     const infinite = isInfinite || percentage === 0;
-
-    const transition: Transition = infinite
-        ? { repeat: Infinity, duration: 2 }
-        : { type: 'spring', damping: 3, stiffness: 30 };
+    const transition: Transition = { repeat: Infinity, duration: 4, ease: 'linear' };
 
     const animate = !infinite
         ? { pathLength: percentage / 100 }
         : {
-              pathLength: [0, 0.3, 0.8, 0.8, 0.999],
-              rotate: [0, 300, 360],
-              opacity: [null, 0.4, 0.9, 1],
+              pathLength: [null, 0.3, 0.45, 0.88, 0.89],
+              opacity: [null, 0.3, 0.45, 1, 0.89],
+              tiems: [0, 0.3, 0.45, 0.88, 1],
           };
 
     return (
-        <Wrapper>
+        <Wrapper animate={{ rotate: 360 }} transition={{ ...transition, ease: 'anticipate' }}>
             <m.svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <m.circle cx="14" cy="14" r="12" stroke="white" strokeWidth="3.84" opacity={0.3} />
+                <circle cx="14" cy="14" r="12" stroke="white" strokeWidth="3.84" opacity={0.3} />
                 <m.circle
                     cx="14"
                     cy="14"
                     r="12"
                     strokeLinecap="round"
                     strokeLinejoin="bevel"
-                    stroke="white"
+                    stroke="currentcolor"
+                    fill="none"
                     strokeWidth="3.84"
-                    style={{ rotate: -90 }}
+                    opacity={0}
+                    pathLength={0}
                     animate={animate}
                     transition={transition}
                 />
