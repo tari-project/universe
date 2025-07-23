cdb449e761cf6ae48a1b12beccd27e8fef356211
Aaron Pepper <pepdog@gmail.com>
2025-07-23 19:17:05 +0700

    fix(ui): remove sync to phone icon from wallet (#2579)
    https://github.com/tari-project/universe/issues/2557
    
    This PR removes the Phone icon from the wallet and adds a "Sync to
    Phone" option in the Settings - Wallet section.
    
    Note: This does **NOT** add the auto trigger for the modal "Modal should
    pop up when you have been mining for at least one hour and when you win
    your first 20xtm".
    
    Im gonna push back on auto-triggering the modal. It would be annoying.
    There are better UI solutions, we should have a `Setup Checklist UI` to
    guide users through this kinda stuff.
    
    - Removed the Sync to Phone icon from the wallet
    
    <img width="1038" height="672" alt="CleanShot 2025-07-18 at 14 51 39@2x"
    src="https://github.com/user-attachments/assets/4dd35e6d-1446-4b2a-a34c-fa234e1567e2"
    />
    
    - Added it to the Wallet Settings section
    
    <img width="2540" height="1586" alt="CleanShot 2025-07-18 at 14 52
    12@2x"
    src="https://github.com/user-attachments/assets/38afa21c-6d7b-4d07-accd-d57d44f68b87"
    />
    
    ---------
    
    Co-authored-by: shan <47271333+shanimal08@users.noreply.github.com>
    Co-authored-by: Shannon Tenner <shanimal@tari.com>

--- src/components/wallet/components/details/actions/WalletCardActions.tsx
+++ src/components/wallet/components/details/actions/WalletCardActions.tsx
@@ -1,16 +1,9 @@
-import { useConfigUIStore, useWalletStore } from '@app/store';
 import ActionCopyAddress from './ActionCopyAddress';
-import ActionPhoneSync from './ActionPhoneSync';
 import { Wrapper } from './styles.ts';
-import { WalletUIMode } from '@app/types/events-payloads.ts';
 
 export default function WalletCardActions() {
-    const isStandardWalletUI = useConfigUIStore((s) => s.wallet_ui_mode === WalletUIMode.Standard);
-    const isWalletScanning = useWalletStore((s) => s.wallet_scanning?.is_scanning);
-
     return (
         <Wrapper>
-            {isStandardWalletUI && !isWalletScanning && <ActionPhoneSync />}
             <ActionCopyAddress />
         </Wrapper>
     );
