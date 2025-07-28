aceb56882f50d3964773f335efe9da8debb123b6
Martins Erts <fp@tari.com>
2025-07-28 16:33:03 +0300

    Add ootle actions

---
+++ src/store/actions/ootleActions.ts
@@ -0,0 +1,44 @@
+import { invoke } from '@tauri-apps/api/core';
+import {
+    AccountsCreateRequest,
+    AccountsCreateResponse,
+    AccountsGetBalancesRequest,
+    AccountsGetBalancesResponse,
+    AccountsListRequest,
+    AccountsListResponse,
+} from '@tari-project/typescript-bindings';
+import { setError } from './appStateStoreActions';
+
+export const ootleListAccounts = async (request: AccountsListRequest): Promise<AccountsListResponse | undefined> => {
+    try {
+        return await invoke('ootle_list_accounts', request);
+    } catch (e) {
+        const message = 'Could not list Ootle accounts';
+        console.error(message, e);
+        setError(message);
+    }
+};
+
+export const ootleCreateAccount = async (
+    request: AccountsCreateRequest
+): Promise<AccountsCreateResponse | undefined> => {
+    try {
+        return await invoke('ootle_create_account', request);
+    } catch (e) {
+        const message = 'Could not create Ootle account';
+        console.error(message, e);
+        setError(message);
+    }
+};
+
+export const ootleGetBalances = async (
+    request: AccountsGetBalancesRequest
+): Promise<AccountsGetBalancesResponse | undefined> => {
+    try {
+        return await invoke('ootle_get_balances', request);
+    } catch (e) {
+        const message = 'Could not get Ootle balances';
+        console.error(message, e);
+        setError(message);
+    }
+};
