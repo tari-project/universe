04e366d1b501c7021c3a3ffbafa4b1c92062aaaf
karczuRF <oskar.karcz@rumblefish.pl>
2025-07-30 11:35:24 +0200

    extract csp directives and display as bullet list on modal

--- src/containers/floating/security/tapplet/TappletCspDialog.tsx
+++ src/containers/floating/security/tapplet/TappletCspDialog.tsx
@@ -1,18 +1,27 @@
 import { useSecurityStore } from '@app/store';
 import { Dialog, DialogContent } from '@app/components/elements/dialog/Dialog.tsx';
-import { Header, Wrapper } from '../common.styles.ts';
+import { CTAWrapper, Header, Wrapper } from '../common.styles.ts';
 import CloseButton from '@app/components/elements/buttons/CloseButton.tsx';
 import { useTranslation } from 'react-i18next';
 import { emit } from '@tauri-apps/api/event';
 import { TextWrapper } from '@app/components/sync/styles.ts';
 import { TextButton } from '@app/components/elements/buttons/TextButton.tsx';
+import { Button } from '@app/components/elements/buttons/Button.tsx';
+
+function extractCspDirectives(csp: string): string[] {
+    return csp
+        .split(';')
+        .map((directive) => directive.trim())
+        .filter((directive) => directive.length > 0);
+}
 
 export default function TappletCspDialog() {
     const { t } = useTranslation('staged-security');
     const modal = useSecurityStore((s) => s.modal);
-    const tappletCsp = useSecurityStore((s) => s.tappletCsp);
     const setModal = useSecurityStore((s) => s.setModal);
     const setTappletCsp = useSecurityStore((s) => s.setTappletCsp);
+    const tappletCsp = useSecurityStore((s) => s.tappletCsp);
+    const tappletCspList = extractCspDirectives(tappletCsp);
 
     const isOpen = modal === 'allow_tapplet_csp';
 
@@ -37,9 +46,30 @@ export default function TappletCspDialog() {
                     <Header>
                         <CloseButton onClick={handleClose} />
                     </Header>
-                    <TextWrapper>{tappletCsp}</TextWrapper>
-                    <TextButton onClick={handleSubmit}>{'Allow CSP'}</TextButton>
-                    <TextButton onClick={handleClose}>{'Reject CSP'}</TextButton>
+                    <TextWrapper>
+                        <ul style={{ paddingLeft: '1.2em', margin: 0 }}>
+                            {tappletCspList.map((directive, index) => (
+                                <li key={index}>{directive}</li>
+                            ))}
+                        </ul>
+                    </TextWrapper>
+                </Wrapper>
+                <Wrapper>
+                    <CTAWrapper>
+                        <Button
+                            onClick={handleSubmit}
+                            variant="black"
+                            fluid
+                            size="xlarge"
+                            disabled={!tappletCsp}
+                            type="submit"
+                        >
+                            {t('Allow Tapplet CSP')}
+                        </Button>
+                        <TextButton color="greyscale" onClick={handleClose}>
+                            {'Reject Tapplet CSP'}
+                        </TextButton>
+                    </CTAWrapper>
                 </Wrapper>
             </DialogContent>
         </Dialog>
