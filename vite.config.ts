c45489ec6ef0cc2dca1efbcea110c71fe7799d0d
Shannon Tenner <shanimal@tari.com>
2025-08-07 15:06:43 +0200

    rm logger

--- vite.config.ts
+++ vite.config.ts
@@ -4,52 +4,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import eslintPlugin from '@nabla/vite-plugin-eslint';
 import react from '@vitejs/plugin-react';
 
-const compilerLogFiles = [];
-
-function logEvent(filename, event) {
-    const shouldLog = event.kind !== 'CompileSuccess';
-    if (shouldLog) {
-        if (!compilerLogFiles.includes(filename)) {
-            compilerLogFiles.push(filename);
-        }
-        console.log(
-            `\n==========================================================Compiler==========================================================`
-        );
-        const line = event?.detail?.loc?.start?.line;
-        console.log(`[Compiler] ${event.kind}: ${filename}${line ? `:${line}` : ''}`);
-
-        if (event.detail?.severity) {
-            console.log('[Compiler] Severity:', event.detail.severity);
-        }
-        if (event.detail?.reason) {
-            console.log('[Compiler] Reason:', event.detail.reason);
-        }
-        if (event.detail?.description) {
-            console.log('[Compiler] Description:', event.detail.description);
-        }
-        if (event.detail?.suggestions) {
-            console.log('[Compiler] Suggestions:', event.detail.suggestions);
-        }
-
-        console.log(
-            `============================================================End============================================================\n`
-        );
-        if (compilerLogFiles.length) {
-            console.log(
-                `\n==========================================================Compiler Totals (${compilerLogFiles.length})==========================================================`
-            );
-            console.log(`[Compiler] Total Files: ${compilerLogFiles.length}`);
-
-            compilerLogFiles.forEach((file) => {
-                console.log(`[Compiler] files: ${file}`);
-            });
-            console.log(
-                `============================================================Totals End============================================================\n`
-            );
-        }
-    }
-}
-
 const plugins: UserConfig['plugins'] = [
     react({
         babel: {
@@ -61,16 +15,7 @@ const plugins: UserConfig['plugins'] = [
                         fileName: true,
                     },
                 ],
-                [
-                    'babel-plugin-react-compiler',
-                    {
-                        logger: {
-                            logEvent(filename, event) {
-                                logEvent(filename, event);
-                            },
-                        },
-                    },
-                ],
+                ['babel-plugin-react-compiler'],
             ],
         },
     }),
