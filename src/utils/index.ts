ee1b72dd91b67d93565a5d7cb14d19b8c9e6ab54
ns212 <nck@tari.com>
2025-08-12 11:33:19 +0000

    feat: add X-Requested-With header

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,5 +5,5 @@ export * from './shared-logger.ts';
 export * from './truncateString.ts';
 
 export const defaultHeaders = {
-  'X-Requested-With': `TariUniverse/${import.meta.env.VITE_TARI_UNIVERSE_VERSION}`
+  'X-Requested-With': `TariUniverse/${import.meta.env.VITE_TARI_UNIVERSE_VERSION || 'unknown' }`
 }
