f88abe1dc271e1572ea8ccfaee3f3b8bbce285db
Misieq01 <bartosz2000walczak@gmail.com>
2025-08-13 12:16:28 +0200

    fix import

--- src-tauri/src/setup/phase_core.rs
+++ src-tauri/src/setup/phase_core.rs
@@ -39,6 +39,7 @@ use crate::{
     setup::setup_manager::SetupPhase,
     tasks_tracker::TasksTrackers,
     utils::{network_status::NetworkStatus, platform_utils::PlatformUtils},
+    UniverseAppState,
 };
 
 use super::{
