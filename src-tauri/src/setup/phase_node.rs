875a2c0b7240bc65c2e09f53944a5394df42bece
Misieq01 <bartosz2000walczak@gmail.com>
2025-08-18 12:19:09 +0200

    fix lint

--- src-tauri/src/setup/phase_node.rs
+++ src-tauri/src/setup/phase_node.rs
@@ -27,10 +27,7 @@ use crate::{
     configs::{config_core::ConfigCore, trait_config::ConfigImpl},
     events_emitter::EventsEmitter,
     events_manager::EventsManager,
-    node::{
-        self,
-        node_manager::{NodeManagerError, STOP_ON_ERROR_CODES},
-    },
+    node::node_manager::{NodeManagerError, STOP_ON_ERROR_CODES},
     progress_trackers::{
         progress_plans::{ProgressPlans, ProgressSetupNodePlan},
         progress_stepper::ProgressStepperBuilder,
