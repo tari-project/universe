ddad25996d7f5569e99c57b551d1f2934df61ca2
Shannon Tenner <shanimal@tari.com>
2025-08-25 14:52:51 +0200

    wip: neaten tx merging

--- src/hooks/wallet/helpers.ts
+++ src/hooks/wallet/helpers.ts
@@ -1,14 +1,17 @@
 import { TransactionInfo } from '@app/types/app-status.ts';
-import { BackendBridgeTransaction, CombinedBridgeWalletTransaction } from '@app/store';
+import { BackendBridgeTransaction, CombinedBridgeWalletTransaction, useWalletStore } from '@app/store';
+import { TariAddressType } from '@app/types/events-payloads.ts';
 
-interface MergeListArgs {
+interface MergeTransactionsArgs {
     walletTransactions: TransactionInfo[];
     bridgeTransactions?: BackendBridgeTransaction[];
-    coldWalletAddress?: string;
 }
-export function convertWalletTransactionToCombinedTransaction(
-    transaction: TransactionInfo
-): CombinedBridgeWalletTransaction {
+
+interface CheckTransactionsArgs {
+    walletTransactions: CombinedBridgeWalletTransaction[];
+    bridgeTransactions?: BackendBridgeTransaction[];
+}
+function convertWalletTransactionToCombinedTransaction(transaction: TransactionInfo): CombinedBridgeWalletTransaction {
     const txDetails = {
         ...transaction,
         txId: transaction.tx_id,
@@ -29,48 +32,72 @@ export function convertWalletTransactionToCombinedTransaction(
     };
 }
 
+function getBridgeItemIndex(bridgeTx: BackendBridgeTransaction, walletTxs: CombinedBridgeWalletTransaction[]) {
+    if (bridgeTx.paymentId) {
+        // Currently we can find the bridge transaction by paymentId
+        return walletTxs.findIndex((walletTx) => walletTx.paymentId === bridgeTx.paymentId);
+    }
+    // Index found by paymentId should be always preferred, but if it is not found we use the deprecated method if exists
+    const coldWalletAddress = useWalletStore.getState().cold_wallet_address;
+    // If the bridge transaction has no paymentId, we try to find it by tokenAmount and destinationAddress which should equal the cold wallet address
+    // This supports older transactions that might not have a paymentId
+    return walletTxs.findIndex(
+        (walletTx) =>
+            walletTx.tokenAmount === Number(bridgeTx.tokenAmount) && walletTx.destinationAddress === coldWalletAddress
+    );
+}
+
 export function mergeTransactionLists({
     walletTransactions,
     bridgeTransactions,
-    coldWalletAddress,
-}: MergeListArgs): CombinedBridgeWalletTransaction[] {
+}: MergeTransactionsArgs): CombinedBridgeWalletTransaction[] {
     const mergedWalletTransactions: CombinedBridgeWalletTransaction[] = walletTransactions.map(
         convertWalletTransactionToCombinedTransaction
     );
 
     bridgeTransactions?.forEach((bridgeTx) => {
-        // If the bridge transaction has no paymentId, we try to find it by tokenAmount and destinationAddress which should equal the cold wallet address
-        // This supports older transactions that might not have a paymentId
-        const depracatedWalletTransactionIndex = mergedWalletTransactions.findIndex(
-            (tx) =>
-                !bridgeTx.paymentId &&
-                tx.tokenAmount === Number(bridgeTx.tokenAmount) &&
-                tx.destinationAddress === coldWalletAddress
-        );
-
-        // Currently we can find the bridge transaction by paymentId
-        const originalWalletBridgeTransactionIndex = mergedWalletTransactions.findIndex(
-            (tx) => tx.paymentId === bridgeTx.paymentId
-        );
-
-        // Index found by paymentId should be always preferred, but if it is not found we use the deprecated method if exists
-        const walletBridgeTransactionIndex =
-            originalWalletBridgeTransactionIndex >= 0
-                ? originalWalletBridgeTransactionIndex
-                : depracatedWalletTransactionIndex;
-
+        const bridgeIndex = getBridgeItemIndex(bridgeTx, mergedWalletTransactions);
         // Don't process if we can't find the transaction in the wallet transactions
-        if (walletBridgeTransactionIndex < 0) return;
+        if (bridgeIndex < 0) return;
 
-        mergedWalletTransactions[walletBridgeTransactionIndex] = {
-            ...mergedWalletTransactions[walletBridgeTransactionIndex],
-            bridgeTransactionDetails: {
-                status: bridgeTx.status,
-                transactionHash: bridgeTx.transactionHash,
-                amountAfterFee: bridgeTx.amountAfterFee,
-            },
+        const baseTransactionDetails = mergedWalletTransactions[bridgeIndex];
+
+        // Don't process if it has already been processed
+        if (baseTransactionDetails?.bridgeTransactionDetails?.transactionHash) return;
+
+        const bridgeTransactionDetails = {
+            status: bridgeTx.status,
+            transactionHash: bridgeTx.transactionHash,
+            amountAfterFee: bridgeTx.amountAfterFee,
+        };
+
+        mergedWalletTransactions[bridgeIndex] = {
+            ...baseTransactionDetails,
+            bridgeTransactionDetails,
         };
     });
 
     return mergedWalletTransactions;
+}
+
+export function shouldRefetchBridgeItems({ walletTransactions, bridgeTransactions }: CheckTransactionsArgs): boolean {
+    const coldWalletAddress = useWalletStore.getState().cold_wallet_address;
+    const tariAddressType = useWalletStore.getState().tari_address_type;
+
+    const isThereANewBridgeTransaction = !!walletTransactions.find(
+        (tx) =>
+            tx.destinationAddress === coldWalletAddress &&
+            !bridgeTransactions?.some(
+                (bridgeTx) => bridgeTx.paymentId === tx.paymentId && Number(bridgeTx.tokenAmount) === tx.tokenAmount
+            )
+    );
+
+    const isThereEmptyBridgeTransactionAndFoundInWallet = !!walletTransactions.find(
+        (tx) => tx.destinationAddress === coldWalletAddress && bridgeTransactions?.length === 0
+    );
+
+    return (
+        tariAddressType === TariAddressType.Internal &&
+        (isThereANewBridgeTransaction || isThereEmptyBridgeTransactionAndFoundInWallet)
+    );
 }
--- src/hooks/wallet/useFetchTxHistory.ts
+++ src/hooks/wallet/useFetchTxHistory.ts
@@ -3,7 +3,7 @@ import { queryClient } from '@app/App/queryClient.ts';
 import { BackendBridgeTransaction, useConfigBEInMemoryStore, useWalletStore } from '@app/store';
 import { fetchTransactionsHistory } from '@app/store/actions/walletStoreActions';
 import { fetchBridgeTransactionsHistory } from '@app/store/actions/bridgeApiActions.ts';
-import { mergeTransactionLists } from './helpers.ts';
+import { mergeTransactionLists, shouldRefetchBridgeItems } from './helpers.ts';
 
 export const KEY_TX = `transactions`;
 export const KEY_BRIDGE_TX = `bridge`;
@@ -11,13 +11,12 @@ export const KEY_BRIDGE_TX = `bridge`;
 export function useFetchTxHistory() {
     const baseUrl = useConfigBEInMemoryStore((s) => s.bridge_backend_api_url);
     const walletAddress = useWalletStore((state) => state.tari_address_base58);
-    const coldWalletAddress = useWalletStore((s) => s.cold_wallet_address);
     const filter = useWalletStore((s) => s.tx_history_filter);
 
     const queriesEnabled = Boolean(walletAddress?.length);
     const baseQueryKeys = [KEY_TX, `walletAddress: ${walletAddress}`];
 
-    const { data: bridgeTransactions } = useQuery<BackendBridgeTransaction[]>({
+    const { data: bridgeTransactions, refetch: refetchBridgeTxs } = useQuery<BackendBridgeTransaction[]>({
         queryKey: [...baseQueryKeys, KEY_BRIDGE_TX],
         queryFn: async () => await fetchBridgeTransactionsHistory(walletAddress),
         enabled: !!baseUrl?.length && queriesEnabled,
@@ -29,7 +28,14 @@ export function useFetchTxHistory() {
             const limit = 20;
             const offset = limit * (pageParam as number);
             const walletTransactions = await fetchTransactionsHistory({ filter, offset, limit });
-            return mergeTransactionLists({ walletTransactions, bridgeTransactions, coldWalletAddress });
+
+            let mergedList = mergeTransactionLists({ walletTransactions, bridgeTransactions });
+
+            if (shouldRefetchBridgeItems({ walletTransactions: mergedList, bridgeTransactions })) {
+                await refetchBridgeTxs();
+                mergedList = mergeTransactionLists({ walletTransactions, bridgeTransactions });
+            }
+            return mergedList;
         },
         enabled: queriesEnabled,
         initialPageParam: 0,
