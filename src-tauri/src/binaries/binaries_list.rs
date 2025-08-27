029debdf91e1d81086c2f974cfa8b00a2a81da58
Misieq01 <bartosz2000walczak@gmail.com>
2025-08-27 10:24:02 +0200

    mvp

--- src-tauri/src/binaries/binaries_list.rs
+++ src-tauri/src/binaries/binaries_list.rs
@@ -72,7 +72,7 @@ impl Binaries {
     }
 
     pub fn binary_file_name(self, version: String) -> PathBuf {
-        match self {
+        let base_path = match self {
             Binaries::Xmrig => {
                 let file_name = format!("xmrig-{version}");
                 PathBuf::from(file_name).join("xmrig")
@@ -109,7 +109,9 @@ impl Binaries {
                 let file_name = "graxil";
                 PathBuf::from(file_name)
             }
-        }
+        };
+
+        base_path
     }
 
     #[allow(clippy::too_many_lines)]
