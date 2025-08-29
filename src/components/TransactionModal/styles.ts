b59cbefed53023976870a5dfa6fc49227961a8bd
Shannon Tenner <shanimal@tari.com>
2025-08-29 12:18:07 +0200

    wip: fix conflicting popovers

--- src/components/TransactionModal/styles.ts
+++ src/components/TransactionModal/styles.ts
@@ -1,44 +1,5 @@
 import * as m from 'motion/react-m';
-import styled, { css } from 'styled-components';
-
-export const Wrapper = styled('div')`
-    position: fixed;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    left: 0;
-    z-index: 999;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    pointer-events: all;
-
-    overflow: hidden;
-    overflow-y: auto;
-
-    padding: 40px;
-
-    @media (max-height: 800px) {
-        align-items: flex-start;
-    }
-`;
-
-export const Cover = styled(m.div)<{ $noClose?: boolean }>`
-    position: fixed;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    left: 0;
-    background: ${({ theme }) => theme.colorsAlpha.darkAlpha[50]};
-    z-index: 0;
-    cursor: pointer;
-
-    ${({ $noClose }) =>
-        $noClose &&
-        css`
-            cursor: default;
-        `}
-`;
+import styled from 'styled-components';
 
 export const BoxWrapper = styled(m.div)`
     width: 100%;
@@ -48,12 +9,12 @@ export const BoxWrapper = styled(m.div)`
     justify-content: flex-end;
     flex-shrink: 0;
     gap: 20px;
-    padding: 30px;
-    border-radius: 20px;
-    box-shadow: 0 4px 74px 0 rgba(0, 0, 0, 0.15);
+    //padding: 30px;
+    //border-radius: 20px;
+    //box-shadow: 0 4px 74px 0 rgba(0, 0, 0, 0.15);
     position: relative;
-    z-index: 1;
-    background-color: ${({ theme }) => theme.palette.background.splash};
+    //z-index: 1;
+    //background-color: ${({ theme }) => theme.palette.background.splash};
 `;
 
 export const TopButton = styled('button')`
