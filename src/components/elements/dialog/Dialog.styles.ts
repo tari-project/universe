7ec7565ad3cf2606402eeb9ca4869dc62423bad0
Aaron Pepper <pepdog@gmail.com>
2025-09-04 18:39:11 +0700

    set modal z-index to a higher number

--- src/components/elements/dialog/Dialog.styles.ts
+++ src/components/elements/dialog/Dialog.styles.ts
@@ -7,7 +7,7 @@ export const Overlay = styled(FloatingOverlay)`
     display: flex;
     align-items: center;
     justify-content: center;
-    z-index: 1;
+    z-index: 9;
 `;
 
 export const ContentWrapper = styled.div<ContentWrapperStyleProps>`
