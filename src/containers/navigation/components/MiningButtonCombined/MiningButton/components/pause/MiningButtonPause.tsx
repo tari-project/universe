877514b0f0fd116aa6e7e4bd1ab8b3253a3fc5c3
Shannon Tenner <shanimal@tari.com>
2025-09-19 15:00:47 +0200

    styling and rm stop icon

--- src/containers/navigation/components/MiningButtonCombined/MiningButton/components/pause/MiningButtonPause.tsx
+++ src/containers/navigation/components/MiningButtonCombined/MiningButton/components/pause/MiningButtonPause.tsx
@@ -5,19 +5,34 @@ import { offset, useClick, useDismiss, useFloating, useInteractions, useRole } f
 import MiningButton from '../../MiningButton.tsx';
 import PauseIcon from '../../../icons/PauseIcon.tsx';
 
-import { IconWrapper, Options, OptionText, OptionWrapper, TriggerWrapper } from './styles.ts';
-import { Trans, useTranslation } from 'react-i18next';
+import { IconWrapper, Options, OptionText, OptionWrapper, TimerAccent, TriggerWrapper } from './styles.ts';
+import { Trans } from 'react-i18next';
 import { TimerIcon } from './TimerIcon.tsx';
 import { PauseOutlineIcon } from './PauseOutlineIcon.tsx';
+import { stopMining } from '@app/store';
 
 interface MiningButtonPauseProps {
     isMining: boolean;
     isMiningButtonDisabled?: boolean;
 }
 
+const container = {
+    hidden: { opacity: 0 },
+    show: {
+        opacity: 1,
+        transition: {
+            staggerChildren: 0.1,
+        },
+    },
+};
+
+const item = {
+    hidden: { opacity: 0, y: -4 },
+    show: { opacity: 1, y: 0 },
+};
+
 export default function MiningButtonPause({ isMining, isMiningButtonDisabled }: MiningButtonPauseProps) {
     const [showPauseOptions, setShowPauseOptions] = useState(false);
-
     const { refs, floatingStyles, context } = useFloating({
         open: showPauseOptions,
         onOpenChange: setShowPauseOptions,
@@ -31,16 +46,43 @@ export default function MiningButtonPause({ isMining, isMiningButtonDisabled }: 
     const { getReferenceProps, getFloatingProps } = useInteractions([click, dismiss, role]);
 
     const handleStopMining = useCallback(async () => {
-        setShowPauseOptions((c) => !c);
-        // await stopMining();
+        await stopMining();
     }, []);
+
+    const handlePause = useCallback(async (hours: number) => {
+        console.info(`Pausing for ${hours} hours!`);
+        await stopMining(); // TODO: add pause with duration logic
+    }, []);
+
+    function buttonClick() {
+        setShowPauseOptions((c) => !c);
+    }
+
+    function renderDurationOption(hours: number) {
+        return (
+            <OptionWrapper variants={item} onClick={() => handlePause(hours)}>
+                <IconWrapper>
+                    <TimerIcon />
+                    <TimerAccent>{hours}</TimerAccent>
+                </IconWrapper>
+                <OptionText>
+                    <Trans
+                        ns="mining-view"
+                        i18nKey="pause.for-duration"
+                        components={{ strong: <strong /> }}
+                        values={{ duration: `${hours} hours` }}
+                    />
+                </OptionText>
+            </OptionWrapper>
+        );
+    }
 
     return (
         <>
             <TriggerWrapper ref={refs.setReference} {...getReferenceProps()}>
                 <MiningButton
                     buttonText="pause-mining"
-                    onClick={handleStopMining}
+                    onClick={buttonClick}
                     disabled={isMiningButtonDisabled}
                     icon={<PauseIcon />}
                     isMining={isMining}
@@ -48,34 +90,18 @@ export default function MiningButtonPause({ isMining, isMiningButtonDisabled }: 
             </TriggerWrapper>
             <AnimatePresence>
                 {showPauseOptions && (
-                    <Options ref={refs.setFloating} style={floatingStyles} {...getFloatingProps()}>
-                        <OptionWrapper>
-                            <IconWrapper>
-                                <TimerIcon />
-                            </IconWrapper>
-                            <OptionText>
-                                <Trans
-                                    ns="mining-view"
-                                    i18nKey="pause.for-duration"
-                                    components={{ strong: <strong /> }}
-                                    values={{ duration: `2 hours` }}
-                                />
-                            </OptionText>
-                        </OptionWrapper>
-                        <OptionWrapper>
-                            <IconWrapper>
-                                <TimerIcon />
-                            </IconWrapper>
-                            <OptionText>
-                                <Trans
-                                    ns="mining-view"
-                                    i18nKey="pause.for-duration"
-                                    components={{ strong: <strong /> }}
-                                    values={{ duration: `8 hours` }}
-                                />
-                            </OptionText>
-                        </OptionWrapper>
-                        <OptionWrapper>
+                    <Options
+                        ref={refs.setFloating}
+                        variants={container}
+                        initial="hidden"
+                        animate="show"
+                        exit="hidden"
+                        style={floatingStyles}
+                        {...getFloatingProps()}
+                    >
+                        {renderDurationOption(2)}
+                        {renderDurationOption(8)}
+                        <OptionWrapper variants={item} onClick={handleStopMining}>
                             <IconWrapper>
                                 <PauseOutlineIcon />
                             </IconWrapper>
--- src/containers/navigation/components/MiningButtonCombined/MiningButton/components/pause/styles.ts
+++ src/containers/navigation/components/MiningButtonCombined/MiningButton/components/pause/styles.ts
@@ -9,35 +9,54 @@ export const Options = styled(m.div)`
     padding: 10px;
     flex-direction: column;
     align-items: flex-start;
-    gap: 2px;
+    gap: 4px;
     border-radius: 10px;
-    background: ${({ theme }) => theme.palette.background.paper};
+    background: ${({ theme }) => theme.palette.background.tooltip};
     box-shadow: 0 10px 50px 0 rgba(0, 0, 0, 0.3);
     backdrop-filter: blur(12px);
     z-index: 2;
 `;
 
-export const OptionWrapper = styled(m.div)`
+export const OptionWrapper = styled(m.button)`
     width: 100%;
     display: grid;
     gap: 8px;
-    padding: 6px 7px;
+    height: 36px;
+    padding: 4px 7px;
     align-items: center;
     grid-template-rows: 1fr;
     grid-template-columns: 22px 1fr;
+    background-color: ${({ theme }) => theme.palette.background.tooltip};
+    border-radius: 8px;
+    transition: background-color 0.2s linear;
+    &:hover {
+        background-color: ${({ theme }) => theme.palette.background.accent};
+    }
 `;
 
+export const TimerAccent = styled.div`
+    color: ${({ theme }) => theme.palette.text.accent};
+    position: absolute;
+    font-weight: 700;
+    font-size: 11px;
+    line-height: 1;
+    bottom: -3px;
+    right: 1px;
+    text-align: right;
+`;
 export const IconWrapper = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+    position: relative;
 
     svg {
         display: flex;
-        max-height: 100%;
+        max-width: 100%;
     }
 `;
 export const OptionText = styled.span`
     font-size: 14px;
+    line-height: 1.2;
     color: ${({ theme }) => theme.palette.text.primary};
 `;
