43fe82130ff609ccfd0f211e90516b402b570517
Shannon Tenner <shanimal@tari.com>
2025-10-09 11:14:20 +0200

    add translation key vals

--- src/containers/navigation/components/MiningButtonCombined/MiningButton/components/tooltip/EcoAlert.tsx
+++ src/containers/navigation/components/MiningButtonCombined/MiningButton/components/tooltip/EcoAlert.tsx
@@ -2,28 +2,30 @@ import { Button } from '@app/components/elements/buttons/Button.tsx';
 import { BodyTextWrapper, CTAContent, CTASection, Icon, Title, Wrapper } from './styles.ts';
 import { Typography } from '@app/components/elements/Typography.tsx';
 import turboIcon from '@app/assets/icons/emoji/tornado.png';
+import { useTranslation } from 'react-i18next';
 
 interface EcoAlertProps {
     onAllClick: () => void;
     onTurboClick: () => void;
 }
 export default function EcoAlert({ onAllClick, onTurboClick }: EcoAlertProps) {
+    const { t } = useTranslation('mining-view');
     return (
         <Wrapper>
-            <Title>{`Not getting enough rewards?`}</Title>
+            <Title>{t('modes.eco.alert_title')}</Title>
             <BodyTextWrapper>
-                <Typography variant="p">{`Eco mode only uses a low percentage of system resources.`}</Typography>
-                <Typography variant="p">{`Try turning the power mode up.`}</Typography>
+                <Typography variant="p">{t('modes.eco.alert_text')}</Typography>
+                <Typography variant="p">{t('modes.eco.alert_text_extra')}</Typography>
             </BodyTextWrapper>
             <CTASection>
                 <Button size="xs" fluid onClick={onAllClick}>
                     <CTAContent>
-                        <Typography>{`See all modes`}</Typography>
+                        <Typography>{t('modes.eco.alert_cta-all')}</Typography>
                     </CTAContent>
                 </Button>
                 <Button size="xs" backgroundColor="teal" fluid onClick={onTurboClick}>
                     <CTAContent>
-                        <Typography>{`Use Turbo️`}</Typography>
+                        <Typography>{t('modes.eco.alert_cta-turbo')}</Typography>
                         <Icon src={turboIcon} />
                     </CTAContent>
                 </Button>
