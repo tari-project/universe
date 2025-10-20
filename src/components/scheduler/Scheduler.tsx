6618208a3a475831f9908dba9a050456a8fcc899
Shannon Tenner <shanimal@tari.com>
2025-10-20 16:07:13 +0200

    wip: add callbacks & invoke

--- src/components/scheduler/Scheduler.tsx
+++ src/components/scheduler/Scheduler.tsx
@@ -3,22 +3,48 @@ import { TextButton } from '@app/components/elements/buttons/TextButton.tsx';
 import { MiningMode } from '@app/components/mode/MiningMode.tsx';
 import { TimePicker } from '@app/components/elements/inputs/time-picker/TimePicker.tsx';
 import { ContentWrapper, CTA, CTAText, FormWrapper, Text, Wrapper } from './styles.ts';
+import { useState } from 'react';
+import { TimeParts } from '@app/components/elements/inputs/time-picker/types.ts';
+import { setShowScheduler } from '@app/store/stores/useModalStore.ts';
+import { invoke } from '@tauri-apps/api/core';
+
+const INIT_START: TimeParts = { hour: '06', minute: '00', cycle: 'AM' };
+const INIT_END: TimeParts = { hour: '04', minute: '30', cycle: 'PM' };
 
 export default function Scheduler() {
+    const [startTime, setStartTime] = useState<TimeParts>(INIT_START);
+    const [endTime, setEndTime] = useState<TimeParts>(INIT_END);
+
+    function handleSave() {
+        console.info('Saving schedule');
+        //invoke
+
+        const payload = {
+            eventId: 'mining_schedule',
+            startTimeValue: Number(startTime.hour),
+            startTimePeriod: startTime.cycle,
+            endTimeValue: Number(endTime.hour),
+            endTimePeriod: endTime.cycle,
+        };
+
+        invoke('add_scheduler_between_event', payload)
+            .then(() => console.info('Saved!'))
+            .catch(console.error);
+    }
     return (
         <Wrapper>
             <ContentWrapper>
                 <Typography variant="h1">{`Mining Schedule`}</Typography>
                 <Text variant="p">{`Set specific times to automatically start mining.`}</Text>
                 <FormWrapper>
-                    <TimePicker label={`Daily Start Time`} initialTime={{ hour: '06', minute: '00', cycle: 'AM' }} />
-                    <TimePicker label={`Daily End Time`} initialTime={{ hour: '04', minute: '30', cycle: 'PM' }} />
+                    <TimePicker label={`Daily Start Time`} initialTime={startTime} handleOnChange={setStartTime} />
+                    <TimePicker label={`Daily End Time`} initialTime={endTime} handleOnChange={setEndTime} />
                     <MiningMode variant="secondary" />
                 </FormWrapper>
             </ContentWrapper>
             <ContentWrapper>
-                <CTA variant="black" size="xlarge" fluid>{`Save schedule`}</CTA>
-                <TextButton fluid>
+                <CTA variant="black" size="xlarge" fluid onClick={handleSave}>{`Save schedule`}</CTA>
+                <TextButton fluid onClick={() => setShowScheduler(false)}>
                     <CTAText>{`Cancel`}</CTAText>
                 </TextButton>
             </ContentWrapper>
