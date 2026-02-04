8c07ab9eeabb087499ce5478750ea7a26394a09b
Shannon Tenner <shanimal@tari.com>
2026-02-04 10:13:52 +0200

    wip: move to node settings

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -2188,15 +2188,17 @@ pub async fn set_custom_node_directory(path: String) -> Result<(), InvokeError> 
         .await;
 
     let new_dir: PathBuf = PathBuf::from(path);
+    let new_dir_full = new_dir.join("node");
 
     match ConfigCore::update_node_data_directory(new_dir.clone()).await {
         Ok(previous) => {
             if let Some(previous) = previous {
                 info!(target: LOG_TARGET_APP_LOGIC, "[ set_custom_node_directory ] previous path {:?}, updating to {:?}", previous, new_dir.clone());
                 let prev_dir_full = previous.join("node");
-                let new_dir_full = new_dir.join("node");
                 fs::rename(prev_dir_full, new_dir_full)
                     .map_err(|e| InvokeError::from(e.to_string()))?;
+            } else {
+                fs::create_dir_all(new_dir_full).map_err(|e| InvokeError::from(e.to_string()))?;
             }
         }
         Err(e) => error!(target: LOG_TARGET_APP_LOGIC, "Could not update directory: {e}"),
--- src/containers/floating/Settings/sections/general/GeneralSettings.tsx
+++ src/containers/floating/Settings/sections/general/GeneralSettings.tsx
@@ -12,12 +12,10 @@ import VisualMode from '@app/containers/main/Dashboard/components/VisualMode.tsx
 
 import AirdropNotificationsSettings from './AirdropNotificationSettings.tsx';
 import { TaskTrayModeSettings } from './TaskTrayModeSettings.tsx';
-import NodeDataLocationSettings from './NodeDataLocation.tsx';
 
 export const GeneralSettings = () => {
     return (
         <>
-            <NodeDataLocationSettings />
             <StartApplicationOnBootSettings />
             <AutoUpdate />
             <PreReleaseSettings />
