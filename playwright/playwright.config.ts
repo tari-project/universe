ed7d35a5826923c267899e14be8f0158525e8a8f
brianp <brian.o.pearce@gmail.com>
2026-03-18 11:44:44 +0100

    reduce motion in tests

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   testDir: './tests',
   fullyParallel: false,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  retries: 0,
   workers: 1,
   reporter: [['html', { open: 'never' }], ['list']],
 
@@ -12,6 +12,7 @@ export default defineConfig({
     trace: 'off',
     screenshot: 'off',
     video: 'off',
+    reducedMotion: 'reduce',
   },
 
   projects: [
